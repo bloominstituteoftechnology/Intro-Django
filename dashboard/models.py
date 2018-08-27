8072e0eeaced8185f4e97439fa64a17fc07ebc20
Fausto Wladimir Fraga <wladimir917@hotmail.com>
2018-08-27 18:01:02 -0400

    Create an App: Lambda Dashboard

---
+++ dashboard/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from uuid import uuid4
+
+# Create your models here.
+class StudentReport(models.Model):
+  student_report_id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+  current_section = models.CharField(max_length=10)
+  project_manager = models.CharField(max_length=30)
+  submitted_sprints = models.PositiveSmallIntegerField()
+  passed_sprints = models.PositiveSmallIntegerField()
+  sprint_pass_rate = models.PositiveSmallIntegerField()
+
+class Sprint(models.Model):
+  sprint_id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+  student_report_id = models.ForeignKey(StudentReport)
+  sprint_name = models.CharField(max_length=40)
+  sprint_status = models.CharField(max_length=20)
+  submission_self_rating = models.PositiveSmallIntegerField()
+  what_went_well = models.TextField(blank=True)
+  what_could_have_gone_better = models.TextField(blank=True)
+  review_rating: models.PositiveSmallIntegerField()
+  review_great: models.TextField(blank=True)
+  requested_improvements = models.TextField(blank=True)
