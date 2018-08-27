0bee657e42b55ea372b94afa47342ea587154624
Danny Vail <danny_vail@outlook.com>
2018-08-27 17:49:15 -0400

    CREATE TASK MODEL In models.py - Add Task data model

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+
+class Task(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid4)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
