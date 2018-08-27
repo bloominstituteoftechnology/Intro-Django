bdc02f2740b3995e99e2443e7c1d7d85b01b675f
Danny Vail <danny_vail@outlook.com>
2018-08-27 18:20:11 -0400

    ADD NEW FIELDS TO TASK DATA MODEL In models.py - Add created_at and last_modified to Task data model

--- notes/models.py
+++ notes/models.py
@@ -6,3 +6,5 @@ class Task(models.Model):
     id = models.UUIDField(primary_key=True, editable=False, default=uuid4)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
