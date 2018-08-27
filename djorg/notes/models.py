68f232ecf610886c982444d9b75b146219f221c5
jesuarva <jesuarva.developer@gmail.com>
2018-08-27 21:37:35 +0200

    MIGRATE Notes-model

--- djorg/notes/models.py
+++ djorg/notes/models.py
@@ -3,6 +3,7 @@ from uuid import uuid4  # To generate a random-unique identifier
 
 
 class Notes (models.Model):
+    # DONE: makemigrations && migrate
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
