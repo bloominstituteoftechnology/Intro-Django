d1e41f68e5b7c494b41a48aa238fc0ec6610e873
Danny Vail <danny_vail@outlook.com>
2018-08-27 18:02:56 -0400

    MOVE KEY TO .ENV FILE In settings.py - Move SECRET_KEY to .env file and link with python-decouple - Move DEBUG to .env file and link with python-decouple

--- djorg/settings.py
+++ djorg/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q8xo%xh59griu2qiq*-ap4+rx+##1b(lkp=^p^4m*lw#2$re$('
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
