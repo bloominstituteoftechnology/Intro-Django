8a0d4d73aa3e0de603ddae61da0f19d42a69f179
bbenefield89 <bsquared18@gmail.com>
2018-08-28 13:14:39 -0400

    ADD NEW FIELDS
    - in notes/models.py
    created_at & last_modified

--- notes/admin.py
+++ notes/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Note
 
 # Register your models here.
+class NoteAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'last_modified')
 
-
-admin.site.register(Note)
+admin.site.register(Note, NoteAdmin)
