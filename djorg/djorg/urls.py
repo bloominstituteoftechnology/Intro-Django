fef597a8d08ff3fbd0eefa68ac7178b0139bad40
jesuarva <jesuarva.developer@gmail.com>
2018-08-29 00:44:58 +0200

    END PART-2 Of Django tutorial.

--- djorg/djorg/urls.py
+++ djorg/djorg/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.urls import path
 from django.contrib import admin
-from django.urls import path, include
+# from django.conf.urls import include
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
+    # path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
--- djorg/notes/models.py
+++ djorg/notes/models.py
@@ -2,8 +2,14 @@ from django.db import models
 from uuid import uuid4  # To generate a random-unique identifier
 
 
-class Notes (models.Model):
+class Note (models.Model):
     # DONE: makemigrations && migrate
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
