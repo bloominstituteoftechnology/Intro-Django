fe13140510402f404032046bd92f83fab53be7db
Fausto Wladimir Fraga <wladimir917@hotmail.com>
2018-08-28 16:51:10 -0400

    PersonalNote Inherits from Note

--- notes/admin.py
+++ notes/admin.py
@@ -3,4 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from .models import Note
 
-admin.site.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+  readonly_fields=('created_at', 'last_modified')
+
+admin.site.register(Note, NoteAdmin)
--- notes/models.py
+++ notes/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from uuid import uuid4
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Note(models.Model):
@@ -8,3 +9,9 @@ class Note(models.Model):
   content = models.TextField(blank=True)
   created_at = models.DateTimeField(auto_now_add=True)
   last_modified = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+     return self.title
+
+class PersonalNote(Note):   # Inherits from Note!
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
