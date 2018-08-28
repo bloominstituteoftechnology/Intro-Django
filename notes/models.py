290ce84e2f032a1a6f98435b94b8c2be8e44eca7
groov1234 <c.groover@outlook.com>
2018-08-28 18:45:59 -0400

    day-2

--- notes/models.py
+++ notes/models.py
@@ -1,8 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 from uuid import uuid4
 
 # Create your models here.
 class Note(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
-    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'<{self.__class__.__name__}: {self.id} {self.title}>'
+
+class PersonalNote(Note):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
