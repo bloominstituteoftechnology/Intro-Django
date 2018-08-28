71588ac7756321ac5dd28986023929db63f3412b
udykas <udykas@gmail.com>
2018-08-28 11:26:53 -0400

    Fixed my problems from yesterday, can now save tasks

--- todo/models.py
+++ todo/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from uuid import uuid4
 
+# Create your models here.
+
 class Todo(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=100)
