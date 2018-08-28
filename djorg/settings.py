0eb21663993461ee1321c4a11de010404c99e827
Cassandra Lamendola <cass.lamendola@gmail.com>
2018-08-28 11:59:36 -0700

    install rest framework and set up permissions

--- djorg/settings.py
+++ djorg/settings.py
@@ -38,8 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'notes'
+    'notes',
+    'rest_framework'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
