477894d9b79ec8a5cea3d061b8fd6318b5d0a889
eddygonzalez9708 <edward.gonzalez9708@gmail.com>
2018-08-27 23:06:32 -0400

    Added migrations and data models.'

--- guides/Day1/django1/settings.py
+++ guides/Day1/django1/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django1.notes', 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
