70e17d4b80a5a495f4b16e7ac6d0a8a7639d5efc
eddygonzalez9708 <edward.gonzalez9708@gmail.com>
2018-08-28 00:23:37 -0400

    Added data with the python shell.

--- guides/Day1/django1/settings.py
+++ guides/Day1/django1/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django1.notes', 
+    'notes', 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
