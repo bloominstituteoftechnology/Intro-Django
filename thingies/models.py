de0231ce09bc3f51d53af5c76a63fb4413ba93fd
Zach Campbell <beazlebubbles@gmail.com>
2018-08-28 16:19:55 -0600

    Added personal note functionality

--- thingies/models.py
+++ thingies/models.py
@@ -7,3 +7,5 @@ class Thingy(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     thingy = models.CharField(max_length=200)
     adjective = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
