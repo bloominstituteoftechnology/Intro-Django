03e8bc3a9fdb297fd1c9b5793bf79bae02930d21
Danny Vail <danny_vail@outlook.com>
2018-08-27 22:52:20 -0400

    REMOVE NOTES/URLS.PY AND USE DJORG/URLS.PY In djorg/urls.py - Add code from notes/urls.py -- should have gone into project folder

--- djorg/urls.py
+++ djorg/urls.py
@@ -15,6 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework import routers
+from notes.api import PersonalNoteViewset
+
+router = routers.DefaultRouter()
+router.register(r'notes', PersonalNoteViewset)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- notes/urls.py
+++
@@ -1,5 +0,0 @@
-from rest_framework import routers
-from notes.api import PersonalNoteViewset
-
-router = routers.DefaultRouter()
-router.register(r'notes', PersonalNoteViewset)
