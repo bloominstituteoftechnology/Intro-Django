501f4b34db052b24e08951622c02e8aa90ef657e
Chris Honda <christopher.honda92@gmail.com>
2018-08-28 12:56:34 -0700

    Added book models and admin privileges

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Book, PersonalBook
 
 # Register your models here.
+admin.site.register(Book)
+admin.site.register(PersonalBook)
--- books/models.py
+++ books/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from uuid import uuid4
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -7,3 +8,8 @@ class Book(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=200)
     genre = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+class PersonalBook(Book):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
