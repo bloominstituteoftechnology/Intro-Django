87b8f596b53081c563cf96b12b78e9dac57399f0
udykas <udykas@gmail.com>
2018-08-28 17:39:17 -0400

    MVP complete for day 2

--- todo/admin.py
+++ todo/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Todo
+from .models import Todo, PersonalTodo
 
 class TodoAdmin(admin.ModelAdmin):
     readonly_fields=('created_at', 'last_modified')
 
-admin.site.register(Todo, TodoAdmin)
+admin.site.register((Todo, PersonalTodo), TodoAdmin)
