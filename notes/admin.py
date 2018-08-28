afb3715e5721bc9bd4b48279717cfc48ba46b2ed
A.D.Faris <adfaris@gmail.com>
2018-08-28 18:46:09 -0400

    Day 2 Complete

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Note, PersonalNote
+class NoteAdmin(admin.ModelAdmin):
+  readonly_fields=('created_at', 'last_modified')
+
+
+admin.site.register((Note, PersonalNote),NoteAdmin)
+# admin.site.register(PersonalNote)
 
 # Register your models here.
--- notes/models.py
+++ notes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from uuid import uuid4
 
 # Create your models here.
@@ -6,4 +7,15 @@ class Note(models.Model):
   id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
   title = models.CharField(max_length = 200)
   content = models.TextField(blank=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+  last_modified = models.DateTimeField(auto_now=True)
+  # def __str__(self):
+  #     # return f'<note:{self.id} {self.title}>'
+  #     return self.title,self.id
 
+# inherit from Note
+class PersonalNote(Note):
+    user = models.ForeignKey(User, on_delete= models.CASCADE)
+    # def __str__(self):
+    #   # return f'<note:{self.id} {self.title}>'
+    #   return self.title,self.id
