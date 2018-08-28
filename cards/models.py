9e4502af2b0f2b59629f52d002503deea9b0f3e7
Bee C <b.choeung@gmail.com>
2018-08-28 19:04:52 -0400

    completed admin registration, working on mvp

--- cards/models.py
+++ cards/models.py
@@ -1,34 +1,37 @@
 from django.db import models
-from enum import Enum
+# from enum import Enum
 from uuid import uuid4
 # Create your models here.
-class ColorChoice(Enum):
-    W = "white"
-    B = "black"
-    R = "red"
-    G = "green"
-    U = "blue"
-class TypeChoice(Enum):
-    sorcery = "sorcery"
-    instant = "instant"
-    land = "land"
-    creature = "creature"
+# class ColorChoice(Enum):
+#     W = "white"
+#     B = "black"
+#     R = "red"
+#     G = "green"
+#     U = "blue"
+# class TypeChoice(Enum):
+#     sorcery = "sorcery"
+#     instant = "instant"
+#     land = "land"
+#     creature = "creature"
 class Card(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=128)
     cost = models.CharField(max_length=16)
     color = models.CharField(
         max_length = 32,
-        choices= [(color, color.value) for color in ColorChoice]
+        # choices= [(color, color.value) for color in ColorChoice]
     )
     cmc = models.IntegerField()
     cardType = models.CharField(
         max_length = 128,
-        choices= [(ctype, ctype.value) for ctype in TypeChoice]
+        # choices= [(ctype, ctype.value) for ctype in TypeChoice]
     )
     subType = models.CharField(max_length=32)
     cardSet = models.CharField(max_length=128)
     text = models.TextField()
+
+    def __str__(self):
+        return self.name
 """
 c = Card(name="Beejeezee", cost="w", color=ColorChoice.W, cmc=1, cardType=TypeChoice.creature, subType="bird", cardSet="the best set", text="Split Second\nWin the game.")
 """
--- djorg/settings.py
+++ djorg/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +21,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(066=^bcnjz5c%t=1i^a3oig-pg!l)cl+dnv3f@r9mo$vh0r2y'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
+
 
 
 # Application definition
