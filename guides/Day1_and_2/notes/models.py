f26c06d3ed6d86915fcccdc26f0c7e22dc144e66
eddygonzalez9708 <edward.gonzalez9708@gmail.com>
2018-08-28 18:05:58 -0400

    Setup personal notes instead of general ones. Implemented user accounts.

--- guides/Day1_and_2/notes/models.py
+++ guides/Day1_and_2/notes/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from uuid import uuid4
+from django.contrib.auth.models import User 
 
 class Note(models.Model): 
     # Create your models here.
@@ -8,3 +9,12 @@ class Note(models.Model):
     content = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self): 
+        return f'<{self.__class__.__name__}: {self.id} {self.title}>'
+
+class PersonalNote(Note): # Inherits from Note! 
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self): 
+        return f'<{self.__class__.__name__}: {self.id} {self.title}>'
