e220ee75712edb73691e8162e7519de371cbff38
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-08-28 14:19:15 -0500

    add to Note model & add PersonalNote class also format data display on admin interface

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Note, PersonalNote
+
 
 # Register your models here.
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'last_modified', "id")
+    readonly_fields = ('created_at', 'last_modified')
+
+admin.site.register((Note, PersonalNote), NoteAdmin)
+
+
