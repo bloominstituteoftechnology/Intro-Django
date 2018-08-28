854d9bfe5179718844bd6918a7e1adbfc1075443
LordOrbnauticus <haywood.d.johnson@gmail.com>
2018-08-28 17:56:59 -0500

    .env; day one officially complete

--- djorg/settings.py
+++ djorg/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECTRET_KEY')
+SECRET_KEY = config("SECTRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = config("DEBUG", cast=bool)
 
 ALLOWED_HOSTS = []
 
