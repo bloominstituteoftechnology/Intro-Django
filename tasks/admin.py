dead6a843428b40ce047866440d061582cab6e94
Bradiowave <divebombwalker@gmail.com>
2018-08-28 13:39:40 -0700

    caught up to lecture and completed with own app

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Task, PersonalTask
+
+admin.site.register((Task, PersonalTask))
+
 # Register your models here.
--- tasks/models.py
+++ tasks/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from uuid import uuid4
 
 class Task(models.Model):
@@ -6,5 +7,8 @@ class Task(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(blank=True)
     isComplete = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
 
-# Create your models here.
+class PersonalTask(Task):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
