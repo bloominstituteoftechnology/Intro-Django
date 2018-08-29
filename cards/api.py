e7f9b7e1d55d291f8ab0b31b4ef1256916a4fc2a
mister-corn <code@ronaldlibago.com>
2018-08-28 21:04:54 -0700

    21:04 PDT 28-Aug: User may post cards to their collection.

--- cards/api.py
+++ cards/api.py
@@ -1,12 +1,26 @@
-from rest_framework import serializers, viewset
-from models import UserCollection
-from models import ColorChoice
-from models import TypeChoices
+from rest_framework import serializers, viewsets
+from .models import UserCollection
+from .models import ColorChoice
+from .models import TypeChoice
 
 class UserCollectionSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = UserCollection
-        fields = ('name', 'color')
+        fields = (
+            'name',
+            'cost',
+            'cmc',
+            'color',
+            'cardType',
+            'subType',
+            'cardSet',
+            'text'
+        )
+    
+    def create(self, validated_data):
+        user = self.context['request'].user
+        note = UserCollection.objects.create(user=user, **validated_data)
+        return note
 
 class UserCollectionViewSet(viewsets.ModelViewSet):
     serializer_class = UserCollectionSerializer
