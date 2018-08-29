716dfa2e65e502bae02d0e5ceb8226c18ef666dc
Fausto Wladimir Fraga <wladimir917@hotmail.com>
2018-08-29 17:08:56 -0400

    CORS setup

--- djorg/settings.py
+++ djorg/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'notes',
     'dashboard',
     'rest_framework',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -133,3 +135,5 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
     ]
 }
+
+CORS_ORIGIN_ALLOW_ALL = True
