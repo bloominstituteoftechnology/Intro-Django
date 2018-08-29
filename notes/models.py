2696176332f9bd57067853acf1bf057ee27c77c1
sagardesai90 <saagardesai@gmail.com>
2018-08-29 18:13:04 -0500

    added dates to notes

--- notes/models.py
+++ notes/models.py
@@ -6,4 +6,8 @@ class Note(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
 
-    # created_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'<Note: {self.title} {self.id}>'
