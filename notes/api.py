eb507ed95ec699dfddae74c9479683d7b1dd4637
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-08-30 05:07:52 +0800

    Did the following:
    1. Exposed the personal notes by creating api.py and implementing its functions.
    
    2. Added routes.
    
    3.Tested the API.
    
    4. Added the required user field.

---
+++ notes/api.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers, viewsets
+from .models import PersonalNote
+
+class PersonalNoteSerializer(serializers.HyperlinkedModelSerializer):
+    
+    def create(self, validated_data):
+        user = self.context['request'].user
+        note = PersonalNote.objects.create(user=user, **validated_data)
+        return note
+
+    class Meta:
+        model = PersonalNote
+        fields = ('title', 'content')
+
+class PersonalNoteViewSet(viewsets.ModelViewSet):
+    serializer_class = PersonalNoteSerializer
+    queryset = PersonalNote.objects.all()
