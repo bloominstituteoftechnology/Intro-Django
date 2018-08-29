b0608b2eebab1fa5c982a420b9b7a33262a7e259
Fausto Wladimir Fraga <wladimir917@hotmail.com>
2018-08-29 15:48:17 -0400

    add rest_framework into Application definition

--- djorg/settings.py
+++ djorg/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = [
 INSTALLED_APPS = [
     'notes',
     'dashboard',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
