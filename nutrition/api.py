cf409fc427e6ee114c8e834c7c003022575c095a
bbenefield89 <bsquared18@gmail.com>
2018-08-29 15:17:04 -0400

    CREATE SERIALIZER & VIEWSET FOR `PersonalMeal`
    - in nutrition/api.py

---
+++ nutrition/api.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers, viewsets
+from .models import PersonalMeal
+
+class PersonalMealSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = PersonalMeal
+        fields = ('type', 'name', 'date', 'calories', 'carbs', 'protein')
+    
+    def create(self, validated_data):
+        user = self.context['request'].user
+        meal = PersonalMeal.objects.create(user=user, **validated_data)
+
+        return meal
+
+
+class PersonalMealViewSet(viewsets.ModelViewSet):
+    serializer_class = PersonalMealSerializer
+    queryset = PersonalMeal.objects.none()
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.is_anonymous:
+            return queryset
+        else:
+            return PersonalMeal.objects.filter(user=user)
