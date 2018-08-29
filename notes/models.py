1844af7d60909b4eec8b05f0c60a9d7d652114da
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-08-29 16:27:26 +0800

    Added new fields "created_at" and "last_modified" and made migrations.

--- notes/models.py
+++ notes/models.py
@@ -6,3 +6,5 @@ class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
