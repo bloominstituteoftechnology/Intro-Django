26fb8529894d3e35c97e2ff3ef6e1a2be13754d0
sagardesai90 <saagardesai@gmail.com>
2018-08-29 16:20:10 -0500

    day 1 fin

--- notes/models.py
+++ notes/models.py
@@ -5,3 +5,5 @@ class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
+
+    # created_at = models.DateTimeField()
