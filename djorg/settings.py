f18bd817e9fae88001edf9c8506f5f85e32517da
samarv <samarvir1996@gmail.com>
2018-08-31 03:42:06 +0530

    heroku deploy2

--- djorg/settings.py
+++ djorg/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 from decouple import config,Csv
 import dj_database_url
+import whitenoise
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -35,7 +36,6 @@ dj_database_url.config(default=config('DATABASE_URL'))
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'rest_framework.authtoken',
     'rest_framework',
     'notes',
