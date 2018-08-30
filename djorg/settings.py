a16694e94da397f7483671735a3f8f8e4ab24b2d
bbenefield89 <bsquared18@gmail.com>
2018-08-30 16:14:20 -0400

    REMOVE `STATICFILES_STORAGE`
    - in settings.py

--- djorg/settings.py
+++ djorg/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATIC_URL = '/static/'
 
 from rest_framework.authentication import TokenAuthentication
