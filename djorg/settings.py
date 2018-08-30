adb508b9bd8844e792ac1aa5a196767fbcb667be
bbenefield89 <bsquared18@gmail.com>
2018-08-30 16:21:55 -0400

    REMOVE STATIC FILES APP CONFIG
    - in settings.py

--- djorg/settings.py
+++ djorg/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    # 'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
@@ -133,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
 from rest_framework.authentication import TokenAuthentication
 
