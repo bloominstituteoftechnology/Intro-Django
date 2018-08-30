a4e1dabb25fc4066d3e55b3a386fc8a6e07367a4
bbenefield89 <bsquared18@gmail.com>
2018-08-30 13:57:51 -0400

    ADD NEW API AUTH ROUTE `/api-token-auth/`
    - in urls.py

--- djorg/urls.py
+++ djorg/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework import routers
 from notes.api import PersonalNoteViewSet
 from nutrition.api import PersonalMealViewSet
+
+from rest_framework.authtoken import views
 
 router = routers.DefaultRouter()
 router.register('notes', PersonalNoteViewSet)
@@ -26,4 +28,5 @@ router.register('nutrition', PersonalMealViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    re_path('api-token-auth/', views.obtain_auth_token)
 ]
