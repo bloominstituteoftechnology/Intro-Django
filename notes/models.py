fcbf2cd6d81d758e4f679b08e4de8d9188737a4e
sagardesai90 <saagardesai@gmail.com>
2018-08-29 23:37:12 -0500

    day 2 fin

--- notes/models.py
+++ notes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from uuid import uuid4
 # Create your models here.
 class Note(models.Model):
@@ -9,5 +10,10 @@ class Note(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f'<Note: {self.__class__.__name__} {self.title} {self.id}>'
+
+class PersonalNote(Note):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
         return f'<Note: {self.title} {self.id}>'
