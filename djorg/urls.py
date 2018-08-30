9bd7e7e72e5946db3d639c0b83e2b9c299b74f88
Bee C <b.choeung@gmail.com>
2018-08-30 16:10:02 -0400

    Day 4: set up the route

--- djorg/urls.py
+++ djorg/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from cards.api import PersonalCardViewSet
+from django.urls import path, include, re_path
+from rest_framework.authtoken import views
 
 router = routers.DefaultRouter()
 router.register(r'cards', PersonalCardViewSet)
@@ -24,4 +26,5 @@ router.register(r'cards', PersonalCardViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    re_path(r'^api-token-auth/', views.obtain_auth_token),
 ]
