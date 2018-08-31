a7d993e3f505c72f06b822cbd809434e995846d8
groov1234 <c.groover@outlook.com>
2018-08-31 11:34:03 -0400

    databases bug

--- djorg/settings.py
+++ djorg/settings.py
@@ -83,10 +83,14 @@ WSGI_APPLICATION = 'djorg.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
 import dj_database_url
 
-DATABASES['default'] = dj_database_url.config(default='DATABASE_URL')
+DATABASES = {
+    'default': {
+        'ENGINE': dj_database_url.config(default='postgres://...'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 
