973abe725a1553dfc1809053289edcb081896e7c
Zach Campbell <beazlebubbles@gmail.com>
2018-08-31 10:44:40 -0600

    added cast to allowed hosts

--- djorg/settings.py
+++ djorg/settings.py
@@ -36,7 +36,9 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = [config("ALLOWED_HOSTS")]
+ALLOWED_HOSTS = [
+    config("ALLOWED_HOSTS", cast=lambda v: [s.strip() for s in v.split(",")])
+]
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
