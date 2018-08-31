c40ad55f8295e83eb6f13f44cbd10df943da146d
groov1234 <c.groover@outlook.com>
2018-08-31 13:54:48 -0400

    .

--- djorg/settings.py
+++ djorg/settings.py
@@ -142,6 +142,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 from rest_framework.authentication import SessionAuthentication, BaseAuthentication, TokenAuthentication
 
 REST_FRAMEWORK = {
