2ba4a9812f028ce2578023f3943311053c44677f
eddygonzalez9708 <edward.gonzalez9708@gmail.com>
2018-08-31 15:09:03 -0400

    Commented out STATIC_STORAGE variable in settings.py

--- django1/settings.py
+++ django1/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'django1.urls'
 
