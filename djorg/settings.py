206207d38e73785facce447c786110429af73c92
groov1234 <c.groover@outlook.com>
2018-08-31 13:33:50 -0400

    ugh

--- djorg/settings.py
+++ djorg/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'ALLOWED_HOSTS',
+    '127.0.0.1',
+]
 
 CORS_ORIGIN_ALLOW_ALL = True
 
