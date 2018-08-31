1ad45390723185db4ae1911e5a07d1943f23f2aa
groov1234 <c.groover@outlook.com>
2018-08-31 14:50:39 -0400

    .

--- djorg/settings.py
+++ djorg/settings.py
@@ -141,8 +141,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 from rest_framework.authentication import SessionAuthentication, BaseAuthentication, TokenAuthentication
