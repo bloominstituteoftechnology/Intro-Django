4221458ae6aa11aa246439ba23d2b721ac97590b
kelly <kelfro@fastmail.com>
2018-08-30 20:28:48 -0700

    testing something

--- djorg/settings.py
+++ djorg/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
