6db238c5a417182be463022eb457052f96679110
AmyShackles <amyshackles@gmail.com>
2018-08-30 21:44:52 -0700

    What about a forward slash?

--- djorg/urls.py
+++ djorg/urls.py
@@ -26,7 +26,7 @@ router.register(r"notes", PersonalNoteViewSet)
 router.register(r"video_archive", PersonalVideoViewSet)
 
 urlpatterns = [
-    path("", include("django.contrib.auth.urls")),
+    path("/", include("django.contrib.auth.urls")),
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
     re_path(r"^api-token-auth/", views.obtain_auth_token),
