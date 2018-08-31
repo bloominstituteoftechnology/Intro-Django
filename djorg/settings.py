b2ae9573812b0e53bc3cc4dc93fcdadddb4c67d4
Xang Thao <xangthao24@yahoo.com>
2018-08-31 10:26:41 -0700

    Reconfigured the DATABASES in setting

--- djorg/settings.py
+++ djorg/settings.py
@@ -80,11 +80,18 @@ WSGI_APPLICATION = 'djorg.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+# Database using URL. sqlite:///PATH
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')  
+    )
 }
 
 
