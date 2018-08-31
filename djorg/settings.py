2397af6fef2bc647e6e58b15f7f49ece8e1bc3c5
Steve <shalomlandsman@gmail.com>
2018-08-31 17:46:14 -0400

    fix database

--- djorg/settings.py
+++ djorg/settings.py
@@ -24,7 +24,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
-DATABASES = {'default': dj_database_url.config(default='sqlite:///db.sqlite3')}
+DATABASES = {}
+
+DATABASES['default'] = dj_database_url.config("DATABASE_URL", conn_max_age=600)
+
+#DATABASES = {'default': dj_database_url.config(default='sqlite:///db.sqlite3')}
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
