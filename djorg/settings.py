9611acf5d4d12b4d8ec4af8b6c425632f195bd0c
groov1234 <c.groover@outlook.com>
2018-08-31 14:28:56 -0400

    .

--- djorg/settings.py
+++ djorg/settings.py
@@ -139,7 +139,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 from rest_framework.authentication import SessionAuthentication, BaseAuthentication, TokenAuthentication
 
