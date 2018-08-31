a551f34dc7aa342457ae5bb362fd3947650cce2e
Brandon Hopper <brandonhopper0@gmail.com>
2018-08-31 15:09:57 -0400

    -edited import of db

--- djorg/djorg/settings.py
+++ djorg/djorg/settings.py
@@ -1,6 +1,6 @@
 import os
 from decouple import config
-from decouple import dj_database_url
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
