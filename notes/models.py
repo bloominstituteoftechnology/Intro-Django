41ee12184d906b4089ceedb08c515129b5292ed4
holly <holly.giang@penguin>
2018-09-01 01:26:17 +0000

    Day 2 MVP

--- notes/models.py
+++ notes/models.py
@@ -1,7 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User 
+
 from uuid import uuid4 
 
 class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '<{self.__class__.__name__}: {self.id} {self.title}>.format'
+
+class PersonalNote(Note):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
