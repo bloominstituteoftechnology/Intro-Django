20c3d479aa22aaaa7bacf85e3f0ae9a2b87dd19c
Tramane Hall <tramanedhall@gmail.com>
2018-09-02 15:08:08 -0400

    added fields to blog post model

--- techblog/models.py
+++ techblog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    title = models.CharField(max_length=250)
+    content = models.TextField(blank=True)
+    
