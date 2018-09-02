90f077e04db1f422d9f026c718278c1cbdf33775
Tramane Hall <tramanedhall@gmail.com>
2018-09-02 18:56:15 -0400

    added api routes, and filter user functionality

--- blog/settings.py
+++ blog/settings.py
@@ -125,6 +125,6 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionOrAnonReadOnly',
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
     ]
 }
