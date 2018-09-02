cabbafeb44f4e82651d5ff5074f0dcd140e708c7
Tramane Hall <tramanedhall@gmail.com>
2018-09-02 15:52:47 -0400

    edited admin interface and added multiuser functionality

--- techblog/admin.py
+++ techblog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields=('created_at', 'last_modified')
+
+admin.site.register(Post, PostAdmin)
+
+
--- techblog/models.py
+++ techblog/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 from uuid import uuid4
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=250)
     content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+class PersonalNote(Post): 
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     
+
+
