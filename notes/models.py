36d1a0c8783080cab7a7f06742ba5879d2578371
maxwashington <mnwashington18@gmail.com>
2018-09-04 15:09:25 -0500

    Created Personal notes for user

--- notes/models.py
+++ notes/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from uuid import uuid4
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Note(models.Model):
@@ -9,8 +10,6 @@ class Note(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
 
-class NoteAdmin(admin.ModelAdmin):
-	readonly_fields=('created_at', 'last_modified')
+class PersonalNote(Note):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-# Register your models here.
-admin.site.register(Note, NoteAdmin)
