f9cfdc3a8f4acc48e8b90442af1e8e5f8cbf609d
maxwashington <mnwashington18@gmail.com>
2018-09-04 14:46:15 -0500

    Added Timezone to Notes

--- notes/models.py
+++ notes/models.py
@@ -6,3 +6,11 @@ class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=250)
     content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+class NoteAdmin(admin.ModelAdmin):
+	readonly_fields=('created_at', 'last_modified')
+
+# Register your models here.
+admin.site.register(Note, NoteAdmin)
