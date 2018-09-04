d529064bf184d18d10b1db6e3c23010b6cafef1d
FireInjun <mk@FireInjun.com>
2018-09-04 19:14:34 -0400

    9/4/2018 7:14 PM

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,47 +1,51 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 axios.defaults.xsrfHeaderName = "X-CSRFToken";
 axios.defaults.xsrfCookieName = "XCSRF-Token";
-
 
 class App extends Component {
   state = {
     user: {
-      username: '',
-      password: ''
+      username: "",
+      password: ""
     },
-    token: '',
+    token: "",
     notes: [{}]
-  }
+  };
 
   handle_input = (e) => {
-    let temp = JSON.parse(JSON.stringify(this.state.user))
-    temp[e.target.name] = e.target.value
-    this.setState({ user: temp })
-  }
+    let temp = JSON.parse(JSON.stringify(this.state.user));
+    temp[e.target.name] = e.target.value;
+    this.setState({ user: temp });
+  };
 
   handle_sign_in = (e) => {
     let user = this.state.user;
-    axios.post('http://localhost:8000/api-token-auth/', user)
-      .then(response => {
+    axios
+      .post("http://mykealkennydjorg.herokuapp.com/api-token-auth/", user)
+      .then((response) => {
         let header = {
-          Authorization: 'Token ' + response.data.token
-        }
-        let properties = 'id title content url'
-        axios.get(`http://localhost:8000/graphql/?query={notes{${properties}}}`, header)
-          .then(notes_res => {
-            console.log(notes_res)
-            let my_notes = Array.from(notes_res.data.data.notes)
-            this.setState({ notes: my_notes })
+          Authorization: "Token " + response.data.token
+        };
+        let properties = "id title content url";
+        axios
+          .get(
+            `http://mykealkennydjorg.herokuapp.com/graphql/?query={notes{${properties}}}`,
+            header
+          )
+          .then((notes_res) => {
+            console.log(notes_res);
+            let my_notes = Array.from(notes_res.data.data.notes);
+            this.setState({ notes: my_notes });
           })
-          .catch(notes_err => {
-            console.log(notes_err)
-          })
+          .catch((notes_err) => {
+            console.log(notes_err);
+          });
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
-      })
-  }
+      });
+  };
 
   render() {
     // console.log(this.state)
@@ -49,18 +53,19 @@ class App extends Component {
       <div className="App">
         <div>
           <input
-            type='text'
-            name='username'
-            placeholder='username'
+            type="text"
+            name="username"
+            placeholder="username"
             onChange={this.handle_input}
           />
         </div>
         <div>
           <input
-            type='password'
-            name='password'
-            placeholder='password'
-            onChange={this.handle_input} />
+            type="password"
+            name="password"
+            placeholder="password"
+            onChange={this.handle_input}
+          />
         </div>
         <div>
           <button onClick={this.handle_sign_in}>Sign In</button>
@@ -74,7 +79,7 @@ class App extends Component {
                 <p>URL: {note.url}</p>
                 <p>Author: {this.state.user.username}</p>
               </div>
-            )
+            );
           })}
         </div>
       </div>
