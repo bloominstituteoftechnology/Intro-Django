24edf64e01e76c0690bd5a1fb216ccc56a1e163b
maxwashington <mnwashington18@gmail.com>
2018-09-04 13:53:29 -0500

    Created Note class At id to Notes

--- notes/models.py
+++ notes/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
 class Note(models.Model):
-    this.name
-    this.user
-    
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    title = models.CharField(max_length=250)
+    content = models.TextField(blank=True)
