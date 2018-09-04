144ebaf31d0ebb629dfc36a1f8acb6b03ec3293e
FireInjun <mk@FireInjun.com>
2018-09-04 19:01:31 -0400

    9/4/2018 7:01 PM

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+from notes.models import PersonalNote
 
 # Create your views here.
+def notes(request):
+    notes_list = PersonalNote.objects.all()
+    template = loader.get_template("notes.html")
+    context = {"notes": notes_list}
+    return HttpResponse(template.render(context, request))
+
