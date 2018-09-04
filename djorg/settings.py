ebf89572eac3c3ff913c457e312b22083a17ed7a
AmyShackles <amyshackles@gmail.com>
2018-09-04 13:11:34 -0700

    Added some bootstrap

--- djorg/settings.py
+++ djorg/settings.py
@@ -140,6 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
