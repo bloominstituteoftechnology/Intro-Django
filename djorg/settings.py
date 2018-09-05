b04a9ebf2fe8eed5091345d2d7c4f621a54715a1
april <martinezapril56@yahoo.com>
2018-09-05 08:40:10 -0400

    addingframework

--- djorg/settings.py
+++ djorg/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'notes',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -120,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES':[
+        'rest_framework.permission.DjangoModelPermissionsOrAnonReadyOnly',
+    ]
+}
