ec43a3fdcb883af920a77d8c6d2bfb169a27e0da
Jacob Henry <jacobiswinning@gmail.com>
2018-09-05 13:18:23 -0600

    added class Note, created two fields title and content

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
+from uuid import uuid4
 # Create your models here.
+class Note(models.Model):
+  title = models.CharField(max_length=200)
+  content = models.TextField(blank=True)
