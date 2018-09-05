b61ad5458942ba8021e5706ce5f2b9de6d325e07
maribelcuales <maribelcuales@gmail.com>
2018-09-06 01:04:50 +0800

    format string to return note id and title

--- notes/models.py
+++ notes/models.py
@@ -7,3 +7,6 @@ class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
+
+    def __str__(self):
+        return f'<Note: {self.id} {self.title}>'  # return format string for note title
