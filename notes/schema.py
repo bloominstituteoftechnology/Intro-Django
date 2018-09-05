3dcf78be134e6be437531a0ccf7aa9642032fa3b
kelly <kelfro@fastmail.com>
2018-09-05 12:31:23 -0700

    Finished Day 2 GraphQl - Mutations

--- notes/schema.py
+++ notes/schema.py
@@ -19,4 +19,26 @@ class Query(graphene.ObjectType):
         else:
             return PersonalNoteModel.objects.filter(user=user)
 
-schema = graphene.Schema(query=Query)
+class CreatePersonalNote(graphene.Mutation):
+    class Arguments:
+        title = graphene.String()
+        content = graphene.String()
+
+    personalnote = graphene.Field(PersonalNote)
+    ok = graphene.Boolean()
+    status = graphene.String()
+
+    def mutate(self, info, title, content):
+        user = info.context.user
+
+        if user.is_anonymous:
+            return CreatePersonalNote(ok=False, status="Must be logged in!")
+        else:
+            new_note = PersonalNoteModel(title=title, content=content, user=user)
+            new_note.save()
+            return CreatePersonalNote(personalnote=new_note, ok=True, status="ok")
+
+class Mutation(graphene.ObjectType):
+    create_personal_note = CreatePersonalNote.Field()
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
