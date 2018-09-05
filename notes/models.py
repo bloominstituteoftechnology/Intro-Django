7e16e7c320d7ba1377020b679aeb76d6088dc491
Jacob Henry <jacobiswinning@gmail.com>
2018-09-05 13:20:25 -0600

    added note to sttings.py

--- notes/models.py
+++ notes/models.py
@@ -4,3 +4,4 @@ from uuid import uuid4
 class Note(models.Model):
   title = models.CharField(max_length=200)
   content = models.TextField(blank=True)
+  id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
