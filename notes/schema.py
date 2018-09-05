cdd6924556866b38514894ed81c9c2363ea30771
JSNavey <jsnavey@gmail.com>
2018-09-05 18:06:18 -0400

    Add schema

---
+++ notes/schema.py
@@ -0,0 +1,29 @@
+from graphene_django import DjangoObjectType
+import graphene
+from .models import PersonalNote as PersonalNoteModel
+
+class PersonalNote(DjangoObjectType):
+    """ Describe which model we want to expose through GraphQL."""
+    class Meta:
+        model = PersonalNoteModel
+        # Describe the data as a node in a graph for GraphQL
+        interfaces = (graphene.relay.Node, )
+
+class Query(graphene.ObjectType):
+    """ Describe which records we want to show."""
+    personalnotes = graphene.List(PersonalNoteType)
+
+    def resolve_notes(self, info):
+        """ Decide what notes to return."""
+        user = info.context.user    # Find this with the debugger
+
+        if  user.is_anonymous:
+            return PersonalNoteModel.objects.none()
+        else:
+            return PersonalNoteModel.objects.filter(user=user)
+
+# Add a schema and attach to the query
+schema = graphene.Schema(query=Query)
+
+
+
