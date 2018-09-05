dbd979bbe761c27d1d1f91bd1bb36d251fe7661b
maxwashington <mnwashington18@gmail.com>
2018-09-05 11:21:26 -0500

    Installed REST framework Added paths too personal notes/user notes Created meta classes for PersonalNotes

--- djorg/settings.py
+++ djorg/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'notes',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -102,7 +103,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ]
+}
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
