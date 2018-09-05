c28743cd7771174ba32c07178c992761df4d3964
Jacob Henry <jacobiswinning@gmail.com>
2018-09-05 15:15:21 -0600

    added Viewsetclass

--- notes/api.py
+++ notes/api.py
@@ -6,3 +6,7 @@ class PersonalNoteSerializer(serializers.HyperlinkedModelSerializer):
   class Meta:
         model = PersonalNote
         fields = ('title', 'content')
+
+class PersonalNoteViewSet(viewsets.ModelViewSet):
+  serializer_class = PersonalNoteSerializer
+  queryset = PersonalNote.objects.all()
