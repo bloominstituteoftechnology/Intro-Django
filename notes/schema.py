457fa7f5e25d5aeea5e49b20b9e25ebc2ade166a
Danny Vail <danny_vail@outlook.com>
2018-09-06 10:56:58 -0400

    DEFINE GRAPHQL SCHEMA In schema.py - Define a schema that describes the data for GraphQL

---
+++ notes/schema.py
@@ -0,0 +1,22 @@
+from graphene_django import DjangoObjectType
+import graphene
+from .models import PersonalNote as PersonalNoteModel
+
+
+class PersonalNote(DjangoObjectType):
+    class Meta:
+        model = PersonalNoteModel
+        interfaces = (graphene.relay.Node, )
+
+
+class Query(graphene.ObjectType):
+    personal_notes = graphene.List(PersonalNote)
+
+    def resolve_personal_notes(self, info):
+        user = info.contet.user
+        if user.is_anonymous:
+            return PersonalNoteModel.objects.none()
+        return PersonalNoteModel.objects.filter(user=user)
+
+
+schema = graphene.Schema(query=Query)
