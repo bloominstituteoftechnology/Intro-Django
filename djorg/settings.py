aa5b147c6061e518177d91569ca4edf49ce46fb2
Obo Agboghidi <oagboghi2@gmail.com>
2018-09-06 16:17:45 -0700

    fixed bug I was having that was breaking django

--- djorg/settings.py
+++ djorg/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
     'corsheader'
 ]
 
