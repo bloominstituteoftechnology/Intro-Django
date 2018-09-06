c129d032c43f841f1f272ba3124b31536997b63f
Danny Vail <danny_vail@outlook.com>
2018-09-06 11:01:55 -0400

    SETUP GRAPHQL URL ENDPOINT In urls.py - Add GraphQL endpoint to graphql/

--- djorg/urls.py
+++ djorg/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework import routers
 from rest_framework.authtoken import views
+from graphene_django.views import GraphQLView
 from notes.api import PersonalNoteViewset
 
 router = routers.DefaultRouter()
@@ -24,6 +25,7 @@ router.register(r'notes', PersonalNoteViewset)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'api/', include(router.urls)),
-    re_path(r'^api-token-auth', views.obtain_auth_token)
+    path('graphql', GraphQLView.as_view(graphiql=True)),
+    path('api/', include(router.urls)),
+    re_path('^api-token-auth', views.obtain_auth_token)
 ]
