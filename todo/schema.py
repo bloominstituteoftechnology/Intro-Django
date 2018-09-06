b331c383f7bb082b492cbf24772efed16965882c
udykas <udykas@gmail.com>
2018-09-06 16:13:37 -0400

    Finished GraphQL day 2. Can add a new personal note from GraphQL endpoint

--- todo/schema.py
+++ todo/schema.py
@@ -20,4 +20,56 @@ class Query(graphene.ObjectType):
         else:
             return PersonalTodo.objects.filter(user=user)
 
-schema = graphene.Schema(query=Query)
+class CreatePersonalTodo(graphene.Mutation):
+
+    class Arguments:
+        # Input fields
+        title = graphene.String()
+        description = graphene.String()
+
+    # Output fields
+    personaltodo = graphene.Field(PersonalTodoType)
+    ok = graphene.Boolean()
+    status = graphene.String()
+
+    def mutate(self, info, title, description):
+        
+        user = info.context.user
+
+        if user.is_anonymous:
+            return CreatePersonalTodo(ok=False, status="must be logged in")
+        else:
+            new_todo = PersonalTodo(title=title, description=description, user=user)
+            new_todo.save()
+            return CreatePersonalTodo(personaltodo=new_todo, ok=True, status="success")
+
+class Mutation(graphene.ObjectType):
+    create_personal_todo = CreatePersonalTodo.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
+
+
+
+# class CreatePersonalTodo(graphene.Mutation):
+
+#     class Arguments:
+#         title = graphene.String()
+#         description = graphene.String()
+
+#     personaltodo = graphene.Field(PersonalTodo)
+#     ok = graphene.Boolean()
+#     status = graphene.String()
+
+#     def mutate(self, info, title, description):
+#         user = info.context.user
+
+#         if user.is_anonymous:
+#             return CreatePersonalTodo(ok=False, status="Must be logged in!")
+#         else:
+#             new_todo = PersonalTodo(title=title, description=description, user=user)
+#             new_todo.save()
+#             return CreatePersonalTodo(personaltodo=new_todo, ok=True, status="ok")
+
+# class Mutation(graphene.ObjectType):
+#     create_personal_todo = CreatePersonalTodo.Field()
