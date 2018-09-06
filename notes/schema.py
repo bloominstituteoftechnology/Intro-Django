e31ad2d67b89db30c55c2ee7b99167816866c005
Bradiowave <divebombwalker@gmail.com>
2018-09-06 13:18:13 -0700

    added mutation functionality to graphql

--- notes/schema.py
+++ notes/schema.py
@@ -1,22 +1,45 @@
 from graphene_django import DjangoObjectType
 import graphene
-from .models import PersonalNote as PersonalNoteModel
+from .models import PersonalNote
 
-class PersonalNote(DjangoObjectType):
+class PersonalNoteType(DjangoObjectType):
     
     class Meta:
-        model = PersonalNoteModel
+        model = PersonalNote
         interfaces = (graphene.relay.Node, )
 
 class Query(graphene.ObjectType):
-    personalnotes = graphene.List(PersonalNote)
+    notes = graphene.List(PersonalNoteType)
 
-    def resolve_personalnotes(self, info):
+    def resolve_notes(self, info):
         user = info.context.user
 
         if user.is_anonymous:
-            return PersonalNoteModel.objects.none()
+            return PersonalNote.objects.none()
         else:
-            return PersonalNoteModel.objects.filter(user=user)
+            return PersonalNote.objects.filter(user=user)
 
-schema = graphene.Schema(query=Query)
+class CreatePersonalNote(graphene.Mutation):
+
+    class Arguments:
+        title = graphene.String()
+        content = graphene.String()
+
+    personalnote = graphene.Field(PersonalNoteType)
+    ok = graphene.Boolean()
+    status = graphene.String()
+
+    def mutate(self, info, title, content):
+        user = info.context.user
+        
+        if user.is_anonymous:
+            return CreatePersonalNote(ok=False, status="must be logged in")
+        else:
+            new_note = PersonalNote(title=title, content=content, user=user)
+            new_note.save()
+            return CreatePersonalNote(personalnote=new_note, ok=True, status="success")
+
+class Mutation(graphene.ObjectType):
+    create_personal_note = CreatePersonalNote.Field()
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
