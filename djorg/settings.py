854530c7ead30f5f51fe8a8b816f1b21427fb035
Herbert Hodgson <herberthodgson@hotmail.com>
2018-09-07 11:50:40 -0400

    Set the STATIC_ROOT setting to the directory from which you’d like to serve the static files

--- djorg/settings.py
+++ djorg/settings.py
@@ -126,6 +126,8 @@ STATIC_URL = "/static/"
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'django-frontend/build/static')
 ]
