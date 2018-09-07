172dc24ec469739f46c1433ad2691a8c24e63ce6
maxwashington <mnwashington18@gmail.com>
2018-09-07 13:09:29 -0500

    Minor changes

--- djorg/settings.py
+++ djorg/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "somethingsecret" #config('SECRET_KEY')
+SECRET_KEY = somethingsecret #config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False #config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS =.herokuapp.com
+ALLOWED_HOSTS = []
 
 
 # Application definition
