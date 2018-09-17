7c6d8096e2a10408f77076ea57fcb501528915b0
cmok4290 <cmok4290@gmail.com>
2018-09-17 16:27:30 -0400

    added nfl app

--- djorg/settings.py
+++ djorg/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'nfl',
     'notes',
     'django.contrib.admin',
     'django.contrib.auth',
---
+++ nfl/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class NflConfig(AppConfig):
+    name = 'nfl'
