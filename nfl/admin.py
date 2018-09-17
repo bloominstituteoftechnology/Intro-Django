fc32d23f8f006ae6d0a0e631cc8ac5950255e829
cmok4290 <cmok4290@gmail.com>
2018-09-17 17:33:47 -0400

    configured admin interface and registered apps

--- nfl/admin.py
+++ nfl/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Team, Game
+
 # Register your models here.
+admin.site.register(Team)
+admin.site.register(Game)
--- nfl/models.py
+++ nfl/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from uuid import uuid4
 
-class Teams(models.Model):
+class Team(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=200)
 
-class Games(models.Model):
+class Game(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     team_1_id = models.UUIDField(primary_key=False, default=uuid4, editable=True)
     team_2_id = models.UUIDField(primary_key=False, default=uuid4, editable=True) 
