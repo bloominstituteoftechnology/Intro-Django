eec4966b10bd5544cb96727e292445d642fba60b
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-09-17 15:45:01 -0500

    ./manage.py migrate

--- djorg/urls.py
+++ djorg/urls.py
@@ -1,4 +1,4 @@
-"""intro_django URL Configuration
+"""djorg URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
--- djorg/wsgi.py
+++ djorg/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for intro_django project.
+WSGI config for djorg project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'intro_django.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djorg.settings')
 
 application = get_wsgi_application()
