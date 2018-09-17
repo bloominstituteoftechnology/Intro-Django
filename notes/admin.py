19311234a74b1aecc6667247a075626251f98390
TangledTessellations <mcevoya@oregonstate.edu>
2018-09-17 15:24:54 -0700

    Add additional fields to Note and Band

--- notes/admin.py
+++ notes/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 from .models import Note, Band
 
+
+class NoteAdmin(admin.ModelAdmin):
+    readonly_fields=('date_added', 'last_modified')
+
+
 # Register your models here.
-admin.site.register(Note)
+admin.site.register(Note, NoteAdmin)
 admin.site.register(Band)
--- notes/models.py
+++ notes/models.py
@@ -3,11 +3,43 @@ from uuid import uuid4
 # Create your models here.
 
 class Note(models.Model):
+    NOTE_TYPE = (
+        ('TD', 'To-Do'),
+        ('WS', 'Words of Wisdom'),
+        ('NS', 'Nonsense')
+    )
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    note_tag = models.CharField(
+        max_length=2,
+        choices=NOTE_TYPE,
+        default='NS'
+    )
+    
 
 class Band(models.Model):
+    # For CharField type_of_music
+    ROCK = 'RO'
+    JAZZ = 'JA'
+    FUNK = 'FU'
+    COUNTRY = 'CO'
+    MUSIC_TYPE = (
+        (ROCK, 'Rock'),
+        (JAZZ, 'Jazz'),
+        (FUNK, 'Funk'),
+        (COUNTRY, 'Country'),
+    )
+
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=200)
     members = models.TextField(blank=True)
+    still_touring = models.BooleanField(default=False)
+    type_of_music = models.CharField(
+        max_length=2,
+        choices=MUSIC_TYPE,
+        default=ROCK,
+    )
+    pertinent_website = models.URLField(default="https://www.rockarchive.com/artists")
