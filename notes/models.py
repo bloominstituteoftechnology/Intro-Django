995412d78ee24429573c15d97d9255e7e1b6492c
MacBook <LonnieDavis@MacBookPros-MacBook-Pro.local>
2018-09-17 17:16:44 -0500

    day2.md complete
    and additional models added

--- notes/models.py
+++ notes/models.py
@@ -1,7 +1,42 @@
 from django.db import models
 from uuid import uuid4
+
+from django.contrib.auth.models import User
+
 # Create your models here.
+
 class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)#cant have a default
+    last_modified = models.DateTimeField(auto_now=True)
+
+class PersonalNote(Note):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Topic(models.Model):
+    top_name = models.CharField(max_length=264,unique=True)
+
+    def __str__(self):
+        return self.top_name
+
+class Webpage(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    name = models.CharField(max_length=264,unique=True)
+    url = models.URLField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+class AccessRecord(models.Model):   
+    name = models.ForeignKey(Webpage, on_delete=models.CASCADE)
+    date = models.DateField()
+
+    def __str__(self):
+        return str(self.date)
+
+
+
