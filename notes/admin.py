1fe07c829fe16a17aa77272f6efdab318933fe22
Erin Meredith <ekmeredith@att.net>
2018-09-17 16:54:20 -0400

    completed migrations II

--- notes/admin.py
+++ notes/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import Note, Movie
 
+class NoteAdmin(admin.ModelAdmin):
+    readonly_fields= ('created_at', 'last_modified')
+
+class MovieAdmin(admin.ModelAdmin):
+    readonly_fields= ['created_at']    
 
 # Register your models here.
-admin.site.register(Note)
-admin.site.register(Movie)
+admin.site.register(Note, NoteAdmin)
+admin.site.register(Movie, MovieAdmin)
--- notes/models.py
+++ notes/models.py
@@ -7,8 +7,14 @@ class Note(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
 
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
 
 class Movie(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=200)
     year = models.CharField(max_length=4)
+    on_dvd = models.BooleanField(default=True)
+    imdb_link = models.URLField(default='http://www.imdb.com')
+
+    created_at = models.DateTimeField(auto_now_add=True)
