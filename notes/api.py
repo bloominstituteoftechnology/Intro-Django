70a16dd39630a7c8799cba4d450381223b463ec4
DirupT <iamcooled@gmail.com>
2018-09-17 17:40:31 -0500

    added user field to be able to add personalnotes from the api

--- notes/api.py
+++ notes/api.py
@@ -7,6 +7,11 @@ class PersonalNoteSerializer(serializers.HyperlinkedModelSerializer):
         model = PersonalNote
         fields = ('title', 'content')
 
+    def create(self, validated_data):
+        user = self.context['request'].user
+        note = PersonalNote.objects.create(user=user, **validated_data)
+        return note
+
 
 class PersonalNoteViewSet(viewsets.ModelViewSet):
     serializer_class = PersonalNoteSerializer
