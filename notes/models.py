57e432ec9d38e299fe3f4f2c7f6757b606fdfea7
Simon <Simon_Freelance@hotmail.com>
2018-09-17 18:05:47 -0400

    Made a User model

--- notes/models.py
+++ notes/models.py
@@ -2,6 +2,17 @@ from django.db import models
 from uuid import uuid4
 # Create your models here.
 
+
+class User(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable = False)
+    username = models.CharField(max_length = 50)
+    password = models.CharField(max_length = 100)
+    permission = (
+        ('admin', 'admin'),
+        ('paid_user', 'customer'),
+        ('free_user', 'prospect')
+    )
+
 class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable = False)
     title = models.CharField(max_length = 200)
