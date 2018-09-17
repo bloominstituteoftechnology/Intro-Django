54ece92f50d70dc6a857843a2c11de7ece9acd76
markdavidwilliams <markwilliams.aus@gmail.com>
2018-09-17 15:39:13 -0500

    project set up created models for notes and movies created and ran migrations for notes and movies used interactive shell to insert into notes and movies tables

--- djorg/settings.py
+++ djorg/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'notes',
+    'movies',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
