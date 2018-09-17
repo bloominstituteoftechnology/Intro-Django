85e1b2b93acc4e3db8abb98722783d7e23c1e361
cmok4290 <cmok4290@gmail.com>
2018-09-17 18:30:40 -0400

    added new fields with migrations to nfl app
    - created at and last modified

--- nfl/admin.py
+++ nfl/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 
 from .models import Team, Game
 
+class TeamAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'last_modified')
+
+class GameAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'last_modified') 
+
 # Register your models here.
-admin.site.register(Team)
-admin.site.register(Game)
+admin.site.register(Team, TeamAdmin)
+admin.site.register(Game, GameAdmin)
