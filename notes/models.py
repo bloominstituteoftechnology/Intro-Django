34bf27eb3d2c6a3a1f29c1437aedab4178f6ee83
Alec <afroenygam@gmail.com>
2018-09-17 18:25:32 -0400

    project changes up to subclass and specialize a model

--- notes/models.py
+++ notes/models.py
@@ -6,3 +6,11 @@ class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+class Tag(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    content = models.CharField(max_length=50)
+    note_id = models.IntegerField(editable=True, null=True, blank=True)
