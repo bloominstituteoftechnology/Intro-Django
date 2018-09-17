17784e43229b11f5ee0f97212b0e60c4a65489b3
Erin Meredith <ekmeredith@att.net>
2018-09-17 16:25:24 -0400

    completed admin interface

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Note, Movie
+
 
 # Register your models here.
+admin.site.register(Note)
+admin.site.register(Movie)
--- notes/models.py
+++ notes/models.py
@@ -6,3 +6,9 @@ class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
+
+
+class Movie(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    title = models.CharField(max_length=200)
+    year = models.CharField(max_length=4)
