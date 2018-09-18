4b92a5e00c35a7a4aff2af198bf90b26a1972595
Vu Cao <cdhvu@yahoo.com>
2018-09-17 17:36:54 -0700

    Add created_at and last_modifed fields for both Note and Tag

--- notes/admin.py
+++ notes/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Note, Tag
 
 # Register your models here.
-admin.site.register([Note, Tag])
+class NoteAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'last_modified',)
+
+admin.site.register(Note, NoteAdmin)
+admin.site.register(Tag)
--- notes/models.py
+++ notes/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from uuid import uuid4
+from django.utils import timezone
 
 # Create your models here.
 class Tag(models.Model):
     id = models.UUIDField(primary_key=True, default = uuid4, editable=False)
     tag_name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(default=timezone.now)
+    last_modified = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.tag_name
@@ -13,6 +16,8 @@ class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
     tag = models.ForeignKey(Tag, default=None, blank=True, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
