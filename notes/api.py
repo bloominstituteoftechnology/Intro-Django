1f333e3141839a72031cc90da9ec5fb25d65192d
Vu Cao <cdhvu@yahoo.com>
2018-09-18 15:45:11 -0700

    Create API for Tag

--- notes/api.py
+++ notes/api.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers, viewsets
-from .models import PersonalNote
+from .models import PersonalNote, Tag
 
 class PersonalNoteSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -9,3 +9,12 @@ class PersonalNoteSerializer(serializers.HyperlinkedModelSerializer):
 class PersonalNoteViewSet(viewsets.ModelViewSet):
     serializer_class = PersonalNoteSerializer
     queryset = PersonalNote.objects.all()
+
+class TagSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ('tag_name',)
+
+class TagViewSet(viewsets.ModelViewSet):
+    serializer_class = TagSerializer
+    queryset = Tag.objects.all()
