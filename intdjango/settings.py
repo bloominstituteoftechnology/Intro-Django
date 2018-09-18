b1a2cc36a353ad6e6ca7aa13123c5d915d0bdfc8
Eric B <timberwolf.eric@gmail.com>
2018-09-18 14:48:57 -0500

    Added DEBUG back into settings.py

--- intdjango/settings.py
+++ intdjango/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
