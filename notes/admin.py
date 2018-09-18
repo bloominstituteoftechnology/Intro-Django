287c269f6a1bdd703225185dfc585c24f92965be
Tyson Gushiken <tysonism@gmail.com>
2018-09-18 14:40:38 -0700

    Add new fields, migrate and connect to admin

--- notes/admin.py
+++ notes/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Note
+from .models import Category
 
-admin.site.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+	readonly_fields=('created_at', 'last_modified')
+
+class CategoryAdmin(admin.ModelAdmin):
+	readonly_fields=('created_at', 'last_modified')
+
+admin.site.register(Note, NoteAdmin)
+admin.site.register(Category, CategoryAdmin)
--- notes/models.py
+++ notes/models.py
@@ -6,10 +6,10 @@ class Note(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    last_modified = models.DateTimeField(auto_now=True)
 
 class Category(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    last_modified = models.DateTimeField(auto_now=True)
