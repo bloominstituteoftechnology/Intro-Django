df0fedab5c4064c4458be0803bea899838ce3b48
cmok4290 <cmok4290@gmail.com>
2018-09-18 17:57:27 -0400

    set up CORS

--- djorg/settings.py
+++ djorg/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'nfl',
     'notes',
     'rest_framework',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,3 +131,5 @@ REST_FRAMEWORK = {
          'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly', 
      ]
 }
+
+CORS_ORIGIN_ALLOW_ALL = True
