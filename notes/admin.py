a978eb26b923dc836b2350b2d8e00af29112d08d
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-09-17 20:48:27 -0700

    subclass added

--- notes/admin.py
+++ notes/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Note
+from .models import Note, PersonalNote
 
 class NoteAdmin(admin.ModelAdmin):
 	readonly_fields=('created_at', 'last_modified')
@@ -7,3 +7,4 @@ class NoteAdmin(admin.ModelAdmin):
 # Register your models here.
 
 admin.site.register(Note, NoteAdmin)
+admin.site.register(PersonalNote)
--- notes/models.py
+++ notes/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-
 from uuid import uuid4
+from django.contrib.auth.models import User
+
 
 class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
@@ -9,3 +10,7 @@ class Note(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
+
+class PersonalNote(Note):   # Inherits from Note
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
