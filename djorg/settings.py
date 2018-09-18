375b4b5ca99186526a1e2b1682ed12621a792e82
cmok4290 <cmok4290@gmail.com>
2018-09-18 15:36:11 -0400

    setup REST framework
    - installed djangorestframework
    - exposed personal note model
    - added api/notes route
    - tested api

--- djorg/settings.py
+++ djorg/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'nfl',
     'notes',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,3 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly', 
+     ]
+}
