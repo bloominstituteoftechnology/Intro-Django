0f3b2ce0d2d6237263060edcb0d812fc86e473b8
markdavidwilliams <markwilliams.aus@gmail.com>
2018-09-18 16:44:19 -0500

    users notes and movies routes now display only the posts related to logged in user

--- movies/api.py
+++ movies/api.py
@@ -7,6 +7,18 @@ class UserMovieSerializer(serializers.HyperlinkedModelSerializer):
       model = UserMovie
       fields = ('title', 'genre')
 
+    def create(self, validated_data):
+      user = self.context['request'].user
+      movie = UserMovie.objects.create(user=user, **validated_data)
+      return movie
+
 class UserMovieViewSet(viewsets.ModelViewSet):
   serializer_class = UserMovieSerializer
-  queryset = UserMovie.objects.all()
+  queryset = UserMovie.objects.none()
+
+  def get_queryset(self):
+    user = self.request.user
+    if user.is_anonymous:
+      return UserMovie.objects.none()
+    else:
+      return UserMovie.objects.filter(user=user)
