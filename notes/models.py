c23a047af4fde48261ad8ebe55facacc4c8cf975
Jameson Brown <jameson@Jamesons-MacBook-Pro.local>
2018-09-17 22:28:06 -0600

    Created PersonalNote class

--- notes/models.py
+++ notes/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from uuid import uuid4
 
+from django.contrib.auth.models import User
+
 class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=200)
@@ -9,8 +11,5 @@ class Note(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
 
-class State(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    name = models.CharField(max_length=20)
-    capital = models.CharField(max_length=20)
-    population = models.IntegerField()
+class PersonalNote(Note):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
