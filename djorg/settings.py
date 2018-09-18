86e5b433b6152178b9f9d6a0aa61578d112980f0
Nicky Chen <nicky.chen0@gmail.com>
2018-09-18 16:04:11 -0700

    add tokens

--- djorg/settings.py
+++ djorg/settings.py
@@ -128,5 +128,9 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
     ]
 }
