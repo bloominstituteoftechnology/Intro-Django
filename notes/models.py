ae77b1471147de7e3a1131c9317a25f8da0cb812
Jameson Brown <jameson@Jamesons-MacBook-Pro.local>
2018-09-18 10:57:11 -0600

    Added expiration field to Note class

--- notes/models.py
+++ notes/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from uuid import uuid4
+from django.utils import timezone
+import datetime
 
 from django.contrib.auth.models import User
 
@@ -7,9 +9,9 @@ class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
-
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
+    expiration = models.DateTimeField(default=timezone.now() + datetime.timedelta(hours=36))
 
 class PersonalNote(Note):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
