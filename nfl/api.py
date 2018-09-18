1e74740c6d6a9c09e274fdad802579d0367a741e
cmok4290 <cmok4290@gmail.com>
2018-09-18 18:08:02 -0400

    configured endpoint for a logged in user

--- nfl/api.py
+++ nfl/api.py
@@ -7,6 +7,19 @@ class TeamSerializer(serializers.HyperlinkedModelSerializer):
         model = Team
         fields = ('name',)
 
+    def create(self, validated_data):
+        # import pdb; pdb.set_trace()
+        # user = self.context['request'].user
+        team = Team.objects.create(**validated_data)
+        return team
+
 class TeamViewSet(viewsets.ModelViewSet):
     serializer_class = TeamSerializer
-    queryset = Team.objects.all()
+    queryset = Team.objects.none()
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_anonymous:
+            return Team.objects.none()
+        else:
+            return Team.objects.all()
