0c0b33d22e2ce62d0bcfc3651dcaa43547dba1f2
CLN <cln@Sags-MacBook-Pro.local>
2018-09-18 18:40:45 -0400

    Added personal note through subclass.

--- notes/admin.py
+++ notes/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 
-from .models import Note
+from .models import Note, PersonalNote
 
 class NoteAdmin(admin.ModelAdmin):
 	readonly_fields=('created_at', 'last_modified')
 
 
-admin.site.register(Note, NoteAdmin)
+admin.site.register(Note, NoteAdmin )
+admin.site.register(PersonalNote)
 
 
 
