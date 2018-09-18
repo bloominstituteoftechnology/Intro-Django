014045d7ef3e781fc7139b296d5cbb2863dab6b4
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2018-09-18 14:26:11 -0400

    Day2: Added and migrated new model

--- Intro_Django/notes/admin.py
+++ Intro_Django/notes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Note
+from .models import Person
+
+class NoteAdmin(admin.ModelAdmin):
+  readonly_fields=('created_at', 'last_modified')
 # Register your models here.
+admin.site.register(Note, NoteAdmin)
+admin.site.register(Person)
--- Intro_Django/notes/models.py
+++ Intro_Django/notes/models.py
@@ -6,3 +6,20 @@ class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+class Person(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    OCCUPATION_CHOICES = (
+        ('CS', 'Computer Scientist'),
+        ('FS', 'Full Stack Web Developer'),
+        ('DS', 'Data Scientist'),
+        ('RE', 'React Engineer'),
+    )
+    occupation = models.TextField(max_length=1, choices=OCCUPATION_CHOICES)
+
+# class Language(models.Model):
+#     language = models.
