e0f3180a166b1d09995b9b149ed7287314d2e213
snowcoding <vimalshah77@gmail.com>
2018-09-18 14:43:07 -0700

    Added REST Framework

--- vshah/settings.py
+++ vshah/settings.py
@@ -41,6 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ]
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
