71bc96a1ff6a2402741d5c6a16d3081fe9cf10b1
Blake Fletcher <blakefletcher@Blakes-MacBook-Air.local>
2018-09-18 11:10:35 -0400

    Added tag models
    Set up and used Django admin interface
    Migrated models with new fields added
    Subclassed and specialized a personal note model

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Note, Tag, PersonalNote
+
+class NoteAdmin(admin.ModelAdmin):
+	readonly_fields=('created_at', 'last_modified')
+
+class TagAdmin(admin.ModelAdmin):
+	readonly_fields=('created_at', 'last_modified')
+    
 
 # Register your models here.
+
+admin.site.register(Note, NoteAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(PersonalNote)
--- notes/models.py
+++ notes/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from uuid import uuid4 
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -7,3 +8,16 @@ class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False) 
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    tag = models.CharField(max_length=20)
+    tags = models.ManyToManyField('Tag', related_name='notes')
+
+class PersonalNote(Note):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Tag(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False) 
+    tag = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
