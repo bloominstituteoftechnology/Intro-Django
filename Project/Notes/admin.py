06669ddcfb00c95fb0629d9141f4ae36e9a3ad12
nickolaussmith <nickolaussmith721@gmail.com>
2018-09-18 09:20:22 -0600

    Completed day 1 and day 2, personal note working, no issues present

---
+++ Project/Notes/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Note
+from .models import PersonalNote
+
+
+
+class NoteAdmin(admin.ModelAdmin):
+	readonly_fields=('created_at', 'last_modified')
+
+
+admin.site.register(Note, NoteAdmin)
+admin.site.register(PersonalNote)
+# Register your models here.
---
+++ Project/Notes/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from uuid import uuid4
+from django.contrib.auth.models import User
+
+id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+
+
+class Note(models.Model):
+    
+    title = models.CharField(max_length=200)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    
+
+class PersonalNote(Note):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+# Create your models here.
