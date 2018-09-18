50b82e7323ec4dff4366144686b7e2c14c096d22
Jameson Brown <jameson@Jamesons-MacBook-Pro.local>
2018-09-18 13:32:13 -0600

    added fix for expiration field

--- notes/models.py
+++ notes/models.py
@@ -5,13 +5,16 @@ import datetime
 
 from django.contrib.auth.models import User
 
+def get_exp():
+    return timezone.now() + datetime.timedelta(hours=36)
+
 class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
-    expiration = models.DateTimeField(default=timezone.now() + datetime.timedelta(hours=36))
+    expiration = models.DateTimeField(default=get_exp)
 
 class PersonalNote(Note):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
