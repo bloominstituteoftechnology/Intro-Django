e260460c8bdd4b7a8a887cbb9b15178054ac41cf
Katie Gorbell <katiegorbell@gmail.com>
2018-09-18 14:46:37 -0500

    added created_at and last_modified fields to Notes model

--- notes/admin.py
+++ notes/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import Note
 
+class NoteAdmin(admin.ModelAdmin):       #This class allows us to see the readonly fields we created in out Notes model
+    readonly_fields=('created_at', 'last_modified')
 
 # Register your models here.
 
-admin.site.register(Note)
+admin.site.register(Note, NoteAdmin)
 
