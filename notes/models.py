8811a935362866feb4c1795d678218881ddf9a5e
emaquino44 <prexy44@yahoo.com>
2018-09-17 22:24:09 -0700

    added user model and personalnotes model - able to make Personal Notes/user

--- notes/models.py
+++ notes/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from uuid import uuid4
 
-
+from django.contrib.auth.models import User
 
 class Note(models.Model):
 		id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
@@ -10,3 +10,6 @@ class Note(models.Model):
 
 		created_at = models.DateTimeField(auto_now_add=True)
 		last_modified = models.DateTimeField(auto_now=True)
+
+class PersonalNote(Note):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
