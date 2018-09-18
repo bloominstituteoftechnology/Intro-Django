d3eaad1a250801a686cc09b473ab7e7f1ad0d17c
cmok4290 <cmok4290@gmail.com>
2018-09-18 17:25:32 -0400

    added filter to personal note query

--- notes/api.py
+++ notes/api.py
@@ -15,4 +15,11 @@ class PersonalNoteSerializer(serializers.HyperlinkedModelSerializer):
 
 class PersonalNoteViewSet(viewsets.ModelViewSet):
     serializer_class = PersonalNoteSerializer
-    queryset = PersonalNote.objects.all()
+    queryset = PersonalNote.objects.none()
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_anonymous:
+            return PersonalNote.objects.none()
+        else:
+            return PersonalNote.objects.filter(user=user)
