deff77d3ad9b915fdf9b6736434946889c52decc
Nicky Chen <nicky.chen0@gmail.com>
2018-09-18 15:44:04 -0700

    add cors

--- djorg/settings.py
+++ djorg/settings.py
@@ -35,6 +35,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'notes',
     'rest_framework',
+    'rest_framework.authtoken',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
