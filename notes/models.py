16bd7c320273e5b70647790ac8a52396f280880e
-tristan- <tristan.linehan@gmail.com>
2018-09-17 18:10:23 -0700

    day1 complete

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+from uuid import uuid4
+
+class Note(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    title = models.CharField(max_length=200)
+    content = models.TextField(blank=True)
