718fce7e4d9a03ffd50d2f49aa01526649c2e6fa
Vu Cao <cdhvu@yahoo.com>
2018-09-18 17:08:33 -0700

    Create filter for user

--- notes/api.py
+++ notes/api.py
@@ -6,9 +6,23 @@ class PersonalNoteSerializer(serializers.HyperlinkedModelSerializer):
         model = PersonalNote
         fields = ('title', 'content')
 
+    def create(self, validated_data):
+        # import pdb; pdb.set_trace()
+        user = self.context['request'].user
+        note = PersonalNote.objects.create(user=user,**validated_data)
+        return note
+
 class PersonalNoteViewSet(viewsets.ModelViewSet):
     serializer_class = PersonalNoteSerializer
     queryset = PersonalNote.objects.all()
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.is_anonymous:
+            return PersonalNote.objects.none()
+        else:
+            return PersonalNote.objects.filter(user=user)
 
 class TagSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
