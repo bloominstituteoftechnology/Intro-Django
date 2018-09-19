426c1a1995f8500c353cf3e2f82f1d0ae233bced
-tristan- <tristan.linehan@gmail.com>
2018-09-19 16:59:49 -0700

    completed day4

--- djorg/settings.py
+++ djorg/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
      ### NOTES & REST ADDED
     'notes',
     'rest_framework',
+    'rest_framework.authtoken',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -132,5 +133,8 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ],
+        'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
     ]
 }
--- djorg/urls.py
+++ djorg/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include 
+from django.urls import path, include, re_path 
  # ADDING include ABOVE, TO ADD /API/ TO URL
  ###
 from rest_framework import routers
 from notes.api import PersonalNoteViewSet
+
+from rest_framework.authtoken import views
 
  # BELOW CONNECTs TO ROUTER, creates new router
 router = routers.DefaultRouter()
@@ -31,5 +33,6 @@ router.register('notes', PersonalNoteViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
      ###
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token)
 ]
