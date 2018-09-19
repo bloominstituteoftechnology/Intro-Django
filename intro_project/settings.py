c64e1481a288683cb019bb7451e0e1c3d477be3e
Austin Merando <ajmerando1@yahoo.com>
2018-09-19 15:27:18 -0700

    day 3 complete

--- intro_project/settings.py
+++ intro_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'notes',
     'rest_framework',
+    'corsheaders',
 ]
 
 REST_FRAMEWORK = {
@@ -48,6 +49,8 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,6 +124,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
--- notes/api.py
+++ notes/api.py
@@ -2,11 +2,23 @@ from rest_framework import serializers, viewsets
 from .models import PersonalNote
 
 class PersonalNoteSerializer(serializers.HyperlinkedModelSerializer):
+    def create(self, validated_data):
+        user = self.context['request'].user
+        note = PersonalNote.objects.create(user=user, **validated_data)
+        return note
 
     class Meta:
         model = PersonalNote
         fields = ('title', 'content')
 
 class PersonalNoteViewSet(viewsets.ModelViewSet):
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.is_anonymous:
+            return PersonalNote.objects.none()
+        else:
+            return PersonalNote.objects.filter(user=user)
+            
     serializer_class = PersonalNoteSerializer
     queryset = PersonalNote.objects.all()
