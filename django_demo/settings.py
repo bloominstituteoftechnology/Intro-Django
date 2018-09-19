d0e37c2e2255ee26d4c1c241770075d40332b45d
Vu Cao <cdhvu@yahoo.com>
2018-09-19 15:48:02 -0700

    Test SessionAuthentication

--- django_demo/settings.py
+++ django_demo/settings.py
@@ -130,6 +130,8 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',  
     )
 }
 
