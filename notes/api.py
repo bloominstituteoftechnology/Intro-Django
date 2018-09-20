c8a78b706bec0152ff93486a80f18aee603136cb
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-09-20 14:00:02 -0500

    Expose the PersonalNotes Model

---
+++ notes/api.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers, viewsets
+from .models import PersonalNote
+
+
+class PersonalNoteSerializer(serializers.HyperlinkedModelSerializer):
+    # Inner class nested inside PersonalNoteSerializer
+    class Meta:
+        model = PersonalNote
+        fields = ('title', 'content')
+
+
+class PersonalNoteViewSet(viewsets.ModelViewSet):
+    serializer_class = PersonalNoteSerializer
+    queryset = PersonalNote.objects.all()
