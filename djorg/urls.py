8d3ff7f885dbd52a775e17e14a9e866a7d91350c
Alec <afroenygam@gmail.com>
2018-09-20 17:37:43 -0400

    GraphQL querying for personal notes updated

--- djorg/urls.py
+++ djorg/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include, re_path
 from rest_framework import routers
 from rest_framework.authtoken import views
 from notes.api import PersonalNoteViewSet
+from graphene_django.views import GraphQLView
+from django.views.decorators.csrf import csrf_exempt
 
 router = routers.DefaultRouter()
 router.register('notes', PersonalNoteViewSet)
@@ -25,5 +27,6 @@ router.register('notes', PersonalNoteViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    re_path(r'api-token-auth/', views.obtain_auth_token)
+    re_path(r'api-token-auth/', views.obtain_auth_token),
+    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
