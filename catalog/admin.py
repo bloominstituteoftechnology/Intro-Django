9254f85490e70fb0bbf8478c7f739829e0323e1f
cmok4290 <cmok4290@gmail.com>
2018-09-19 20:03:54 -0400

    registered models and fixed migrations

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from catalog.models import Author, Genre, Book, BookInstance, Language
 
-# Register your models here.
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(BookInstance)
+admin.site.register(Language)
+
