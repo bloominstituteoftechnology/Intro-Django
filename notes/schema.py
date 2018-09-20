baf0498239b759884ded28e47ca277c8ae1ef7a8
cmok4290 <cmok4290@gmail.com>
2018-09-20 17:56:48 -0400

    added graphql mutation

--- notes/schema.py
+++ notes/schema.py
@@ -24,5 +24,28 @@ class Query(graphene.ObjectType):
         else:
             return PersonalNote.objects.filter(user=user)
 
+class CreatePersonalNote(graphene.Mutation):
+    """Describes a model for creating a note."""
+    class Arguments:
+        title = graphene.String()
+        content = graphene.String()
+
+    personalnote = graphene.Field(PersonalNote)
+    ok = graphene.Boolean()
+    status = graphene.String()
+
+    def mutate(self, info, title, content):
+        user = info.context.user
+
+        if user.is_anonymous:
+            return CreatePersonalNote(ok=False, status='Must be logged in!')
+        else:
+            new_note = PersonalNoteModel(title=title, content=content, user=user)
+            new_note.save()
+            return CreatePersonalnote(personalnote=new_note, ok=True, status='ok')
+
+class Mutation(graphene.ObjectType):
+    create_personal_note = CreatePersonalNote.Field()
+
 # Add a schema and attach to the query
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation=Mutation)
