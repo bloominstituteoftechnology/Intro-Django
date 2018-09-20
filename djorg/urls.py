6f80bff9002adaf6821ff7fd34b04ad89f23833d
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-09-20 16:06:33 -0700

    about route added via templateView

--- djorg/urls.py
+++ djorg/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.views.generic import TemplateView
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
@@ -26,6 +27,7 @@ router.register(r'notes', PersonalNoteViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('api-token-auth/', views.obtain_auth_token,)
-    path('', )
+    path('api-token-auth/', views.obtain_auth_token),
+    path('about/', TemplateView.as_view(template_name="about.html"))
+
 ]
