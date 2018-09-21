0d9819cdbc56c0e6ba77c9f9e693344fbcd759b7
markdavidwilliams <markwilliams.aus@gmail.com>
2018-09-21 13:14:20 -0500

    error server now runs, some typo in my whitenoise middleware

--- djorg/settings.py
+++ djorg/settings.py
@@ -46,9 +46,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleWare',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
