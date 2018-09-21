a9608d31cf261432f86772b5d12b7f1113b036e7
DirupT <iamcooled@gmail.com>
2018-09-21 11:33:42 -0500

    added static_root

--- django_project/settings.py
+++ django_project/settings.py
@@ -136,4 +136,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
