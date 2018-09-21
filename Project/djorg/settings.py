ac60ce4a25092920da71621be3560b5a72a388a3
nickolaussmith <nickolaussmith721@gmail.com>
2018-09-21 12:46:21 -0600

    a

--- Project/djorg/settings.py
+++ Project/djorg/settings.py
@@ -119,7 +119,7 @@ WSGI_APPLICATION = 'djorg.wsgi.application'
 #     }
 # }
 DATABASES = {}
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES['default'] = dj_database_url.config(default='sqlite:///PATH',conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
