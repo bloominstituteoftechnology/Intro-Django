7737a46e3a0957d35f3153c29d06ff8fc9345f0a
Gabriel Duquette <gabrielduquette@gmail.com>
2018-09-20 23:37:40 -0400

    add staticfiles folder

--- djorg/settings.py
+++ djorg/settings.py
@@ -81,12 +81,12 @@ WSGI_APPLICATION = 'djorg.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 DATABASES = {
     'default': dj_database_url.config("sqlite:///db.sqlite3")
