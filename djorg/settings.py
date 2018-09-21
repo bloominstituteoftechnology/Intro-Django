568d22ae03c61de612bf05f015e08ee58ef194c0
TangledTessellations <mcevoya@oregonstate.edu>
2018-09-21 09:01:06 -0700

    Revert last changes

--- djorg/settings.py
+++ djorg/settings.py
@@ -12,9 +12,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
-import dj-database-url
+import dj_database_url
 from decouple import config
-
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
