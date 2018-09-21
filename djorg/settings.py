09b136caf004b81b8a14b040e609fe7f7bb28311
Gabriel Duquette <gabrielduquette@gmail.com>
2018-09-20 22:49:25 -0400

    change database config

--- djorg/settings.py
+++ djorg/settings.py
@@ -83,14 +83,11 @@ WSGI_APPLICATION = 'djorg.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': dj_database_url.config("sqlite:///db.sqlite3")
+}
 
-DATABASES['default'] = dj_database_url.config("sqlite:///db.sqlite3")
+# DATABASES['default'] = dj_database_url.config("sqlite:///db.sqlite3")
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
