3ec8a9db1a08461c856c21f865baeb9b117d07da
Vu Cao <cdhvu@yahoo.com>
2018-09-21 11:02:00 -0700

    Adjust setting for personal note api

--- django_demo/settings.py
+++ django_demo/settings.py
@@ -139,8 +139,8 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-        # 'rest_framework.authentication.BasicAuthentication',
-        # 'rest_framework.authentication.SessionAuthentication',  
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',  
     )
 }
 
