29328c32227f213e747190c337b9715240bfc564
JJ Ashcraft <jj@jjashcraft.com>
2018-09-21 12:02:15 -0400

    testing static folders/favicon

--- josh/settings.py
+++ josh/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'josh/static/'
 
 
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
