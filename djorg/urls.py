19a26cc9913d47b15616a4fcbbde62060a8b152d
M.Real <mreal126@protonmail.com>
2018-09-21 11:57:45 -0400

    updated requirements.txt, not sure if it's the issue with deploying

--- djorg/urls.py
+++ djorg/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path,include,re_path
+from django.urls import path, include, re_path
 
 from rest_framework import routers
 from notes.api import PersonalNoteViewSet
