309a91ec560d88da50e289f249047421acb8b9c0
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-09-21 08:52:53 -0700

    heroku configs done

--- djorg/settings.py
+++ djorg/settings.py
@@ -57,6 +57,8 @@ MIDDLEWARE = [
 
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ROOT_URLCONF = 'djorg.urls'
