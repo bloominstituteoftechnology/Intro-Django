232e1fbbcacf8308c4ae26d418766e90a9f6babd
JJ Ashcraft <jj@jjashcraft.com>
2018-09-21 12:06:15 -0400

    updated cors settings

--- josh/settings.py
+++ josh/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 
 
 INSTALLED_APPS = [
+    'django-cors-headers',
     'notes',
     'rest_framework',
     'rest_framework.authtoken',
@@ -44,7 +45,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cors-headers',
 ]
 
 MIDDLEWARE = [
