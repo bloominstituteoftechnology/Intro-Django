636da03f644a5c66c1c02446e0ae002f7599e710
Austin Merando <ajmerando1@yahoo.com>
2018-09-21 09:59:30 -0700

    settings allowed hosts fix

--- intro_project/settings.py
+++ intro_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
-ALLOWED_HOSTS = [".herokuapp.com"]
+ALLOWED_HOSTS = [".herokuapp.com","localhost","127.0.0.1"]
 
 
 # Application definition
