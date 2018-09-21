ba91923023c5fe7544df67bb4731da678aa7683f
M.Real <mreal126@protonmail.com>
2018-09-21 13:41:04 -0400

    Added corsheaders, trying to migrate again..

--- djorg/settings.py
+++ djorg/settings.py
@@ -47,8 +47,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
