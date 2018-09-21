8c3f9a521978efb9d64c285c58d30ad568f52bec
nickolaussmith <nickolaussmith721@gmail.com>
2018-09-21 13:18:01 -0600

    Pusher

--- djorg/settings.py
+++ djorg/settings.py
@@ -42,6 +42,8 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 CORS_ORIGIN_ALLOW_ALL = True
 
 
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -152,6 +154,13 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "static"),
+    ]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage' 
 
 
 # Static files (CSS, JavaScript, Images)
