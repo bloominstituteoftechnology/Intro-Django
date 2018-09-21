fd7da25757c2e45e564415feda12dc99411ed5c7
M.Real <mreal126@protonmail.com>
2018-09-21 13:42:52 -0400

    some whitespace errors maybe?

--- djorg/settings.py
+++ djorg/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',cast=bool)
 
-ALLOWED_HOSTS =  config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
 CORS_ORIGIN_ALLOW_ALL = True
 
