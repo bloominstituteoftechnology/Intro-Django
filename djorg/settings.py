e33a25ad07d6f0d4f0023a94be9bce0f8d93bd7a
-tristan- <tristan.linehan@gmail.com>
2018-09-21 16:27:46 -0700

    it's alive!

--- djorg/settings.py
+++ djorg/settings.py
@@ -37,8 +37,9 @@ DEBUG = config('DEBUG', cast=bool)
 
  ##### BELOW CHANGED FOR HEROKU
  # ALLOWED_HOSTS = []
- 
-ALLOWED_HOSTS = ['*']
+
+# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://first-heroku-app-by-tristan.herokuapp.com/']
  ###### BELOW ONE ADDED FOR CORS
 #CORS_ORIGIN_ALLOW_ALL = config('CORS_ORIGIN_ALLOW_ALL', cast=bool)
 
