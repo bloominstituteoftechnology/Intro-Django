4b2b97d40211ddd9483f155b97a562d76203e5d0
JacobLayton <jlayjlay3@gmail.com>
2018-09-21 12:35:32 -0700

    Added STATIC_ROOT

--- djorg/settings.py
+++ djorg/settings.py
@@ -128,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
