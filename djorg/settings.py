c52f6017f3bc577e42f1627324a19c006b34f8e4
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-09-21 10:10:08 -0700

    allowed host updated

--- djorg/settings.py
+++ djorg/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [os.environ["ALLOWED_HOSTS"]]
 
 
 # Application definition
