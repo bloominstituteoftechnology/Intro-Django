51db13aac61007f6c7d3b0b2a935e45786920883
Vu Cao <cdhvu@yahoo.com>
2018-09-21 10:58:32 -0700

    Create static folder

--- django_demo/settings.py
+++ django_demo/settings.py
@@ -126,8 +126,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
+# STATIC_FILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# STATICFILES_DIRS = [os.path.join(PROJECT_ROOT, "staticfiles")]
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
