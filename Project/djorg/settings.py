5bd795d9b271d13eb5142e3b89afccbea4883a5c
nickolaussmith <nickolaussmith721@gmail.com>
2018-09-21 12:41:58 -0600

    plz

--- Project/djorg/settings.py
+++ Project/djorg/settings.py
@@ -109,16 +109,17 @@ WSGI_APPLICATION = 'djorg.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
+# DATABASES = {
+#     'default': {
         
         
 
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-DATABASES['default'] = dj_database_url.config(default='sqlite:///PATH',conn_max_age=600)
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
