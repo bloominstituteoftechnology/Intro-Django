9ed49171a95a73df1cccaeb1b322872b27a5cc0f
M.Real <mreal126@protonmail.com>
2018-09-21 13:26:18 -0400

    test

--- djorg/settings.py
+++ djorg/settings.py
@@ -131,9 +131,9 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+    # os.path.join(BASE_DIR, 'static'),
+# )
 
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
