789a31f5340fc7ed78c690a3ba2006a34e436489
Simon <Simon_Freelance@hotmail.com>
2018-09-21 13:06:47 -0400

    Made adjustments

--- djorg/settings.py
+++ djorg/settings.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
-from decouple import config
-import dj_database_url
+from decouple import config, dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -84,7 +83,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-DATABASES['default'] = dj_database_url.config(default='sqlite:///../db.sqlite3')
+DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'))
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
