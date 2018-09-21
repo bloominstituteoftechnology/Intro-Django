b3d074fa7800558da47a8db227f56a67ebcb24af
Gabriel Duquette <gabrielduquette@gmail.com>
2018-09-21 13:58:25 -0400

    remove whitenoise

--- djorg/settings.py
+++ djorg/settings.py
@@ -49,7 +49,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
