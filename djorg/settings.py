5b938e60ecb385b0220520835463d90e607ea005
Simon <Simon_Freelance@hotmail.com>
2018-09-21 12:49:52 -0400

    Retry config

--- djorg/settings.py
+++ djorg/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-dj_database_url.config(default=f'sqlite:///{DATABASES['default']['NAME']}')
+DATABASES['default'] = dj_database_url.config(default='sqlite:///../db.sqlite3')
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
