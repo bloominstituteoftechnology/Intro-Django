e76572f6647918a19591055d8ce0f687e9748602
Alec <afroenygam@gmail.com>
2018-09-21 12:03:31 -0400

    imported missing package to djorg/settings.py

--- djorg/settings.py
+++ djorg/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-from decouple import config
+from decouple import config, Csv
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
