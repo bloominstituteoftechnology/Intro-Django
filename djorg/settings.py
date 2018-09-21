1ee633ecba03606fb419b095410be2b90159e88e
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-09-21 11:58:05 -0500

    attempt to fix Bad Request (400)

--- djorg/settings.py
+++ djorg/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = [config('ALLOWED_HOSTS', cast=str)]
+ALLOWED_HOSTS = [config('ALLOWED_HOSTS')]
 
 
 # Application definition
