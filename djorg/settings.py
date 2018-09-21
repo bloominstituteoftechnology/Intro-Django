d2734560a55c3196371cb536490e1e99080985f6
Jameson Brown <jameson@Jamesons-MacBook-Pro.local>
2018-09-21 10:47:04 -0600

    deployment settings

--- djorg/settings.py
+++ djorg/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
