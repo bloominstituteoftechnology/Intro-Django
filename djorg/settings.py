0426bc46353dce0a89957a5c0e52f50343f290a8
emaquino44 <prexy44@yahoo.com>
2018-09-21 09:24:08 -0700

    deployed but still debugging heroku issues

--- djorg/settings.py
+++ djorg/settings.py
@@ -146,6 +146,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
