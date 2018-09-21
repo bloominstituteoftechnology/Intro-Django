5f157fcdc7ae1676cedf1dec6e6460ed754c2b1d
Gabriel Duquette <gabrielduquette@gmail.com>
2018-09-20 22:47:04 -0400

    comment out config stuff

--- djorg/settings.py
+++ djorg/settings.py
@@ -23,15 +23,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+# SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
-
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1'
-]
+# DEBUG = config('DEBUG', cast=bool)
+#
+# ALLOWED_HOSTS = [
+#     'localhost',
+#     '127.0.0.1'
+# ]
 
 
 # Application definition
