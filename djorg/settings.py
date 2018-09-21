1aaa3e5667c4c5be23fe72d2360606f0e0a1bbf5
JacobLayton <jlayjlay3@gmail.com>
2018-09-21 09:49:47 -0700

    Staticfiles caused an error so I commented it out

--- djorg/settings.py
+++ djorg/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 
