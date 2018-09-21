d055adc3546a7f26e65ab83097abc73ac14ea7c3
JJ Ashcraft <jj@jjashcraft.com>
2018-09-21 12:14:13 -0400

    more debugging settings.py

--- josh/settings.py
+++ josh/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 
 
 INSTALLED_APPS = [
+    # 'django-cors-headers',
     'notes',
     'rest_framework',
     'rest_framework.authtoken',
@@ -48,7 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    # 'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
