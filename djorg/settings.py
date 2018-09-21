7f734200b83994ec9c3d8281b762484939e81755
TangledTessellations <mcevoya@oregonstate.edu>
2018-09-21 12:45:02 -0700

    m

--- djorg/settings.py
+++ djorg/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = config('DEBUG', cast=bool)
-DEBUG = False
+DEBUG = config('DEBUG')
 
 if DEBUG:
     SECRET_KEY = 'mysecretkey'
