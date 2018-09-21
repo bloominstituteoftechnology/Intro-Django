5d33d35b32682e773e591229b5f8193ba80fa0f8
JJ Ashcraft <jj@jjashcraft.com>
2018-09-21 14:05:44 -0400

    debugging again

--- josh/settings.py
+++ josh/settings.py
@@ -85,8 +85,8 @@ WSGI_APPLICATION = 'josh.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite',
-        'NAME': os.path.join(BASE_DIR, 'db.postgres'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
