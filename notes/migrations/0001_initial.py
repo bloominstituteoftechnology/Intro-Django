625e5ddfcc8b0a710af2b2a56b1de53781656748
JJ Ashcraft <jj@jjashcraft.com>
2018-09-21 16:24:19 -0400

    updated

--- notes/migrations/0001_initial.py
+++ notes/migrations/0001_initial.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Note',
             fields=[
-                ('primary_id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
             ],
         ),
     ]
