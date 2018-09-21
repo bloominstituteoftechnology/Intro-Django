f513d4f80d179b6511ab15bc432f9ab0f94fbcfe
Alec <afroenygam@gmail.com>
2018-09-21 12:10:33 -0400

    fix error in djorg/settings.py

--- djorg/settings.py
+++ djorg/settings.py
@@ -82,12 +82,12 @@ WSGI_APPLICATION = 'djorg.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 DATABASES['default'] = dj_database_url.config(default='DATABASE_URL')
 
