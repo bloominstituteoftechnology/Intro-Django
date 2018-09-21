93588b3e0ddaf07dcfde3bb55d2876de038a3006
-tristan- <tristan.linehan@gmail.com>
2018-09-21 12:40:48 -0700

    added STATIC_ROOT

--- djorg/settings.py
+++ djorg/settings.py
@@ -146,6 +146,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+ ###
+STATIC_ROOT = './djorg/static'
+
 
  ### SETTINGS FOR 
 REST_FRAMEWORK = {
