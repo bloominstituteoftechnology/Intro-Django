24fc0304cbf512205fac3bfad34e867effe76d02
DirupT <iamcooled@gmail.com>
2018-09-21 11:28:20 -0500

    removed static files giving error

--- django_project/settings.py
+++ django_project/settings.py
@@ -137,5 +137,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
