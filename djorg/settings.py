8ed2a825c6f5a6ac0d6210920e8d705e59120035
emaquino44 <prexy44@yahoo.com>
2018-09-22 01:30:42 -0700

    still working

--- djorg/settings.py
+++ djorg/settings.py
@@ -125,10 +125,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 
