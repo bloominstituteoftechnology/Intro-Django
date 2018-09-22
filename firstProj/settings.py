fac9d3f19c590a98afad7974bccd983525950058
MacBook <LonnieDavis@MacBookPros-MacBook-Pro.local>
2018-09-21 19:25:23 -0500

    change

--- firstProj/settings.py
+++ firstProj/settings.py
@@ -11,11 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
 import dj_database_url
-
 #import django_heroku
-
 from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
