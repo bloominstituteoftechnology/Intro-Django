9c90bc1e9523829a57b89f947b10f4aeb6146346
Adrian Adames <adrianadames@gmail.com>
2018-09-24 17:57:54 -0400

    Added personal note subclass and did associated migrations. Confirmed to work via the django admin interface.

--- notes/models.py
+++ notes/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from uuid import uuid4
-# Create your models here.
+
+#model that corresponds to the records in the User table
+from django.contrib.auth.models import User
+
 
 class Note(models.Model):
     id = models.UUIDField(primary_key=True, default = uuid4, editable=False)
@@ -9,3 +12,6 @@ class Note(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
+
+class PersonalNote(Note):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
