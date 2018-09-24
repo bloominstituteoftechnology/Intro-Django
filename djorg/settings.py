1c5eb736b2d229c77f768c29514d5a98ccfedd93
noxasaxon <34722463+noxasaxon@users.noreply.github.com>
2018-09-24 03:46:52 -0700

    try removing whitenoise

--- djorg/settings.py
+++ djorg/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'djorg.urls'
