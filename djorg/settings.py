c37c1d38af6d9d17047e50aa8ce8becca643a09e
noxasaxon <34722463+noxasaxon@users.noreply.github.com>
2018-09-24 04:04:23 -0700

    update to database setup, app is deployed to heroku

--- djorg/settings.py
+++ djorg/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] = dj_database_url.config(config('DATABASE_URL'), conn_max_age=600)
+DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'), conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
