688ea5cd0b06848f47b6579b4870aec78845a0e2
Daniel Chamorro <dan@dcitllc.com>
2018-09-25 19:04:05 -0400

    Trying to fix staic files

--- djorg/settings.py
+++ djorg/settings.py
@@ -18,7 +18,7 @@ from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
-STATIC_DIR = os.path.join(BASE_DIR, "static")
+# STATIC_DIR = os.path.join(BASE_DIR, "static")
 
 
 # Quick-start development settings - unsuitable for production
