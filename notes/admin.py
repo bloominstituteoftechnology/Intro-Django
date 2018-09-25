2a8c38b57391f62a61c62bd4a85d4c79162b4a30
Randy Calderon <randycweb@gmail.com>
2018-09-25 12:11:09 -0700

    Added PersonalNote class

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
 # Register your models here.
+from .models import Note
+
+class NoteAdmin(admin.ModelAdmin):
+    readonly_fields=('created_at', 'last_modified')
+
+admin.site.register(Note, NoteAdmin)
