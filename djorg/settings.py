96d5edf3978c8ed3aca93c0663d35ed999d6b2fb
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-09-26 10:20:47 -0700

    p to P

--- djorg/settings.py
+++ djorg/settings.py
@@ -136,6 +136,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 STATIC_URL = '/static/'
