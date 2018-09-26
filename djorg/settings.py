16daedf5c087d8aa9ef69886d8a48d422a8fee5c
Ari Mercado <ari@Apples-Mac-3.local>
2018-09-26 12:03:52 -0700

    added authorization endpoint

--- djorg/settings.py
+++ djorg/settings.py
@@ -35,6 +35,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'notes',
+    'rest_framework',
+    'rest_framework.authtoken',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,3 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSEŠS': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadyOnly',
+    ],
+    'DEFAULT_AUTHENTICATIONS_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
