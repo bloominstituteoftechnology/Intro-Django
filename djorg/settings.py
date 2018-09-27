9748db6393cddc67d5da5fb5778f082f3ad41c3e
Randy Calderon <randycweb@gmail.com>
2018-09-26 20:46:07 -0700

    Added routes

--- djorg/settings.py
+++ djorg/settings.py
@@ -35,8 +35,9 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'notes',
-    'rest_framework',
     'corsheaders',
+    'rest_framework',
+    'rest_framework.authtoken',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,10 +46,17 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ),
 }
 
 MIDDLEWARE = [
@@ -133,3 +141,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
