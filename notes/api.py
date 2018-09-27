bce97311319f036a1d8a6be2b5fadd926938c601
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2018-09-27 13:47:39 -0700

    implemented AuthJWT

--- notes/api.py
+++ notes/api.py
@@ -5,9 +5,22 @@ class PersonalNoteSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = PersonalNote
-        fields = '__all__'
+        fields = ("title","content")#'__all__'
+
+    def create(self, validated_data):
+        #import pdb; pdb.set_trace()
+        user = self.context['request'].user
+        note = PersonalNote.objects.create(user=user, **validated_data)
+        return note
 
 class PersonalNoteViewSet(viewsets.ModelViewSet):
-
     queryset = PersonalNote.objects.all()
     serializer_class = PersonalNoteSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.is_anonymous:
+            return PersonalNote.objects.none()
+        else:
+            return PersonalNote.objects.filter(user=user)
