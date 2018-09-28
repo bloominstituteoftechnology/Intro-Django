c371fad86d199a711f48eae54b493536aa47dabf
Randy Calderon <randycweb@gmail.com>
2018-09-27 23:30:10 -0700

    Heroku imports plus procfile

--- djorg/settings.py
+++ djorg/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -142,3 +143,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+django_heroku.settings(locals())
--- notes/api.py
+++ notes/api.py
@@ -14,11 +14,16 @@ class PersonalNoteSerializer(serializers.HyperlinkedModelSerializer):
         model = PersonalNote
         fields = ('title', 'content')
 
+        def create(self, validated_data):
+            user = self.context['request'].user
 
+            note = PersonalNote.objects.create(user=user, **validated_data)
+            
+            return note
 
 class PersonalNoteViewSet(viewsets.ModelViewSet):
     serializer_class = PersonalNoteSerializer
-    queryset = Note.objects.none()
+    queryset = PersonalNote.objects.none()
 
     def get_queryset(self):
         user = self.request.user
