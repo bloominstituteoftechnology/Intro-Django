74ba962d7e7abee97579c3a299379b3b8e7bf41e
Randy Calderon <randycweb@gmail.com>
2018-09-28 00:12:51 -0700

    Changes to import

--- djorg/settings.py
+++ djorg/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -143,4 +142,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Configure django app for heroku
+import django_heroku
 django_heroku.settings(locals())
