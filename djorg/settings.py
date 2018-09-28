2252b4efc3e4c2fc700941602053d3ebfe397843
unknown <may.alex43@gmail.com>
2018-09-28 16:27:46 -0400

    fixed quotes

--- djorg/settings.py
+++ djorg/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'djorg.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES[‘default’] = dj_database_url.config(default=config(‘DATABASE_URL’))
+DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'))
 
 
 # Password validation
