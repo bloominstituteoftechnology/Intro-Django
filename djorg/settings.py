d5e9b8ca51f070cd5bcee8cf133bfb18ccea8aaa
Adrian Adames <adrianadames@gmail.com>
2018-09-28 12:46:24 -0400

    Installed dependencies required for deployment. Finished with steps 1-6 in the README.

--- djorg/settings.py
+++ djorg/settings.py
@@ -13,6 +13,13 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 from decouple import config
+import dj_database_url
+
+MIDDLEWARE = [
+  # 'django.middleware.security.SecurityMiddleware',
+  'whitenoise.middleware.WhiteNoiseMiddleware',
+  # ...
+]
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -28,8 +35,11 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast = bool)
 
-ALLOWED_HOSTS = []
+host = os.environ.get('ALLOWED_HOSTS')
+ALLOWED_HOSTS = [host]
 
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES['default'] = dj_database_url.config('DATABASE_URL')
 
 # Application definition
 
