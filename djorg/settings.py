064ce3f0ffbc3e6a025b5eaa957caa84067cb736
Ari Mercado <ari@Apples-Mac-3.local>
2018-09-28 12:50:37 -0700

    changed setting.py file's database

--- djorg/settings.py
+++ djorg/settings.py
@@ -82,16 +82,16 @@ WSGI_APPLICATION = 'djorg.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
-#   'default': dj_database_url.config('DATABASE_URL', default='sqlite:///db.sqlite3')
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
 # }
+
+DATABASES = {
+    'default': dj_database_url.config(default = config('DATABASE_URL')),
+}
 
 
 # Password validation
