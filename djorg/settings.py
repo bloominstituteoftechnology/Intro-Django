5b19d98275583d70163570f71ec92a1bca8ae6ac
Randy Calderon <randycweb@gmail.com>
2018-09-28 11:37:05 -0700

    Changes to setting.py middleware

--- djorg/settings.py
+++ djorg/settings.py
@@ -60,9 +60,9 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleWare',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
