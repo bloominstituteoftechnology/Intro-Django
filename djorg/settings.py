ec64415c09918fb0d6ce67280ccc98d0c564cc25
unknown <may.alex43@gmail.com>
2018-09-28 17:07:48 -0400

    attempting to fix error

--- djorg/settings.py
+++ djorg/settings.py
@@ -85,8 +85,13 @@ WSGI_APPLICATION = 'djorg.wsgi.application'
 # DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'))
 
 DATABASES = {
-   'default': dj_database_url.config(default = config('DATABASE_URL')),
+   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   }
 }
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
