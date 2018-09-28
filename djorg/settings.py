c9e1da19db485d09912dbdbd194d0261130ae58d
unknown <may.alex43@gmail.com>
2018-09-28 16:24:33 -0400

    added database

--- djorg/settings.py
+++ djorg/settings.py
@@ -82,12 +82,7 @@ WSGI_APPLICATION = 'djorg.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES[‘default’] = dj_database_url.config(default=config(‘DATABASE_URL’))
 
 
 # Password validation
