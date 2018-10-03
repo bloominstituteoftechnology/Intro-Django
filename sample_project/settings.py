3e3d15be35533e129935b51c679bb8dfa5152a59
sulaiman sanusi <sulaiman.sanusi@icloud.com>
2018-10-04 00:12:39 +0100

    database indetation

--- sample_project/settings.py
+++ sample_project/settings.py
@@ -85,9 +85,9 @@ WSGI_APPLICATION = 'sample_project.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
- DATABASES = {
+DATABASES = {
     'default': dj_database_url.config(default = config('DATABASE_URL')),
-} 
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
