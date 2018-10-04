0d78359e88b3c9ccf1846d0a50bebdd840fe2e26
sulaiman sanusi <sulaiman.sanusi@icloud.com>
2018-10-04 01:08:21 +0100

    removed static files

--- sample_project/settings.py
+++ sample_project/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
