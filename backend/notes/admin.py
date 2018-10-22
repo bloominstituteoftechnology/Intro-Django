6a65ef5feaadee307391d94a67fdabe5f7662ac6
ckopecky <ckopecky@github.com>
2018-10-22 10:25:55 -0700

    starting over

--- backend/notes/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Note, PersonalNote
-# Register your models here.
-
-class NoteAdmin(admin.ModelAdmin):
-	readonly_fields=('created_on', 'last_modified')
-
-admin.site.register((Note,PersonalNote), NoteAdmin)
