4a56386645dd4f97614814d70279ed8b5cea34c4
francistse23 <wtse.ucsd@gmail.com>
2018-10-22 14:07:37 -0700

    Added two new fields, created_at and last_modified. Page now displays both read only information.

--- notes/admin.py
+++ notes/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Note
 # Register your models here.
 
-admin.site.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    readonly_fields=('created_at','last_modified')
+
+
+admin.site.register(Note, NoteAdmin)
