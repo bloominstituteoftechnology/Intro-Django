e11a02845850aa9edc7a683b220fc8e69c8e46a1
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-10-22 12:44:08 -0700

    setup admin interface

--- MentHealthAPI/admin.py
+++ MentHealthAPI/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post, PersonalPost
+
+
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields=('created_at', 'last_modified')
+
+
+admin.site.register((PersonalPost, Post), PostAdmin)
