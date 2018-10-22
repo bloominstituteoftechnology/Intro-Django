f9885b48ccd5a00994453eb2d3e7431ef718af41
Rachel DiCesare <racheldicesare@missrachelthedeveloper.com>
2018-10-22 18:43:16 -0400

    completed mvp

--- log/models.py
+++ log/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from uuid import uuid4
+
 
 # Create your models here.
 class Topic(models.Model):
    """A topic the user is learning about"""
+   id = models.UUIDField(primary_key=True, default=uuid4,editable=False)
+   title = models.CharField(max_length=200, default='')
    text = models.CharField(max_length=200)
-   date_added = models.DateTimeField(auto_now_add=True)
+   
+
+  
