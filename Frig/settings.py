1a9a61feef9755e1192ab68698629277d1765c89
bri79319 <bri79319@gmail.com>
2018-10-22 18:34:46 -0400

    added secret key and debug to env file

--- Frig/settings.py
+++ Frig/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "0aoqa^(zb!s+h3z(%$acizo1pz872!r7ahlez#cj(bx64_l2ee"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True commented out
 
 ALLOWED_HOSTS = []
 
