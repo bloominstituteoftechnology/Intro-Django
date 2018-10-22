79bbf0cba8125602da8975142fa5f07d03ebe98b
mkerbleski <mkerbles@gmail.com>
2018-10-22 15:03:32 -0600

    created and populated .env

--- foodJournal/models.py
+++ foodJournal/models.py
@@ -4,5 +4,5 @@ from uuid import uuid4
 # Create your models here.
 class Meal(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    title = models.CharField(max_lenght="300")
+    title = models.CharField(max_length=300)
     ingredients = models.TextField(blank=True)
