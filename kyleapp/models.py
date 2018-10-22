648e2c9cf9213edd4cf346c0bca5b022d9675428
meltzerkyle <meltzerkyle@gmail.com>
2018-10-22 18:20:34 -0500

    completed day 1, got boilerplate running

---
+++ kyleapp/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from uuid import uuid4
+# Create your models here.
+
+class Note(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    title = models.CharField(max_length=200)
+    content = models.TextField(blank=True)
