1d057e3dacf48c75f01ec7317c06f6bfc9b0c06e
chainchompa <chainchompa@gmail.com>
2018-10-22 17:31:09 -0400

    created games migration and added 1 game to db

--- djorg/settings.py
+++ djorg/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'notes',
+    'games',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- games/models.py
+++ games/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 from uuid import uuid4
 
 class Game(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=200)
-    release_date = models.IntegerField(max_length=4)
+    release_date = models.IntegerField(
+        validators=[
+            MaxValueValidator(1900),
+            MinValueValidator(2020)
+        ])
