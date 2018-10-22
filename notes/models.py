244634236577e61d60ad733d9cc804d94002a1d5
Alberto-Funk <laptopreneur@gmail.com>
2018-10-22 15:09:20 -0700

    Tags model

--- notes/models.py
+++ notes/models.py
@@ -1,17 +1,22 @@
 from django.db import models
 from uuid import uuid4
+import django.utils.timezone
 # Create your models here.
 
 # Note Record
 # models.Model is for inheritance, Note will inherit from models.Model import
 # Add fields for Note table, plus a unique identifier
+class User(models.Model):
+  id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+  username = models.CharField(max_length=32)
+
 class Note(models.Model):
   id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
   title = models.CharField(max_length=200)
   content = models.TextField(blank=True)
+  user_id = models.ForeignKey(User, default=10, on_delete=models.CASCADE)
 
-
-
-class User(models.Model):
+class Tag(models.Model):
   id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-  username = models.CharField(max_length=32)
+  tag_name = models.CharField(max_length=50)
+  note_id = models.ForeignKey(Note, on_delete=models.CASCADE)
