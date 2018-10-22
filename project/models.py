6c96b6f2dd9a6b4531ae721ccdbced6f4e185b37
Kairi <kairijb@gmail.com>
2018-10-22 17:02:02 -0500

    made migrations and a person class similar to note

--- project/models.py
+++ project/models.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from uuid import uuid4
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=50)
+    about = models.TextField(blank=True)
