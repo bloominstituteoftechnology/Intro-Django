2aa7347ccc4e29b0d8907b06b8f588585bd7535f
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-10-22 17:47:14 -0400

    Created user owned note, registered both to admin

--- simplenotes/models.py
+++ simplenotes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from uuid import uuid4
 
 # Create your models here.
@@ -9,3 +10,7 @@ class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class PersonalNote(Note):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
