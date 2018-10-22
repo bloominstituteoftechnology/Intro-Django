7e6614d0f6836c3636de34da7eb4d16c55d4c0d7
AI2EJAY <JHammett1314@gmail.com>
2018-10-22 17:50:00 -0500

    Everything works

--- djorg/settings.py
+++ djorg/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = '#_e@zoi#awg_td$btojs&-*ga5+6)b1s+jip414kv+i5widp^7'
 SECRET_KEY = config('SECRET_KEY')
-
+print(SECRET_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
