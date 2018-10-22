00f27c0322a2a7746c827fac6fd9b24026f01f1a
mkerbleski <mkerbles@gmail.com>
2018-10-22 14:42:50 -0600

    add Meal models

--- mike/settings.py
+++ mike/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'foodJournal',
 ]
 
 MIDDLEWARE = [
@@ -98,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
