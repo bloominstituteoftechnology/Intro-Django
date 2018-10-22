9a9309262b06a5b452ad3af807ec4598bb6008c8
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-10-22 15:30:05 -0400

    created note model

--- simplenotes/models.py
+++ simplenotes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
+
+
+class Note(models.Model):
+    content = models.TextField(blank=True)
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
