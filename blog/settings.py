585ebcee018cced158fc2c6a8c256887b005ef6e
your <jurgenkela@gmail.com>
2018-10-22 19:29:16 -0400

    Day1 MVP completed

--- blog/settings.py
+++ blog/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from decouple import config 
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i69biy(ib)%g2k#k-xh6!3&37jhvlncj=ou@bb1@n2sb*7(fza'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -31,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog_1',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- blog_1/models.py
+++ blog_1/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
+
+
+class Blog(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    title = models.CharField( max_length=30)
+    content = models.TextField(blank=True)
+
