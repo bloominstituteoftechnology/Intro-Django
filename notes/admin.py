ad7807ac362eab2b946281c595774b28140655b9
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-10-22 15:35:08 -0600

    Migrated personal notes to database that link to specific user

--- notes/admin.py
+++ notes/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Notes
+from .models import Notes, PersonalNote
 
 class NotesAdmin(admin.ModelAdmin):
     readonly_fields = ('created_at', 'last_modified')
 
 # Register your models here.
 admin.site.register(Notes, NotesAdmin)
+admin.site.register(PersonalNote)
--- notes/models.py
+++ notes/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from uuid import uuid4
 
+from django.contrib.auth.models import User
+
 # Create your models here.
 class Notes(models.Model):
     id = models.UUIDField(primary_key = True, default = uuid4, editable = False)
@@ -8,3 +10,6 @@ class Notes(models.Model):
     content = models.TextField(blank = True)
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
+
+class PersonalNote(Notes):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
