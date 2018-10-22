97e4d8ca8e57efb08cac064227691182c2820db5
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-10-22 16:06:44 -0500

    models.txt file made

---
+++ djangoweek/env.py
@@ -0,0 +1,3 @@
+# ALLOWED_HOSTS=.localhost
+# DEBUG = True
+# SECRET_KEY = 'yi3$+8$_gemj70qx=a@7b)5#^t_x6^5i@c@ln^0l1_!fh34e#+'
--- djangoweek/settings.py
+++ djangoweek/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+from decouple import config, Csv
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,13 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'yi3$+8$_gemj70qx=a@7b)5#^t_x6^5i@c@ln^0l1_!fh34e#+'
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
