842523792c63b3142e3d0d96cae14591c52bf733
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-10-22 15:11:25 -0600

    Added timestamp migrations and updated admin notes view

--- notes/admin.py
+++ notes/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Notes
+
+class NotesAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'last_modified')
+
 # Register your models here.
-admin.site.register(Notes)
+admin.site.register(Notes, NotesAdmin)
