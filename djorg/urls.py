c649f27d1bc683af11e046e9aa957b33e90cba87
francistse23 <wtse.ucsd@gmail.com>
2018-10-23 11:31:38 -0700

    implemented two new classes in api.py (PersonalNoteSerializer and PersonalNoteViewset).

--- djorg/urls.py
+++ djorg/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework import routers
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
