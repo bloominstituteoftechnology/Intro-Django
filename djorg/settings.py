40fa248a8aa9fd63d2edc844b458bbf13f22efe5
MichaelBoegner <cmikeb@gmail.com>
2018-10-23 16:53:55 -0400

    CORS Installed. Day 3 Completed

--- djorg/settings.py
+++ djorg/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'rest_framework',
     'notes',
     'django.contrib.admin',
@@ -43,7 +44,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
