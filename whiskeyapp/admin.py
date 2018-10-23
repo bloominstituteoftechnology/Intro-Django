419b3ab73ca62895cb86e8530e0b712fc271ccc8
keegn <keegan.t.burkett@gmail.com>
2018-10-23 16:54:27 -0500

    Added subclass model and another field within the subclass

--- whiskeyapp/admin.py
+++ whiskeyapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Whiskey, PersonalWhiskey
+
+class WhiskeyAdmin(admin.ModelAdmin):
+    readonly_fields=('created_at', 'last_modified')
 
 # Register your models here.
+admin.site.register(Whiskey, WhiskeyAdmin)
+admin.site.register(PersonalWhiskey)
