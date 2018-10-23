01e760e78aa673dfa04c8cd90c26a9502af29a8a
your <jurgenkela@gmail.com>
2018-10-23 19:14:20 -0400

    Admin interface and Migrations w/ new fields completed

--- blog_1/admin.py
+++ blog_1/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Blog
 # Register your models here.
+
+class BlogAdmin(admin.ModelAdmin):
+    readonly_fields=('created_at', 'last_modified')
+
+admin.site.register(Blog, BlogAdmin)
--- blog_1/models.py
+++ blog_1/models.py
@@ -8,4 +8,6 @@ class Blog(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField( max_length=30)
     content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
 
