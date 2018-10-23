025e19429db6637e1ba4dc9d14c4e7ef1831fc38
David Pok <david.pok@gmail.com>
2018-10-22 20:27:12 -0700

    adds migrations

--- notANotesApp/settings.py
+++ notANotesApp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,17 +21,18 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ccmfk_bfi@e&35_lkoc1v@=!_yu-&xqwneim%-7xpk^@@_^(*_'
 
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
+DEBUG = config('DEBUG', cast=bool)
 ALLOWED_HOSTS = []
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'notNotes',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
