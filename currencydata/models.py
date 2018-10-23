f0d6078d35e0912ce2d140768cfa5e6cf957825b
codejoncode <jonathanjholloway@gmail.com>
2018-10-23 17:20:27 -0500

    Able to add to the personal class and see it also able to use the objects.all() method to show the data in the table.

--- currencydata/models.py
+++ currencydata/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.utils import timezone 
 from uuid import uuid4
+from django.contrib.auth.models import User
 # Create your models here.
 class Currencydata (models.Model):
     id = models.UUIDField(primary_key =True, default=uuid4, editable=False)
@@ -9,7 +11,22 @@ class Currencydata (models.Model):
     the_low = models.FloatField()
     the_close = models.FloatField()
     bar_type = models.CharField(max_length = 7)#'bull'  'bear'  'neutral'
-    created_at = models.DateTimeField(auto_now_add=True)
+    # created_at = models.DateTimeField(auto_now_add=True, default=timezone.now)
+    created_at = models.DateTimeField(default=timezone.now)
+    #Adjustment made because of error message received. 
+    #the options auto_now auto_now_add and default 
+    # are mutally exclusive only one of these options may be present.
     last_modified = models.DateTimeField(auto_now=True)
 
+# End of Currencydata Class
+
+#Personal(per-user)
+class PersonalCurrencydata(Currencydata):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    #What this is doing is importing Django's built in user class model with 
+    #something called a foreign key to create a reference to data on another table.
+    #On_delete=models.CASCADE helps with integrity of the data.  protect consistency.
+    #There shouldn't be an item in one table that references the foreign key of something
+    #that has been removed from another. 
+#end of Personal Class
 # Currencydata(date = '2018.09.12', the_open = 1.6963, the_high = 1.69636, the_low = 1.69596, the_close = 1.69619, bar_type = 'bear')
