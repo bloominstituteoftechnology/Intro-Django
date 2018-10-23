74efe8f6dc2ff074898227dc103c73ddd7c34912
rverdi642 <rverdi642@gmail.com>
2018-10-23 17:00:37 -0500

    Richard Verdier - Day2 complete

--- OddJobBoard/admin.py
+++ OddJobBoard/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import OddJobBoard
 # Register your models here.
-admin.site.register(OddJobBoard)
+class OddJobBoardAdmin(admin.ModelAdmin):
+	readonly_fields=('created_at', 'last_modified')
+
+# Register your models here.
+admin.site.register(OddJobBoard, OddJobBoardAdmin)
+#admin.site.register(OddJobBoard)
--- OddJobBoard/models.py
+++ OddJobBoard/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from uuid import uuid4
 # Create your models here.
 class OddJobBoard(models.Model):
@@ -7,3 +8,9 @@ class OddJobBoard(models.Model):
     area = models.TextField(blank=True)
     work = models.TextField(blank=True)
     email = models.CharField(max_length=100)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+class PersonalNote(OddJobBoard):   # Inherits from Note!
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
