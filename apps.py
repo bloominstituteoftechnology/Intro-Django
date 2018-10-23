8df6cd390763278ed016c26277ed538d6d91206b
JoCatherine Griggs <jocafrin@gmail.com>
2018-10-23 16:35:44 -0400

    Day 1 complete

--- apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TheDjangoistClothesConfig(AppConfig):
-    name = 'the_djangoist_clothes'
--- the_djangoist_clothes/models.py
+++ the_djangoist_clothes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from uuid import uuid4
+
+class Garment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
 
 # Create your models here.
