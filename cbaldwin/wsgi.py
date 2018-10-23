f59b7c0c82c8d18061d3a7c2f5c7047af1798057
Carey <careycbaldwin@gmail.com>
2018-10-23 15:03:51 -0400

    set up a new app called 'greenline'

--- cbaldwin/wsgi.py
+++ cbaldwin/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for djorg project.
+WSGI config for cbaldwin project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djorg.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cbaldwin.settings')
 
 application = get_wsgi_application()
--- greenline/models.py
+++ greenline/models.py
@@ -3,7 +3,11 @@ from uuid import uuid4
 
 # Create your models here.
 
-class Note(models.Model):
+class Post(models.Model):
     id = models.UUIDField(primary_key = True, default = uuid4, editable = False)
     title = models.CharField(max_length = 200)
     content = models.TextField(blank = True)
+    date = models.DateTimeField(auto_now_add = True)
+    author = models.TextField()
+
+
