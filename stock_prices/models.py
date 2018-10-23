c1d8b662d25df7ace8c0d3901d4e12d5a8f843dc
vanceleon <vanceleon44@gmail.com>
2018-10-23 16:03:46 -0400

    setting up the project

--- stock_prices/models.py
+++ stock_prices/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
+class Companies(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable-False)
+    name = models.CharField(max_length=200)
+    
+
+
+
+# class StockPrices(models.Model):
+#     comp_id = models.
+#     date = models.
+#     close_price = 
+#     adj_close = 
+
