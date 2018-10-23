0e7f66a5cf6099b47dcb51388783eedcf201fb22
Sania Parekh <sparekh@Sanias-MacBook.local>
2018-10-23 15:27:48 -0500

    models migrated manage.py shell added example cat, showing when checking objects.all

--- catsdirectory/models.py
+++ catsdirectory/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
+class Cat(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    breed = models.CharField(max_length=200)
+    facts = models.TextField(blank=True)
