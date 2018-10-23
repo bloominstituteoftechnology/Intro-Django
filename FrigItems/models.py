a0e62123c9fb1764fd622417c5862b4e5cc640c2
bri79319 <bri79319@gmail.com>
2018-10-23 16:02:57 -0400

    added notes to table and now working on users

--- FrigItems/models.py
+++ FrigItems/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from uuid import uuid4
 
+from django.contrib.auth.models import User
+
 # Create your models here.
 
 
@@ -9,3 +11,10 @@ class Note(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
     url = models.URLField(blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+
+class PersonalNote(Note):
+    user = models.ForeignKey(User)
