beaf491b824ec01e4a3047652c72a1f43ce21049
Fred <sohnf90@yahoo.com>
2018-10-23 03:52:26 -0700

    Day 1 Done

--- djorg/settings.py
+++ djorg/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c1u39-*5bb1o(ia&5xo0=93rmft3v%ip8(q8+z_5ao4e(sq(0l'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'notes'
 ]
 
 MIDDLEWARE = [
--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
+
+class Note(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField(blank=True)
+    T_or_F = models.BooleanField(blank=True)
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
