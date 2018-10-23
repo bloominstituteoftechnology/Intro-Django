d7b6aa1a201df8099bed5d2e14c8994559ce62a1
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-10-23 16:14:50 -0500

    Added to module
    I added more fields to my Movies class

--- horror_movies/admin.py
+++ horror_movies/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 from .models import Movies
+
+
 # Register your models here.
 
 admin.site.register(Movies)
--- horror_movies/models.py
+++ horror_movies/models.py
@@ -6,4 +6,23 @@ class Movies(models.Model):
     id = models.UUIDField(primary_key=True, editable=False, default=uuid4)
     title = models.CharField(max_length = 200)
     content = models.TextField(blank = True)
+    review = models.TextField(blank = True)
+    RottenTomatoes = models.URLField(blank=True)
     url = models.URLField(blank=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+# class Review(models.Model):
+#     id = models.UUIDField(primary_key=True, editable=False, default=uuid4)
+#     author = models.CharField(max_length = 200)
+#     content = models.TextField(blank = True)
+#     created_at=models.DateTimeField(auto_now_add=True)
+#     last_modified = models.DateTimeField(auto_now=True)
+
+# class Releases(models.Model):
+#     id = models.UUIDField(primary_key=True, editable=False, default=uuid4)
+#     title = models.CharField(max_length = 200)
+#     month = models.CharField(max_length = 200)
+
+
+
