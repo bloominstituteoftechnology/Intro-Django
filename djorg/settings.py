41ecb1ad3bee5a5ca40c4d46f81091a576de4be3
AI2EJAY <JHammett1314@gmail.com>
2018-10-23 11:20:58 -0500

    some admin stuff in

--- djorg/settings.py
+++ djorg/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '#_e@zoi#awg_td$btojs&-*ga5+6)b1s+jip414kv+i5widp^7'
 SECRET_KEY = config('SECRET_KEY')
 print(SECRET_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
