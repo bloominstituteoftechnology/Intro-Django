bb4c3530863489dfb8754341db37591dc3873717
Carey <careycbaldwin@gmail.com>
2018-10-23 19:15:47 -0400

    fixed 'date created' and 'date modified'
    remember, readonly_field is a built-in variable

--- greenline/admin.py
+++ greenline/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Post, UserPost
 
 class PostAdmin(admin.ModelAdmin):
-    read_only = ('date_created', 'date_modified')
+    readonly_fields = ('date_created', 'date_modified')
 
 # Register your models here.
 admin.site.register(Post, PostAdmin)
