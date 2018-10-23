342d7e890a622baf8851fcf315f59f57cc505525
Carey <careycbaldwin@gmail.com>
2018-10-23 16:29:51 -0400

    created subclass UserPost and registered in admin

--- greenline/admin.py
+++ greenline/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, UserPost
 
 class PostAdmin(admin.ModelAdmin):
     read_only = ('date_created', 'date_modified')
 
 # Register your models here.
 admin.site.register(Post, PostAdmin)
-
+admin.site.register(UserPost)
--- greenline/models.py
+++ greenline/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from uuid import uuid4
 
+from django.contrib.auth.models import User
+
 # Create your models here.
 
 class Post(models.Model):
@@ -11,5 +13,10 @@ class Post(models.Model):
     date_modified = models.DateTimeField(auto_now = True)
     author = models.TextField()
 
+#make a subclass of posts
+class UserPost(Post):  #inherit properties from Note
+    # use foreign key to create a reference to another table
+    # CASCADE allows data to be consistent so that when a record is removed, related data will also be removed
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
 
 
