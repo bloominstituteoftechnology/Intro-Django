8436de20ae33f8e483c2154c17bf9b48a2ce355c
James Phillips <bri79319@gmail.com>
2018-10-24 16:46:38 -0400

    Day 4 work

--- Music/settings.py
+++ Music/settings.py
@@ -40,12 +40,14 @@ INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
+    "corsheaders",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
