f2a7094a2f30a4cda88bee520d566e6b99b8cc45
Charlie Winslow <cwinslow22@yahoo.com>
2018-10-24 15:23:11 -0500

    added the required user field

--- roster/api.py
+++ roster/api.py
@@ -7,6 +7,14 @@ class PersonalPlayerSerializer(serializers.HyperlinkedModelSerializer):
         model = PersonalPlayer
         fields = ('firstName', 'lastName', 'position', 'team')
 
+    def create(self, validated_data):
+        # import pdb; pdb.set_trace()
+        user = self.context['request'].user
+
+        player = PersonalPlayer.objects.create(user=user, **validated_data)
+        
+        return player
+
 class PersonalPlayerViewSet(viewsets.ModelViewSet):
     serializer_class = PersonalPlayerSerializer
     queryset = PersonalPlayer.objects.all()
