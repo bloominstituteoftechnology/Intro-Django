4ba454323ced1ae77c4d5916af42b275c1756d24
Grant Reighard <grant.reighard@gmail.com>
2018-10-23 21:38:12 -0400

    Added token auth for rest. Finished Day 4.

--- djorg/settings.py
+++ djorg/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'films',
     'rest_framework',
+    'rest_framework.authtoken',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,12 +123,21 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Authentication
+
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+
 # REST Framework
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES' : (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    )
 }
 
 # CORS
