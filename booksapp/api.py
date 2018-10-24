c5ef4e26ce98a20a41c8b04673aa0997a24edaa6
Sukhada Gholba <sukhadagholba@gmail.com>
2018-10-23 19:21:19 -0700

    modified api to show Personal Book List based on the currentlt logged-in user

--- booksapp/api.py
+++ booksapp/api.py
@@ -5,9 +5,22 @@ class PersonalBookSerializer(serializers.HyperlinkedModelSerializer):
     
     class Meta:
         model = PersonalBook
-        fields = ('title', 'author', 'description')
+        fields = ('title', 'author')
 
+    def create(self, validated_data):
+        #import pdb; pdb.set_trace()
+        user = self.context['request'].user
+        book = PersonalBook.objects.create(user=user,**validated_data)
+        return book
 
 class PersonalBookViewSet(viewsets.ModelViewSet):
     serializer_class = PersonalBookSerializer
     queryset = PersonalBook.objects.all()
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.is_anonymous:
+            return PersonalBook.objects.none()
+        else:
+            return PersonalBook.objects.filter(user=user)
