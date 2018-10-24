751a31f2132d807995f3f0013cba8b19c892cfd6
Sania Parekh <sparekh@Sanias-MacBook.local>
2018-10-24 15:33:54 -0500

    Admin interface and migrations II

--- catsdirectory/models.py
+++ catsdirectory/models.py
@@ -6,3 +6,6 @@ class Cat(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     breed = models.CharField(max_length=200)
     facts = models.TextField(blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
