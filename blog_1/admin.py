714cdfd55f4a37f3d583f77ed6f12f9353fee05a
your <jurgenkela@gmail.com>
2018-10-24 10:18:52 -0400

    Day 2 MVP completed

--- blog_1/admin.py
+++ blog_1/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Blog
+from .models import Blog, PersonalBlog
 # Register your models here.
 
 class BlogAdmin(admin.ModelAdmin):
     readonly_fields=('created_at', 'last_modified')
 
-admin.site.register(Blog, BlogAdmin)
+admin.site.register((Blog, PersonalBlog), BlogAdmin)
--- blog_1/models.py
+++ blog_1/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from uuid import uuid4
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -11,3 +12,5 @@ class Blog(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
 
+class PersonalBlog(Blog):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
