43c4bf05d55c1ddbeb8e1abbc00c8a92c283d193
your <jurgenkela@gmail.com>
2018-10-24 18:19:15 -0400

    Day 3 MVP completed

---
+++ blog_1/api.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers, viewsets
+from .models import PersonalBlog
+
+class PersonlBlogSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = PersonalBlog
+        fields = ('title', 'content')
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+        blog = PersonalBlog.objects.create(user=user, **validated_data)
+        return blog
+
+
+class PersonalBlogViewset(viewsets.ModelViewSet):
+    serializer_class = PersonlBlogSerializer
+    queryset = PersonalBlog.objects.none()
+    
+    def get_queryset(self):
+        user = self.request.user
+        
+        if user.is_anonymous:
+            return PersonalBlog.objects.none()
+        else:
+            return PersonalBlog.objects.filter(user=user)
