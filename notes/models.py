f75d7976fb05d4b9ac42245819f22f2f27fe06e8
Fred <sohnf90@yahoo.com>
2018-10-23 23:19:02 -0700

    SQL

--- notes/models.py
+++ notes/models.py
@@ -8,7 +8,7 @@ class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
-    T_or_F = models.BooleanField(blank=True)
+    likes_seafood = models.BooleanField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
 
