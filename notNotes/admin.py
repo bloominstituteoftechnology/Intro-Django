f37c2b63f74a401c2089ef3ead7b34dea2760f39
David Pok <david.pok@gmail.com>
2018-10-23 22:47:18 -0700

    finishes day 2

--- notNotes/admin.py
+++ notNotes/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import NotNote, NotNoteAdmin, PersonalNotNote
 
 # Register your models here.
+admin.site.register(NotNote, NotNoteAdmin)
+admin.site.register(PersonalNotNote)
--- notNotes/models.py
+++ notNotes/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib import admin
+from django.contrib.auth.models import User
 from uuid import uuid4
 
 # Create your models here.
@@ -7,3 +9,11 @@ class NotNote(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
     url = models.URLField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+class NotNoteAdmin(admin.ModelAdmin):
+    readonly_fields=('created_at', 'last_modified')
+
+class PersonalNotNote(NotNote): #Inherits from NotNote!
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
