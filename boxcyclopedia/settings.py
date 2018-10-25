46d7493cead0fd1bb4f0a6bcd328c6f12e02ea12
tulf <timzulf@gmail.com>
2018-10-25 11:17:00 -0400

    fixed configs to add

--- boxcyclopedia/settings.py
+++ boxcyclopedia/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 from decouple import config
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -106,6 +107,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 # Rest Framework config.
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
