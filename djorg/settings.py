bdf5f8a05bceb5373ee4876def3ae776007d55f5
francistse23 <wtse.ucsd@gmail.com>
2018-10-25 13:49:21 -0700

    commented out STATIC_ROOT.

--- djorg/settings.py
+++ djorg/settings.py
@@ -156,6 +156,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, ‘static’)
+# STATIC_ROOT = os.path.join(BASE_DIR, ‘static’)
 
 STATIC_URL = '/static/'
