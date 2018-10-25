4b33e1fe3ce2dcfc51db7bc0afeb2b4a76fe8d61
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-10-24 22:14:34 -0500

    Finished Day 3

--- djangoweek/settings.py
+++ djangoweek/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK ={
-    'DEFAULT_PERMISSION_CLASSES':{
+    'DEFAULT_PERMISSION_CLASSES':[
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    }
+    ]
 }
