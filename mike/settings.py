2eeee3788e1177b0fea69b982b689700a46e4aa2
mkerbleski <mkerbles@gmail.com>
2018-10-25 14:49:20 -0600

    add corsheaders to import again and again

--- mike/settings.py
+++ mike/settings.py
@@ -15,7 +15,7 @@ from decouple import config
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 from decouple import config
 import dj_database_url
-import django-cors-headers
+from django-cors-headers import corsheaders
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
