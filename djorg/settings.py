1e7edb7e77de84862e45fc089b36a8237603bab0
francistse23 <wtse.ucsd@gmail.com>
2018-10-25 11:25:34 -0700

    Modified env file/settings to debug.

--- djorg/settings.py
+++ djorg/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS')
+ALLOWED_HOSTS = ['*']
 
 CORS_ORIGIN_ALLOW_ALL = config('CORS', cast=bool)
 # Application definition
