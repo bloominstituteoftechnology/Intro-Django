51aad1e716a94748fc5f5b6ed06631cddce005a3
Grant Reighard <grant.reighard@gmail.com>
2018-10-25 17:07:34 -0400

    Edited DATABASES config in settings.py.

--- djorg/settings.py
+++ djorg/settings.py
@@ -79,15 +79,19 @@ WSGI_APPLICATION = 'djorg.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'dj_database_url.parse("postgres://...", conn_max_age=600)',
+        'NAME': 'postgresql-crystalline-16433',
     }
 }
-
-DATABASES['default'] = dj_database_url.parse('postgres://...', conn_max_age=600)
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
