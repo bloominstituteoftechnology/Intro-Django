b8470d7a9a2ca068f24a6ded20c1345dd5724fb9
Sukhada Gholba <sukhadagholba@gmail.com>
2018-10-25 13:35:32 -0700

    added CORS sert up

--- books/settings.py
+++ books/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'booksapp',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     'graphene_django',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -149,3 +151,5 @@ REST_FRAMEWORK = {
 GRAPHENE = {
     'SCHEMA': 'booksapp.schema.schema'
 }
+
+CORS_ORIGIN_ALLOW_ALL =True
