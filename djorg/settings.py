e2dcd1b850592995c5f61a136975433ededa4084
Fred <sohnf90@yahoo.com>
2018-10-25 13:37:29 -0700

    Set up token authentication

--- djorg/settings.py
+++ djorg/settings.py
@@ -42,12 +42,20 @@ INSTALLED_APPS = [
     'notes',
     'rest_framework',
     'corsheaders',
+    'rest_framework.authtoken',
 ]
+
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ),
 }
 
 MIDDLEWARE = [
