4332fb019dac437df5a05adf4146bcb7ffa29d21
“ericathompson” <ericathompson.nc@gmail.com>
2018-10-25 08:44:16 -0700

    Day 2 Complete

--- groceries/models.py
+++ groceries/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from uuid import uuid4
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
@@ -7,4 +9,9 @@ class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
-    
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+class PersonalNote(Note):   # Inherits from Note!
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
