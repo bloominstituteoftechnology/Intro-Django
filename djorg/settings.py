9f93770238748f9ed4f1e332c3c9df2ddaa0f789
MichaelBoegner <cmikeb@gmail.com>
2018-10-25 17:23:36 -0400

    databases restored

--- djorg/settings.py
+++ djorg/settings.py
@@ -84,16 +84,14 @@ WSGI_APPLICATION = 'djorg.wsgi.application'
 # Databases
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 DATABASES['default'] = dj_database_url.config(default='sqlite:///db.sqlite3')
-DATABASES['default'] = dj_database_url.parse('sqlite:///db.sqlite3', conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
