f1d89810a6ebef0e4ad88c0fd1875ecdfb909f12
francistse23 <wtse.ucsd@gmail.com>
2018-10-25 14:26:27 -0700

    added staticfiles folder. reconfigured static_xx in settings.py.

--- djorg/settings.py
+++ djorg/settings.py
@@ -156,6 +156,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-# STATIC_ROOT = os.path.join(BASE_DIR, ‘static’)
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
