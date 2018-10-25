853d5ea730672913544089313b5152367bf4ba9e
MichaelBoegner <cmikeb@gmail.com>
2018-10-25 17:45:15 -0400

    Databses Default needs to exist as currently written.

--- djorg/settings.py
+++ djorg/settings.py
@@ -84,14 +84,14 @@ WSGI_APPLICATION = 'djorg.wsgi.application'
 # Databases
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
-DATABASES = dj_database_url.config(default='sqlite:///db.sqlite3')
+DATABASES['default'] = dj_database_url.config(default='sqlite:///db.sqlite3')
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
