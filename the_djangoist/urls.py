39fb3d0afa3104248f03f9f68499a819ea9ec51a
JoCatherine Griggs <jocafrin@gmail.com>
2018-10-25 13:57:12 -0400

    Routes have been initialized

--- the_djangoist/urls.py
+++ the_djangoist/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework import routers
+from the_djangoist_clothes.api import UserGarmentViewSet
+
+router = routers.DefaultRouter()
+router.register(r'the_djangoist_clothes', UserGarmentViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include (router.urls)),
 ]
--- the_djangoist_clothes/api.py
+++ the_djangoist_clothes/api.py
@@ -1,12 +1,12 @@
 from rest_framework import serializers, viewsets
 from .models import UserGarment, Garment
 
-    class UserGarmentSerializer(serializers.HyperlinkedModelSerializer):
+class UserGarmentSerializer(serializers.HyperlinkedModelSerializer):
 
-        class Meta:
-            model = UserGarment
-            fields = ('name', 'description')
+    class Meta:
+        model = UserGarment
+        fields = ('name', 'description')
 
-    class UserGarmentViewSet(viewsets.ModelViewSet):
-        serialiser_class = UserGarmentSerializer
-        queryset = UserGarment.objects.all()
+class UserGarmentViewSet(viewsets.ModelViewSet):
+    serialiser_class = UserGarmentSerializer
+    queryset = UserGarment.objects.all()
