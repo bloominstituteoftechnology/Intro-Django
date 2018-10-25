12afc7de065de834ae9015ffaee63c5885d26c55
jordan-massingill <jordan_massingill@hotmail.com>
2018-10-25 14:30:50 -0500

    Completed adding token authorization to project and successfully testing

--- djarg/settings.py
+++ djarg/settings.py
@@ -132,6 +132,8 @@ REST_FRAMEWORK = {
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     )
 }
--- djarg/urls.py
+++ djarg/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 from rest_framework import routers
 from mydb.api import ShelterDogViewSet, DogViewSet
@@ -29,5 +29,5 @@ router.register('dogs', DogViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('api-token-auth', views.obtain_auth_token),
+    re_path(r'^api-token-auth/', views.obtain_auth_token)
 ]
