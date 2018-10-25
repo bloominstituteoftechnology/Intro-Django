d8b218cf6bb8b8d0030974a501fd775eac99013d
MichaelBoegner <cmikeb@gmail.com>
2018-10-25 18:26:05 -0400

    tweak

--- djorg/settings.py
+++ djorg/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(default='sqlite:///db.sqlite3')
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
