2b8474bd1a4d0fd3026d90025c452418b8508dd1
sca4cs <shannon.atkinson.ca@gmail.com>
2018-10-25 19:34:41 -0400

    static files

--- djorg/settings.py
+++ djorg/settings.py
@@ -133,6 +133,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
