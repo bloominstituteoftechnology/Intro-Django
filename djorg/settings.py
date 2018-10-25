87d99e8fc245a9ad012b7742cec3bbee6646e99e
francistse23 <wtse.ucsd@gmail.com>
2018-10-25 13:35:49 -0700

    added static_root in settings.py based on heroku log.

--- djorg/settings.py
+++ djorg/settings.py
@@ -156,5 +156,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, ‘staticfiles’)
 STATIC_URL = '/static/'
