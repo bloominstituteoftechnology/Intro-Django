2dd18a37a15232d6b5ddf9155bf845eaf62e7140
francistse23 <wtse.ucsd@gmail.com>
2018-10-25 14:01:08 -0700

    updated TEMPLATES-DIRS in settings.py.

--- djorg/settings.py
+++ djorg/settings.py
@@ -94,7 +94,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
