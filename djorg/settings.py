74a81d85f234dbfd1ccaf99af6296b2abf16157a
Grant Reighard <grant.reighard@gmail.com>
2018-10-25 18:15:20 -0400

    Updated static settings to try to get rid of Error 500 when opening app on web.

--- djorg/settings.py
+++ djorg/settings.py
@@ -135,6 +135,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 # Authentication
 
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
