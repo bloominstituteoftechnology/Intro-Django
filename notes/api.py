3bb938abee11635b2321af8c21a144142cd79093
skennedy93 <sean47900@gmail.com>
2018-10-24 20:51:40 -0500

    day 3 done

--- notes/api.py
+++ notes/api.py
@@ -3,10 +3,22 @@ from .models import PersonalNote
 
 class PersonalNoteSerializer(serializers.HyperlinkedModelSerializer):
 
-     class Meta:
+    class Meta:
         model = PersonalNote
         fields = ('title', 'content')
 
+    def create(self, validated_data):
+        user = self.context['request'].user
+        note = PersonalNote.objects.create(user = user, **validated_data)
+        return note
+
 class PersonalNoteViewSet(viewsets.ModelViewSet):
     serializer_class = PersonalNoteSerializer
-    queryset = PersonalNote.objects.all() 
+    queryset = PersonalNote.objects.none()
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_anonymous:
+            return PersonalNote.objects.none()
+        else:
+            return PersonalNote.objects.filter(user=user) 
