0493d36cdde2d49b8faba7d5f862f489792c2ae0
mkerbleski <mkerbles@gmail.com>
2018-10-25 14:44:54 -0600

    adjust order of middleware again and again

--- mike/settings.py
+++ mike/settings.py
@@ -62,9 +62,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 
     'django.contrib.sessions.middleware.SessionMiddleware',
