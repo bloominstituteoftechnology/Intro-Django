8207df2860069e839565e78eaa6d2b380d6c0ba8
Fred <sohnf90@yahoo.com>
2018-10-26 10:00:30 -0700

    Debugged DATABASES in setting.py

--- djorg/settings.py
+++ djorg/settings.py
@@ -102,7 +102,7 @@ WSGI_APPLICATION = 'djorg.wsgi.application'
 #     }
 # }
 
-DATABASES['default'] = dj_database_url.config(con_max_age=600)
+DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
 
 
 # Password validation
