5239a233b4ff6e62e4df1c470d5d3aa5483e261f
cubsfan46350 <cubsfan.46350@msn.com>
2018-10-26 16:18:35 -0500

    debug

--- djorg/settings.py
+++ djorg/settings.py
@@ -147,5 +147,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Configure Django App for Heroku.
-import django_heroku
-django_heroku.settings(locals())
+#import django_heroku
+#django_heroku.settings(locals())
