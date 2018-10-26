fb5e36f9d44ca7973a9988a4bb5af35e9466798b
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-10-26 10:36:48 -0400

    Trying to fix allowed hosts

--- organizer/settings.py
+++ organizer/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS')
 
 CORS_ORIGIN_ALLOW_ALL = True
 
