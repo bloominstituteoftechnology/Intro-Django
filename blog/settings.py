bf20d4951d163918012dc2fde885c595bde3e8ce
your <jurgenkela@gmail.com>
2018-10-26 13:06:49 -0400

    updste

--- blog/settings.py
+++ blog/settings.py
@@ -104,8 +104,7 @@ DATABASES = {}
 #     }
 # }
 
-DATABASES['default'] = dj_database_url.parse(config(conn_max_age=600)
-
+DATABASES['default'] = dj_database_url.parse(config('DATABASE_URL'), conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
