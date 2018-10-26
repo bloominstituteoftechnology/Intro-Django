4c80f97d54694bcd9f8e4ebf5f1c7f93a5e372fe
Kairi <kairijb@gmail.com>
2018-10-26 17:39:04 -0500

    deployed to heroku

--- kjango/settings.py
+++ kjango/settings.py
@@ -92,6 +92,7 @@ DATABASES['default'] = dj_database_url.parse('postgres://...', conn_max_age=600)
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
