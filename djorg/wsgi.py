e59734be698d32ca1457b05c06133fbec5d9f1aa
AI2EJAY <JHammett1314@gmail.com>
2018-10-26 12:05:05 -0500

    might be safe to deploy

--- djorg/wsgi.py
+++ djorg/wsgi.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djorg.settings')
 
