5046a6bcca03a8e327fbd2062421ec84962889f6
adamcglee <adamcglee@gmail.com>
2018-10-26 08:33:37 -0700

    added STATIC_ROOT in settings.py

--- djorg/settings.py
+++ djorg/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 
