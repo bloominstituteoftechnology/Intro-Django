a0ebe4f2e9a3d1433c8449d3ce57ad7e0e6dba19
vanceleon <vanceleon44@gmail.com>
2018-10-26 12:29:50 -0400

    databases

--- stock_performance/settings.py
+++ stock_performance/settings.py
@@ -78,20 +78,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'stock_performance.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-# DATABASES = {
-#     'default': dj_database_url.config(conn.conn_max_age=600)
-# }
+DATABASES = {}
 
 DATABASES['default'] = dj_database_url.config(conn.conn_max_age=600)
 
