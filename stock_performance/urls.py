55537c814ccbcaa22b7ce244b9c124d26dc64f0b
vanceleon <vanceleon44@gmail.com>
2018-10-26 18:02:46 -0400

    edit to urls

--- stock_performance/urls.py
+++ stock_performance/urls.py
@@ -22,7 +22,7 @@ from stock_prices.api import PersonalCompaniesViewSet
 
 router = routers.DefaultRouter()
 # router.register(r'/', PersonalCompaniesViewSet)
-router.register(r'companies/', PersonalCompaniesViewSet)
+router.register(r'companies', PersonalCompaniesViewSet)
 re_path(r'^api-token-auth/', views.obtain_auth_token)
 
 
