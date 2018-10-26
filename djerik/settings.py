32447700778db32ea096ed269b7ef02f965f07b7
Erik Kimsey <erik@erikkimsey.com>
2018-10-26 13:49:17 -0400

    Changed allowed hosts in .env

--- djerik/settings.py
+++ djerik/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool)
 
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS')
 
 
 # Application definition
