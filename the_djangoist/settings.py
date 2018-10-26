c08ce2d91777d257fd1fb0fb0b257bf05ff86455
JoCatherine Griggs <jocafrin@gmail.com>
2018-10-26 14:22:05 -0400

    trying again for allowed hosts

--- the_djangoist/settings.py
+++ the_djangoist/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = [‘herokuapp.com’]
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 CORS_ORIGIN_ALLOW_ALL = True
 
