c43c4feec65b8c98b7ef656eb7a20e6c398f7298
AI2EJAY <JHammett1314@gmail.com>
2018-10-26 13:42:59 -0500

    possible fix

--- djorg/wsgi.py
+++ djorg/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
+
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djorg.settings')
