2fea8b6cdf02b1ba94e2d832b2e11dcb03e7f7b2
Alberto-Funk <laptopreneur@gmail.com>
2018-10-26 01:18:20 -0700

    Args

--- notesapp/models.py
+++ notesapp/models.py
@@ -5,35 +5,8 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Note(models.Model):
   id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
   title = models.CharField(max_length=200)
   content = models.TextField(blank=True)
   created_at = models.DateTimeField(auto_now_add=True)
   last_modified = models.DateTimeField(auto_now=True)
-
-class PersonalNote(Note):
-  user = models.ForeignKey(User, on_delete=models.CASCADE)
-  private_content = models.TextField(blank=True)
-
-
-# When I create a PersonalNote, it also adds it to the Note table
-# might have to cut inherit
-"""
-https://docs.graphene-python.org/projects/django/en/latest/tutorial-plain/
-
-class Category(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-
-class Ingredient(models.Model):
-    name = models.CharField(max_length=100)
-    notes = models.TextField()
-    #***
-    category = models.ForeignKey(
-        Category, related_name='ingredients', on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
-"""
--- notesapp/schema.py
+++ notesapp/schema.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from graphene_django import DjangoObjectType
 import graphene
 from .models import Note, PersonalNote
+from graphene_django import DjangoConnectionField
 
 class NoteType(DjangoObjectType):
   class Meta:
@@ -12,34 +13,40 @@ class PersonalNoteType(DjangoObjectType):
   class Meta:
     model = PersonalNote
     interfaces = (graphene.relay.Node,)
+    #filter_fields = ['title', 'content']
+    filter_fields = {
+      'title': ['exact', 'icontains', 'istartswith'],
+      'content': ['exact', 'icontains'],
+    }
 
 
 class Query(graphene.ObjectType):
-  notes = graphene.List(NoteType)
-  personalnotes = graphene.List(PersonalNoteType)
+  personalnotes = DjangoConnectionField(PersonalNoteType)
+  # notes = graphene.List(NoteType)
+  # personalnotes = graphene.List(PersonalNoteType)
 
 
-  def resolve_notes(self, info):
-    user = info.context.user
-    print(user)
-    for note in Note.objects.all():
-      print('1', note == Note)
-      print('INST', isinstance(note, PersonalNote))
-      print('SUB', issubclass(type(note), Note))
-      print('TYPE', type(note) is Note)
-      print(type(note))
-      print('OIOI', type(note) == Note)
-      print(note.__class__)
+  # def resolve_notes(self, info):
+  #   user = info.context.user
+  #   print(user)
+  #   for note in Note.objects.all():
+  #     print('1', note == Note)
+  #     print('INST', isinstance(note, PersonalNote))
+  #     print('SUB', issubclass(type(note), Note))
+  #     print('TYPE', type(note) is Note)
+  #     print(type(note))
+  #     print('OIOI', type(note) == Note)
+  #     print(note.__class__)
 
-    return Note.objects.all()
+  #   return Note.objects.all()
 
 
-  def resolve_personalnotes(self, info):
-    user = info.context.user
-    if user.is_anonymous:
-      return PersonalNote.objects.none()
-    else:
-      return PersonalNote.objects.filter(user=user)
+  # def resolve_personalnotes(self, info):
+  #   user = info.context.user
+  #   if user.is_anonymous:
+  #     return PersonalNote.objects.none()
+  #   else:
+  #     return PersonalNote.objects.filter(user=user)
 
 
 schema = graphene.Schema(query=Query)
