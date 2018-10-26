b52adcdc06ac74af4c6e42916ec056e40fde7ab0
Will Kwon <willkwon@Williams-MacBook-Air.local>
2018-10-26 10:20:36 -0700

    changed

--- quotes/settings.py
+++ quotes/settings.py
@@ -79,9 +79,13 @@ WSGI_APPLICATION = 'quotes.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }}
 
-DATABASES['default']=dj_database_url.config(config('DATABASE_URL'))
+DATABASES['default']['NAME']=dj_database_url.config(config('DATABASE_URL'))
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
