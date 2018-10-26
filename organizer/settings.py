f0d86197dccdcac7395efad1d37f66d3a8dc63a5
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-10-26 10:49:33 -0400

    still working on getting allowed_hosts to work

--- organizer/settings.py
+++ organizer/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 from rest_framework.authentication import (
     SessionAuthentication, BasicAuthentication, TokenAuthentication)
-from decouple import config
+from decouple import config, Csv
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 CORS_ORIGIN_ALLOW_ALL = True
 
