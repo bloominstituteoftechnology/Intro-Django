767078e80e65e994b63c612db43c2e158c0e1a78
sca4cs <shannon.atkinson.ca@gmail.com>
2018-10-25 20:15:56 -0400

    add static files to staticfiles folder

--- djorg/settings.py
+++ djorg/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 REST_FRAMEWORK = {
@@ -146,6 +146,3 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     )
 }
-
-import django_heroku
-django_heroku.settings(locals())
