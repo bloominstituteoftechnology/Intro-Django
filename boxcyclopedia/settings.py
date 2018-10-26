231032158912c6287b0f254122e45e2fbff168ad
tulf <timzulf@gmail.com>
2018-10-25 23:11:52 -0400

    staticfi,es

--- boxcyclopedia/settings.py
+++ boxcyclopedia/settings.py
@@ -83,12 +83,14 @@ WSGI_APPLICATION = 'boxcyclopedia.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+DATABASES = {}
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
@@ -147,4 +149,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
