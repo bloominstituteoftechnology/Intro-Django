d49b2368fb43975cbaef164fc3f19ad68a632432
Sam Khaled <samkhaled@outlook.com>
2018-10-27 19:46:02 -0400

    added django-heroku to deployment

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 from decouple import config
 import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -153,3 +154,5 @@ STATIC_URL = '/static/'
 
 # Static File config/WhiteNoise
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# Activate Django-Heroku.
+django_heroku.settings(locals())
