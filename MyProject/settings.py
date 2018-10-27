93a56bfc488cefd1ea42fc6a1162834325c028cc
Sam Khaled <samkhaled@outlook.com>
2018-10-27 12:59:26 -0400

    Added Procfile and got deployement to Heroku ready

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 from decouple import config
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -25,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = config('DEBUG')
+DEBUG = config('DEBUG', cast=bool)
 # DEBUG = os.environ.get("DEBUG", False)
 
 ALLOWED_HOSTS = [
@@ -36,7 +37,7 @@ ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1'
     ]
-# ALLOWED_HOSTS = config('CORS_ORIGIN_WHITELIST').split(',')
+# ALLOWED_HOSTS = config('CORS_ORIGIN_WHITELIST', cast=lambda str:[s.strip() for s in str.split(',')])
 # CORS_ORIGIN_ALLOW_ALL = True
 # Application definition
 
@@ -57,6 +58,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -89,12 +91,15 @@ WSGI_APPLICATION = 'MyProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
@@ -144,3 +149,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Static File config/WhiteNoise
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
