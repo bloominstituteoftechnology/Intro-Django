cfc7ccd9735a97f87ead077484bd304fd2965c09
darthMinimus <irishseanvaldivia@gmail.com>
2018-11-07 14:09:44 -0600

    Working through lecture video.

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Note(models.Model):
+    id = 
+    title = models.CharField(max_length = 30)
+    body = models.TextField(blank = True)
+    author = models.CharField()
+    date = models.DateField()
