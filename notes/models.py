fbbc6d9a1e04dbb4f40afac1dda21322ea83bbe9
Andrew Jarrett <ahrjarrett@gmail.com>
2018-11-06 22:08:12 -0600

    create notes app, initialize empty Note model

---
+++ notes/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+from uuid import uuid4
+
+# Create your models here.
+class Note(models.Model):
+    # id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
