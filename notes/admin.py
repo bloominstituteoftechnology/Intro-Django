93fbea0b1b4d3388932a7ac1b1faacdbb4f59d31
unknown <alexisreyes90@gmail.com>
2018-11-07 22:52:39 -0500

    Day 2 MVP complete

--- notes/admin.py
+++ notes/admin.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Note, PersonalNote
 
-# Register your models here.
+
+class NoteAdmin(admin.ModelAdmin):
+	readonly_fields=('created_at', 'last_modified')
+
+admin.site.register(Note)
+admin.site.register(PersonalNote)
--- notes/models.py
+++ notes/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
 from uuid import uuid4
 
@@ -8,3 +9,10 @@ class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=150)
     content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+
+# Inherits from Note!
+class PersonalNote(Note):   
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
