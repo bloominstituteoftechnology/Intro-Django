c5c2783c4444bd2459adbc61b8cdd45a329807e5
Victor Montoya <victoramontoya@gmail.com>
2018-11-07 20:34:40 -0800

    first commit - day 1 doneski

---
+++ notes/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from uuid import uuid4
+
+# Create your models here.
+class Note ( models.Model ):
+    id = models.UUIDField( primary_key = True, default = uuid4, editable = False)
+    title = models.CharField( max_length = 30)
+    body = models.TextField( blank = True )
+    author = models.CharField(max_length=30, blank=True)
+    date = models.DateTimeField( auto_now_add=True)
+    last_updated = models.DateTimeField( auto_now = True)
+    completed = models.BooleanField(default = False)
