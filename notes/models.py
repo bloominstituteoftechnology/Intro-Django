fbd23b9411f513288800d28fdfec80264bfd2ffb
iamthuypham <tpham045@hotmail.com>
2018-11-07 19:20:34 -0800

    Day 2 complete

--- notes/models.py
+++ notes/models.py
@@ -2,10 +2,16 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from uuid import uuid4
+from django.contrib.auth.models import User
+import uuid
 
 # Create your models here.
 class Note(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=30)
     description = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+class PersonalNote(Note):   # Inherits from Note!
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
