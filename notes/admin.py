b3b9d4bd42b425fbb25591d0393bf12c668405ad
darthMinimus <irishseanvaldivia@gmail.com>
2018-11-09 14:35:34 -0600

    Learning as I go, fixed default error and created Author model.

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Author, Note
+
+admin.site.register(Author)
+admin.site.register(Note)
+
--- notes/models.py
+++ notes/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from uuid import uuid4
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Note(models.Model):
@@ -9,5 +10,16 @@ class Note(models.Model):
     author = models.CharField(max_length = 30)
     date = models.DateField(auto_now=True)
     completed = models.BooleanField(default = False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+
+class Author(models.Model):
+    id = models.UUIDField(primary_key = True, default = uuid4, editable = False)
+    name = models.CharField(max_length = 33)
+
+
+
+
 
 
