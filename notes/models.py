a14bc8b9779b2509c363ac037a3ef25c9ab8ed0c
unknown <alexisreyes90@gmail.com>
2018-11-10 12:57:55 -0500

    added extra fields

--- notes/models.py
+++ notes/models.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import User
 from django.db import models
 from uuid import uuid4
 
+
+# admin note
 class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=150)
@@ -12,7 +14,18 @@ class Note(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
 
+class Personal_Tags(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    title = models.CharField(max_length=150)
+    note = models.ForeignKey(Note, on_delete=models.CASCADE)
 
-# Inherits from Note!
+
+# Inherits from Note! per user
 class PersonalNote(Note):   
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # ptag = models.ForeignKey(Personal_Tags, on_delete=models.CASCADE)
+
+
+
+    
+
