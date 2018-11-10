fbbacdfc74713bb10fe889fb5b83598a6e50edb1
darthMinimus <irishseanvaldivia@gmail.com>
2018-11-10 15:54:56 -0600

    Finishing up day 2.

--- notes/models.py
+++ notes/models.py
@@ -22,7 +22,7 @@ class PersonalNote(Note):
 class Author(models.Model):
     id = models.UUIDField(primary_key = True, default = uuid4, editable = False)
     name = models.CharField(max_length = 33)
-
+    
 
 
 
