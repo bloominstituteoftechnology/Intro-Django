551ee744c8bd0a9e62a782f45127a91d234ea052
Victor Montoya <victoramontoya@gmail.com>
2018-11-10 10:42:07 -0800

    added serializer and viewset

--- notes/api.py
+++ notes/api.py
@@ -1,5 +1,13 @@
-from rest_framework import serializers
+from rest_framework import serializers, viewsets
 from .models import Author, Note, PersonalNote
 
 
 class PersonalNoteSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = PersonalNote
+        fields = ('title', 'body')
+
+
+class PersonalNoteViewSet(viewsets.ModelViewSet):
+    serializer_class = PersonalNoteSerializer
+    queryset = PersonalNote.objects.all()
