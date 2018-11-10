e1cd319354d4796d3b02d902cfe347107e87ceba
Andrew Jarrett <ahrjarrett@gmail.com>
2018-11-10 13:52:33 -0600

    create NoteAdmin and AuthorAdmin classes, update admin settings

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Author, Note
 
-# Register your models here.
+class NoteAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'updated_at')
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'username')
+
+# Register models
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Note, NoteAdmin)
--- notes/models.py
+++ notes/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 from uuid import uuid4
 
-# Create your models here.
 class Note(models.Model):
-    id     = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    title  = models.CharField(max_length=255)
-    author = models.CharField(max_length=32)
-    date   = models.DateTimeField(auto_now=True)
-    completed = models.BooleanField(default=False)
-    body   = models.TextField(blank=True)
+    id         = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    title      = models.CharField(max_length=255)
+    author     = models.CharField(max_length=32)
+    # difference btwn auto_now and auto_now_add is
+    # auto_now_add is invoked once, when the object is first created
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    completed  = models.BooleanField(default=False)
+    body       = models.TextField(blank=True)
+
+class Author(models.Model):
+    id         = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    first_name = models.CharField(max_length=32)
+    last_name  = models.CharField(max_length=32)
+    username   = models.CharField(max_length=32)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
