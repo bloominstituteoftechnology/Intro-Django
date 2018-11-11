888814be72eeaf2b6ab198a0e0705e82399064e8
Andrew Jarrett <ahrjarrett@gmail.com>
2018-11-11 11:50:12 -0600

    update models to user User class on Note & Author classes

--- notes/admin.py
+++ notes/admin.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from .models import Author, Note
 
 class NoteAdmin(admin.ModelAdmin):
+    list_display = ('title', 'written_by', 'updated_at')
+    list_filter = ('updated_at', 'completed')
     readonly_fields = ('created_at', 'updated_at')
 
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'username')
+    list_display = ('user',)
 
 # Register models
 admin.site.register(Author, AuthorAdmin)
--- notes/models.py
+++ notes/models.py
@@ -1,21 +1,25 @@
+from django.contrib.auth.models import User
 from django.db import models
 from uuid import uuid4
+
+class Author(models.Model):
+    id         = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    user       = models.ForeignKey(User, blank=True, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # first_name = models.CharField(max_length=32)
+    # last_name  = models.CharField(max_length=32)
+    # username   = models.CharField(max_length=32)
 
 class Note(models.Model):
     id         = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title      = models.CharField(max_length=255)
-    author     = models.CharField(max_length=32)
-    # difference btwn auto_now and auto_now_add is
-    # auto_now_add is invoked once, when the object is first created
+    author     = models.ForeignKey(Author, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     completed  = models.BooleanField(default=False)
     body       = models.TextField(blank=True)
 
-class Author(models.Model):
-    id         = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    first_name = models.CharField(max_length=32)
-    last_name  = models.CharField(max_length=32)
-    username   = models.CharField(max_length=32)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    def written_by(self):
+        # why is username on `author.user` instead of just `author` ?
+        return self.author.user.username
