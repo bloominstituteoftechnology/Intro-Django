c4263b788b96dd29e274fd0efb768d11deaf79df
darthMinimus <irishseanvaldivia@gmail.com>
2018-11-14 12:05:00 -0600

    Good morning, on my grind early.

--- demo/urls.py
+++ demo/urls.py
@@ -14,14 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from rest_framework import routers
 from notes.api import PersonalNoteViewSet
 from django.urls import path, include
 
-
-router = routers.DefaultRouter
-router.register(r'notes', PersonalNoteViewSet)
+router = routers.DefaultRouter()
+router.register('notes', PersonalNoteViewSet)
+router.register('api', PersonalNoteViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
