9a6ed6e5edaf7f0837d2461f559f39d644e3a5fb
darthMinimus <irishseanvaldivia@gmail.com>
2018-11-13 20:35:48 -0600

    Coding along with Elissa.

--- notes/admin.py
+++ notes/admin.py
@@ -2,14 +2,14 @@ from django.contrib import admin
 
 
 # Register your models here.
-from .models import Author, Note
+from .models import Author, Note, PersonalNote
 
 class NoteAdmin(admin.ModelAdmin):
 	readonly_fields=('created_at', 'last_modified')
+	list_filter=('created_at')
 
 
 admin.site.register(Author)
 admin.site.register(Note, NoteAdmin)
-
 
 
