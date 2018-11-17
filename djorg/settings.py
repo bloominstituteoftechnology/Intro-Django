a19a126d4e95dc6fa771cc7bc2ce97729c50b799
unknown <alexisreyes90@gmail.com>
2018-11-17 12:43:38 -0500

    generated new secret key, added imports for dj_database

--- djorg/settings.py
+++ djorg/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
