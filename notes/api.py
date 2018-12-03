938839870d799572f1e805f81826a1f5996dac20
Tom Tarpey <tomtarpeydev@gmail.com>
2018-12-03 21:10:58 +0000

    (MVP DAY 3) : tested the api get method and wrote and tested the create method

--- notes/api.py
+++ notes/api.py
@@ -1,11 +1,20 @@
 from rest_framework import serializers, viewsets
-from .models import PersonalNote
-
-serializer_class = PersonalNoteSerializer
-queryset = PersonalNote.objects.all()
+from .models import PersonalNote, Note
 
 class PersonalNoteSerializer(serializers.HyperlinkedModelSerializer):
-     # Inner class nested inside PersonalNoteSerializer
+
     class Meta:
         model = PersonalNote
         fields = ('title', 'content')
+    
+    def create(self, validated_data):
+        user = self.context['request'].user
+        note = PersonalNote.objects.create(user=user, **validated_data)
+        return note
+
+class PersonalNoteViewSet(viewsets.ModelViewSet):
+    serializer_class = PersonalNoteSerializer
+    queryset = PersonalNote.objects.all()
+
+
+    
