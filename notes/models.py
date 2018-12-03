18ba21d285d6a402d27ed89fb802ba3d7646f0b5
Tom Tarpey <tomtarpeydev@gmail.com>
2018-12-03 17:44:27 +0000

    (MVP DAY 1) : wrote the initial notes class in the notes model file

--- notes/models.py
+++ notes/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from uuid import uuid4
 
 # TODO: Note class
 
+class Note(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField(blank=True)
+    pub_date = models.DateTimeField('date_published')
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
