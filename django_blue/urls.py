d3609fd7b0506ebe4960001bd07d1428662d75d8
Csillag61 <csillagok@gmail.com>
2018-12-03 15:30:36 -0500

    project-app connection created

--- django_blue/urls.py
+++ django_blue/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import path, url, iclude
+from users_app import views
 
 urlpatterns = [
+    url(r'^$'. views.index, name='index'),
     path('admin/', admin.site.urls),
+    url(r'^users/', include('users_app.urls')),
 ]
