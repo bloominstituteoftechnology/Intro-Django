2580f5577de69aa0fc674ddc6701eda2a2b66f42
travis-russell <russell.travis@yahoo.com>
2018-12-03 16:53:56 -0600

    day1 finished

--- djorg/notes/models.py
+++ djorg/notes/models.py
@@ -3,6 +3,6 @@ from uuid import uuid4
 
 
 class Note(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=200)
     content = models.TextField(max_length=200)
-    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
