6c32088197ec0ef8ab67c6a8d382999f9b09bef2
mparedes003 <michelle.g.paredes@gmail.com>
2018-12-03 10:50:49 -0800

    FIxed typo on primary key. Added notes to the INSTALLED_APPS in settings.py to let our projectknow that notes app exists.

--- notes/models.py
+++ notes/models.py
@@ -6,6 +6,6 @@ class Note(models.Model): # This gives our new class access to all of the built-
   # Primary key is how the database tracks records.
   # Default calls a function to randomly generate a unique identifier.
   # We make editable false because we never want to change the key
-  id = models.UUIDField(primary_key+True, default=uuid4,editable=False) # serves as a unique identifier for each record
+  id = models.UUIDField(primary_key=True, default=uuid4, editable=False) # serves as a unique identifier for each record
   title = models.CharField(max_length=200)
   content = models.TextField(blank=True)
