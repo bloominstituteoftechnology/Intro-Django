408b0ebcfcc65d33b720a1599d75687eff5e2723
Tom Tarpey <tomtarpeydev@gmail.com>
2018-12-03 20:21:46 +0000

    (MVP DAY 3) : installed rest framework and added it to the projects apps. also added thr permission classes

--- djorg/settings.py
+++ djorg/settings.py
@@ -39,7 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
