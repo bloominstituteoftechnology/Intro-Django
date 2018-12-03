dd871364a0f9386dc48aa056405935dd3d49a8af
BrianARuff <thelonewolf198927@gmail.com>
2018-12-03 14:11:15 -0500

    added a book model

--- books/models.py
+++ books/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from uuid import uuid4
 
 # Create your models here.
-class Note(models.Model):
+class Book(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=200, blank=False, editable=True)
-    content = models.TextField(blank=False, editable=True)
+    author = models.CharField(max_length=200, blank=False, editable=True)
