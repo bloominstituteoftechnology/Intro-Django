4f623876ddcfa33ae836a327db74287fa0a95225
mparedes003 <michelle.g.paredes@gmail.com>
2018-12-03 10:30:11 -0800

    Added data model Note in models.py. Added fields for id to act as index, title for title of note and content for content of note.

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
+class Note(models.Model): # This gives our new class access to all of the built-in functionality in models.Model
+  # Primary key is how the database tracks records.
+  # Default calls a function to randomly generate a unique identifier.
+  # We make editable false because we never want to change the key
+  id = models.UUIDField(primary_key+True, default=uuid4,editable=False) # serves as a unique identifier for each record
+  title = models.CharField(max_length=200)
+  content = models.TextField(blank=True)
