75fdcc63b5702d23653d17490f747791eabb34a5
jlaluces123 <jlalucescareer123@gmail.com>
2018-12-03 15:09:30 -0800

    Day 1 - Intro: Created Note Model and added it to the INSTALLED_APPS

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
+class Note(models.Model): # --> Inherit from models.Model
+  id = models.UUIDField(primary_key = True, default = uuid4, editable = False) 
+  # --> Primary id || always unique and searchable || 
+  # --> uuid4 will generate a super long string for it
+  # --> not editable
+  title = models.CharField(max_length = 200)
+  content = models.TextField(blank = True) # --> Kind of like a text input field || allow field to be blank
