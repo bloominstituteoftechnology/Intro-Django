2072a9e8a55ed17c3ad7d72936d307c46c81ff89
Csillag61 <csillagok@gmail.com>
2018-12-03 15:47:13 -0500

    users_app/migration got rid of "no model.object" error

--- django_blue/urls.py
+++ django_blue/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import path, url, iclude
+from django.conf.urls import url
+from django.conf.urls import include
+from django.urls import path
 from users_app import views
 
 urlpatterns = [
-    url(r'^$'. views.index, name='index'),
+    url(r'^$', views.index, name='index'),
     path('admin/', admin.site.urls),
     url(r'^users/', include('users_app.urls')),
 ]
