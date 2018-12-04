2b2a1e412a63a048030c27938ad76fc47b188db6
Anthony Greb <agreb255@gmail.com>
2018-12-04 18:49:25 -0500

    got to SQL

---
+++ notes/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from .models import Note, PersonalNote
+
+
+class NoteAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'last_modified')
+
+
+admin.site.register(Note, NoteAdmin)
+admin.site.register(PersonalNote)
