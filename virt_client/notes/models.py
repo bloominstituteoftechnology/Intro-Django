742c04fc80c94f0e5aff7805eb37bc4bbe7885c9
Nicocchi <nicocchilover@gmail.com>
2018-12-04 12:19:56 -0500

    personal notes

--- virt_client/notes/models.py
+++ virt_client/notes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from uuid import uuid4
 
 
@@ -8,3 +9,7 @@ class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
+
+
+class PersonalNote(Note):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
