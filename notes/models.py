cb7cbdb852043f342a790e3a8674549736c06f6f
verydecent <wnjhwng@gmail.com>
2018-12-04 12:14:24 -0800

    creted one more Model

--- notes/models.py
+++ notes/models.py
@@ -7,7 +7,25 @@ class Note(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
 
-# When changing Model, you must
-#   ./manage.py makemigration
-#   ./manage.py migrate 
-# in order to update the model
+class URLS(models.Model):
+    title = models.CharField(max_length=100)
+    url = models.URLField(max_length=200)
+
+# In instances where Models need to be updated,
+#  you must ./manage.py makemigration, then ./manage.py migrate to update the model
+
+# models.Model
+#   gives Django functionality 
+#   eg access to fields such as UUIDField,Charfield, TextField, URLField
+
+# The difference between ./manage.py shell and pipenv shell
+#   think of scope locality
+
+# One advantage to using Django, you can create data quick and easily in the ./manage.py shell
+"""
+for i in range(0, 40):
+    n = Note(title=f"Title{i}", content="Details")
+    # save() is inherited from models.Model
+    # save() is the save button on the GUI
+    n.save()
+"""
