d78375828e0816f78e23d897b93152d17481a0dc
verydecent <wnjhwng@gmail.com>
2018-12-04 11:38:17 -0800

    migrates Notes Model

--- notes/models.py
+++ notes/models.py
@@ -6,3 +6,8 @@ class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
+
+# When changing Model, you must
+#   ./manage.py makemigration
+#   ./manage.py migrate 
+# in order to update the model
