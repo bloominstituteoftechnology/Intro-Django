604b4868383d47a0dd4ad4b76f288f7f0071532c
Timothy Hoang <timh1203@gmail.com>
2018-12-03 19:27:04 -0600

    Add City subclass

--- travelust/models.py
+++ travelust/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from uuid import uuid4
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Country(models.Model):
@@ -11,3 +12,8 @@ class Country(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
+
+
+class City(Country):
+    location = models.ForeignKey(User, on_delete=models.CASCADE)
+
