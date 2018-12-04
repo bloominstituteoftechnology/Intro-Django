76ae8ad644a0d4aae0b1eda459b9cdf1383c66fc
Nicocchi <nicocchilover@gmail.com>
2018-12-04 12:18:10 -0500

    migrations with new fields

--- virt_client/notes/models.py
+++ virt_client/notes/models.py
@@ -6,3 +6,5 @@ class Note(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
