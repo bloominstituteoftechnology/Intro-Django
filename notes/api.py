aea4283818dcd81702b693abe58ff6df4e7f7eec
Carlos Vargas <cornielleandres@gmail.com>
2018-12-04 14:14:29 -0500

    Add the required user field.

--- notes/api.py
+++ notes/api.py
@@ -2,10 +2,13 @@ from rest_framework import serializers, viewsets
 from .models import PersonalNote
 
 class PersonalNoteSerializer(serializers.HyperlinkedModelSerializer):
-	# Inner class nested inside PersonalNoteSerializer
-    class Meta:
-        model = PersonalNote
-        fields = ('title', 'content')
+	class Meta:	# Inner class nested inside PersonalNoteSerializer
+		model = PersonalNote
+		fields = ('title', 'content')
+	def create(self, validated_data):
+		user = self.context['request'].user
+		note = PersonalNote.objects.create(user=user, **validated_data)
+		return note
 
 class PersonalNoteViewSet(viewsets.ModelViewSet):
 	serializer_class = PersonalNoteSerializer
