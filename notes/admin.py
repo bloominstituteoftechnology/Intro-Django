78ff6debc751f1cd13fb871201ca2082136d0179
jaymzsocool <james.mcraejr@gmail.com>
2018-12-04 16:26:27 -0500

    added missing day stuff

--- notes/admin.py
+++ notes/admin.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 from .models import Note, PersonalNote
 
 # Register your models here.
-
-admin.site.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+	readonly_fields=('created_at', 'last_modified')
+    
+admin.site.register(Note, NoteAdmin)
 admin.site.register(PersonalNote)
 
--- notes/models.py
+++ notes/models.py
@@ -9,6 +9,8 @@ class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
 
 class PersonalNote(Note):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
