2fc986449be116d7bc4a908e142d62177cbbac75
mparedes003 <michelle.g.paredes@gmail.com>
2018-12-04 13:27:23 -0800

    Filtered the results by  user. in api.py

--- notes/api.py
+++ notes/api.py
@@ -12,9 +12,31 @@ class PersonalNoteSerializer(serializers.HyperlinkedModelSerializer):
         model = PersonalNote
         fields = ('title', 'content')
 
+    def create(self, validated_data): # overrides the default creates functionality
+      # import pdb; pdb.set_trace()  # Start the debugger here
+
+      user = self.context['request'].user
+      # make a new persoinal note with the validated_data
+      note = PersonalNote.objects.create(user=user, **validated_data)
+        # return the note
+      return note
+
 class PersonalNoteViewSet(viewsets.ModelViewSet): # this will tell us which row we are interested in showing
   # Links it back to the serializer class we made, PersonalNoteSerializer
   serializer_class = PersonalNoteSerializer
   # add which records to search for
   # for now we are searching all the records
   queryset = PersonalNote.objects.all()
+
+  # overrides queryset to be more specific
+  # has access to the request directly, so it can be found with self.request.user
+  def get_queryset(self): 
+    user = self.request.user
+
+    # If user.is_anonymous
+    if user.is_anonymous:
+      # return an empty dictionary of notes
+      return PersonalNote.objects.none()
+    else:
+      # use a filter to return only the correct ones with Note.objects.filter(user=user)
+      return PersonalNote.objects.filter(user=user)
