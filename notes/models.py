5fe039a5aba49a320a066b107066764f7ce70337
MichelleOk <umbralcompendium@gmail.com>
2018-12-04 06:11:40 -0700

    day 1 complete

---
+++ notes/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from uuid import uuid4
+
+class Note(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField(blank=True)
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+
+    def __str__(self):
+        return (f"Title: {self.title} Content: {self.content}")
+
