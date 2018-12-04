16df01543032b94b0aca17eb6a600e7f524fca6a
Tom Tarpey <tomtarpeydev@gmail.com>
2018-12-04 12:37:01 +0000

    (POLLS APP) : refactored index view

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Question
 
-# index view
+# Index View
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
