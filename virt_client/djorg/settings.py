b67a463d6a1a45ffa9bd62cc7f86b5280142a5bc
Nicocchi <nicocchilover@gmail.com>
2018-12-04 12:52:04 -0500

    expose the personalnotes model

--- virt_client/djorg/settings.py
+++ virt_client/djorg/settings.py
@@ -38,8 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'notes',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
