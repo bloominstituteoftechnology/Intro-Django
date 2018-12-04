03007a7964e1cc4bb559f6fb253db1f6e49ed512
Tom Tarpey <tomtarpeydev@gmail.com>
2018-12-04 12:27:00 +0000

    (STRETCHING) : added was_published_recently method to the Question class

--- polls/models.py
+++ polls/models.py
@@ -6,6 +6,9 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date_published')
 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
     def __str__(self):
         return self.question_text
 
