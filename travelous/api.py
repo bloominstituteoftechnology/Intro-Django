055e8f61cf412437e74197093c8d7f4e0a8f1838
Timothy Hoang <timh1203@gmail.com>
2018-12-04 12:46:44 -0600

    Add subclass exposure

---
+++ travelous/api.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers, viewsets
+from .models import City
+
+
+class CitySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = City
+        fields = ("country", "capital")
+
+
+class CityViewSet(viewsets.ModelViewSet):
+    serializer_class = CitySerializer
+    queryset = City.objects.all()
