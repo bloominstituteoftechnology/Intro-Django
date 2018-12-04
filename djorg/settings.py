ce8f065c4a357fa9a7af3a8c9864373629213d65
iAmAdamReid <adam@adamreid.me>
2018-12-04 15:25:50 -0800

    Attempt to fix browser error table_name__old was unsuccessful

--- djorg/settings.py
+++ djorg/settings.py
@@ -78,6 +78,8 @@ WSGI_APPLICATION = 'djorg.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': './database.sqlite3'
+        # OLD VALUE
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
