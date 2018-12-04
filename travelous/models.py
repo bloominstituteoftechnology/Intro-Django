6b6fea7ee788484966008744d75519d274d00d61
Timothy Hoang <timh1203@gmail.com>
2018-12-04 09:45:57 -0600

    Add Country model

--- travelous/models.py
+++ travelous/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
+class Country(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    country = models.CharField(max_length=200)
+    capital = models.TextField(blank=True)
+    wiki = models.URLField()
+    visited = models.BooleanField(default=False)
