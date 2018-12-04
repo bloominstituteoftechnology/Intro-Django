b31e0ae650886e7d288dd0e938c534f9c14fc056
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2018-12-03 16:51:17 -0800

    day 4 instructions complete

--- djorg/settings.py
+++ djorg/settings.py
@@ -128,12 +128,16 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
         ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ]
 }
--- djorg/urls.py
+++ djorg/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 from rest_framework import routers
 from notes.api import PersonalNoteViewSet
@@ -27,5 +27,5 @@ router.register('notes', PersonalNoteViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('api-token-auth/', views.obtain_auth_token),
+    re_path(r'^api-token-auth/', views.obtain_auth_token),
 ]
