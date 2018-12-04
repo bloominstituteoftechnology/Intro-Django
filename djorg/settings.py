9a20bac5e95e6dbebab5fb940ab85c055c2f6920
Tom Tarpey <tomtarpeydev@gmail.com>
2018-12-04 12:24:27 +0000

    (STRETCHING) : adding ___str___ methods to the models in the polls app

--- djorg/settings.py
+++ djorg/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'notes.apps.NotesConfig',
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+# Question Model
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date_published')
+
+    def __str__(self):
+        return self.question_text
+
+
+# Choice Model
+
+class Choice(models.Model):
+    question = models.ForeighKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.choice_text
