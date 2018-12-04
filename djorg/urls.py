1f16b2b889e11459f74e82822258c7ee79042933
Carlos Vargas <cornielleandres@gmail.com>
2018-12-04 15:32:39 -0500

    Set up the route.

--- djorg/urls.py
+++ djorg/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework import routers
+from rest_framework.authtoken import views
 from notes.api import PersonalNoteViewSet
 
 router = routers.DefaultRouter()
@@ -24,4 +25,5 @@ router.register(r'notes', PersonalNoteViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    re_path(r'^api-token-auth/', views.obtain_auth_token),
 ]
