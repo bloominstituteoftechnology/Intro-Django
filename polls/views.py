524420e552719e2fdc6543f0e85142b43840cec4
Tom Tarpey <tomtarpeydev@gmail.com>
2018-12-04 12:49:33 +0000

    (POLLS APP) : added template for results view

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from django.template import loader
 
@@ -37,3 +37,7 @@ def vote(request, question_id):
         selected_choice.save()
 
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+    def results(request, question_id):
+        question = get_object_or_404(Question, pk=question_id)
+        return render(request, 'polls/results.html', {'question': question})
