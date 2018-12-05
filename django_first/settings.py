f3f9b3db522eba0eaaf011d3ff476b792aa7171c
Luisan <luisangelsc@gmail.com>
2018-12-04 19:07:39 -0600

    .env files

--- django_first/settings.py
+++ django_first/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+from decouple import config
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,10 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v4amxljl%swer!y+z+1%$_nr_ub85#2u5$u6^jg@p5hrczkw58'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
+#LUISAN: config siempre regresa un string, y queremos un boolean
+#por eso el cast=bool
 
 ALLOWED_HOSTS = []
 
