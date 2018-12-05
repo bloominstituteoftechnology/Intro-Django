eed69b21741cd547247689eb04adaef8a839f082
Lucas Beemer <lucasbeemer@gmail.com>
2018-12-05 14:21:23 -0800

    same bug, have not solved it

--- notes/migrations/0003_personalnote.py
+++ notes/migrations/0003_personalnote.py
@@ -9,7 +9,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('notes', '0002_auto_20181204_2331'),
+        ('notes', '0002_initial'),
     ]
 
     operations = [
