6931e4652ffe241f60590930abbe271fa2ddf7f1
MichelleOk <umbralcompendium@gmail.com>
2018-12-05 14:40:15 -0700

    finishing up day 3

--- notes/admin.py
+++ notes/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Note
+from .models import Note, PersonalNote
 
 # Register your models here.
 admin.site.register(Note)
+admin.site.register(PersonalNote)
