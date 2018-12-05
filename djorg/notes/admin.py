0f3fbfa310fc300bbbc33da7d54e9e24ae5f46b1
travis-russell <russell.travis@yahoo.com>
2018-12-05 10:27:27 -0600

    time fields

--- djorg/notes/admin.py
+++ djorg/notes/admin.py
@@ -2,3 +2,5 @@ from django.contrib import admin
 from .models import Note
 
 # Register your models here.
+
+admin.site.register(Note)
--- djorg/notes/models.py
+++ djorg/notes/models.py
@@ -6,3 +6,6 @@ class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=200)
     content = models.TextField(max_length=200)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
