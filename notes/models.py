b1b5518cfdb454718bcad13875e3ca36efc61f7f
Cameron Ray <cameronray@Camerons-MacBook-Pro.local>
2018-12-05 15:13:30 -0800

    trying to finish all of day 2 stuff

--- notes/models.py
+++ notes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+
 from uuid import uuid4
 
 # Create your models here.
@@ -7,3 +8,6 @@ class Note(models.Model):  # extends the Model class from django.db
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)  
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)  # see day2.md re: arguments here
+    last_modified = models.DateTimeField(auto_now=True)
+
