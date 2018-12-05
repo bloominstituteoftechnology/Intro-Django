ad47aca6447c4fbeb2e54fce79a6774cd19480db
Carlos Vargas <cornielleandres@gmail.com>
2018-12-05 15:09:52 -0500

    Register Recipe and Ingredient mdoels with admin site.

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Recipe, Ingredient
+
+class RecipeAdmin(admin.ModelAdmin):
+	readonly_fields=('created_at', 'last_modified')
 
 # Register your models here.
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient)
