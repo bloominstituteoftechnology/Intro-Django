a31c359e24503355b2ab6858d0e10af31bc89d69
Timothy Hoang <timh1203@gmail.com>
2018-12-05 09:39:22 -0600

    Add SC and homepage layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import styled from 'styled-components'
+import Login from './components/Login'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <Div1>
+        <Img1 src={logo} alt="logo" />
+        <Login />
+      </Div1>
+    )
   }
 }
+
+const Div1 = styled.div`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const Img1 = styled.img`
+  animation: App-logo-spin1 infinite 30s linear;
+  height: 20vmin;
+
+  @keyframes App-logo-spin1 {
+  from {
+    transform: rotate(360deg);
+  }
+  to {
+    transform: rotate(0deg);
+  }
+}
+`
 
 export default App;
---
+++ client/src/components/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      <p>Login</p>
+    </div>
+  )
+}
+
+export default Login
