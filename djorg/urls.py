5d6c593384661bd057d2304c17daf70b3f61dd47
David Doswell <davidoliverdoswell@icloud.com>
2018-12-04 18:02:57 -0800

    registered default router, api and user request

--- djorg/urls.py
+++ djorg/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from rest_framework import routers
+from notes.api import PersonalNoteViewSet
+from django.urls import path, include
+
+router = routers.DefaultRouter()
+router.register(r'notes', PersonalNoteViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls))
 ]
--- notes/api.py
+++ notes/api.py
@@ -1,10 +1,16 @@
 from rest_framework import serializers, viewsets
 from .models import PersonalNote
 
-class Meta:
-     model = PersonalNote
-     fields = ('title', 'content')
-
 class PersonalNoteSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+         model = PersonalNote
+         fields = ('title', 'content')
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+        note = PersonalNote.objects.create(user=user, **validated_data)
+        return note
+
+class PersonalNoteViewSet(viewsets.ModelViewSet):
     serializer_class = PersonalNoteSerializer
     queryset = PersonalNote.objects.all()
