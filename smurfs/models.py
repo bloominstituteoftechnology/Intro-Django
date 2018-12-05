3a39d6c62afada48a539497e21dd58057953199a
mparedes003 <michelle.g.paredes@gmail.com>
2018-12-05 12:01:28 -0800

    Changed django model data types in smurfs class age and size from CharField to PositiveSmallIntegerField so it can take in small positive numbers from 0 to 32767.

--- smurfs/models.py
+++ smurfs/models.py
@@ -9,9 +9,9 @@ class Smurf(models.Model): # This gives our new class access to all of the built
   # Default calls a function to randomly generate a unique identifier.
   # We make editable false because we never want to change the key
   id = models.UUIDField(primary_key=True, default=uuid4, editable=False) # serves as a unique identifier for each record
-  name = models.CharField(max_length=200)
-  age = models.CharField(max_length=200)
-  size = models.CharField(max_length=200)
+  name = models.CharField(max_length=50)
+  age = models.PositiveSmallIntegerField('age in years')
+  size = models.PositiveSmallIntegerField('height in inches')
   created_at = models.DateTimeField(auto_now_add=True) # track created dates, auto_now_add only sets create
   last_modified = models.DateTimeField(auto_now=True) # track modified dates, auto_now sets both create and update
 
