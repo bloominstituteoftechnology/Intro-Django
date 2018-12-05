a849ad9082d1be66a4d27caa32c1b4795621f2e6
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-12-04 19:59:06 -0500

    Migration w/ New Fields Now tracking created and modified dates

--- notes/admin.py
+++ notes/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Note
 
-admin.site.register(Note)
+
+
 
 # Register your models here.
+class NoteAdmin(admin.ModelAdmin):
+	readonly_fields=('created_at', 'last_modified')
+
+admin.site.register(Note, NoteAdmin)
