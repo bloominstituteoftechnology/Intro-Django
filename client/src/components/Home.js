5fb89187b385497d1999e35f52c7e93976804a47
Timothy Hoang <timh1203@gmail.com>
2018-12-05 16:47:40 -0600

    Add new country form

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,34 +1,63 @@
 import React from 'react'
 import styled from 'styled-components'
+import NewCountryForm from './NewCountryForm'
 
 class Home extends React.Component {
+  state = {
+    isCreating: false
+  }
+
   componentDidMount() {
     this.props.getData()
   }
 
+  toggleCreate = (e) => {
+    e.preventDefault()
+    this.setState({ isCreating: !this.state.isCreating })
+  }
+
   render() {
     return (
-      <Div>
+      <>
         {
-          this.props.countries.map(country => (
-            <Div2 key={country.country}>
-              <p><Span1>Country</Span1>: {country.country}</p>
-              <p><Span1>Capital</Span1>: {country.capital}</p>
-              <p><Span1>Wikipedia</Span1>: {country.wiki}</p>
-              {country.visited ? <p><Span1>Visited</Span1>: Yes</p> : <p><Span1>Visited</Span1>: No</p>}
-            </Div2>
-          ))
+          this.state.isCreating ? (
+            <Div1>
+              <NewCountryForm
+                toggleCreate={this.toggleCreate}
+              />
+              <Button1 onClick={e => this.toggleCreate(e)}>Cancel</Button1>
+            </Div1>
+          ) : (
+              <Div1>
+                <Div2>
+                  {
+                    this.props.countries.map(country => (
+                      <Div3 key={country.country}>
+                        <p><Span1>Country</Span1>: {country.country}</p>
+                        <p><Span1>Capital</Span1>: {country.capital}</p>
+                        <p><Span1>Wikipedia</Span1>: {country.wiki}</p>
+                        {country.visited ? <p><Span1>Visited</Span1>: Yes</p> : <p><Span1>Visited</Span1>: No</p>}
+                      </Div3>
+                    ))
+                  }
+                </Div2>
+                <Button1 onClick={e => this.toggleCreate(e)}>Add New</Button1>
+              </Div1>
+            )
         }
-      </Div>
+      </>
     )
   }
 }
 
-const Div = styled.div`
+const Div1 = styled.div`
+  text-align: center;
+`
+const Div2 = styled.div`
   display: flex;
   justify-content: space-evenly;
 `
-const Div2 = styled.div`
+const Div3 = styled.div`
   border: 1px solid #61dafb;
   border-radius: 5px;
   margin: 1rem;
@@ -36,6 +65,19 @@ const Div2 = styled.div`
 `
 const Span1 = styled.span`
   font-weight: 700;
+`
+const Button1 = styled.button`
+  width: 100px;
+  margin: 1rem auto;
+  background: #61DAFB;
+  padding: 5px;
+  font-size: 16px;
+  border-radius: 2px;
+  border: none;
+  box-shadow: 0 2px 2px gray;
+  &:hover {
+    cursor: pointer;
+  }
 `
 
 export default Home
---
+++ client/src/components/NewCountryForm.js
@@ -0,0 +1,121 @@
+import React from 'react'
+import styled from 'styled-components'
+
+class CountryForm extends React.Component {
+  state = {
+    country: "",
+    capital: "",
+    wiki: "",
+    visited: false
+  }
+
+  handleChange = (e) => {
+    if (e.target.name === "visited") {
+      this.setState({ visited: !this.state.visited })
+    }
+    else {
+      this.setState({
+        [e.target.name]: e.target.value
+      })
+    }
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.setState({
+      country: "",
+      capital: "",
+      wiki: "",
+      visited: false
+    })
+  }
+
+  render() {
+    return (
+      <Form1 onSubmit={this.handleSubmit}>
+        <Label>
+          <Input
+            name="country"
+            type="text"
+            placeholder="🌐 country"
+            onChange={this.handleChange}
+            value={this.state.country}
+          />
+        </Label>
+        <Label>
+          <Input
+            name="capital"
+            type="text"
+            placeholder="📌 capital"
+            onChange={this.handleChange}
+            value={this.state.capital}
+          />
+        </Label>
+        <Label>
+          <Input
+            name="wiki"
+            type="text"
+            placeholder="𝘞 wikipedia"
+            onChange={this.handleChange}
+            value={this.state.wiki}
+          />
+        </Label>
+        <Label>
+          Visited:
+          <Input
+            name="visited"
+            type="checkbox"
+            placeholder="🛩 visited"
+            onChange={this.handleChange}
+            checked={this.state.visited}
+          // value={this.state.visited}
+          />
+        </Label>
+        <Button
+          type="submit"
+        >
+          Submit
+        </Button>
+      </Form1>
+    )
+  }
+}
+
+const Form1 = styled.form`
+  background: white;
+  display: flex;
+  flex-direction: column;
+  border: none;
+  border-radius: 5px;
+  box-shadow: 0 0 10px black;
+  padding: 1rem;
+`
+const Label = styled.label`
+  margin: 1rem;
+  color: black;
+  text-align: center;
+`
+const Input = styled.input`
+  border-radius: 2px;
+  padding: 5px;
+  font-size: 16px;
+  text-align: center;
+  margin: 0 1rem;
+  box-shadow: 0 2px 2px gray;
+  border: none;
+`
+const Button = styled.button`
+  width: 50%;
+  margin: 1rem auto;
+  background: #61DAFB;
+  padding: 5px;
+  font-size: 16px;
+  border-radius: 2px;
+  border: none;
+  box-shadow: 0 2px 2px gray;
+  &:hover {
+    cursor: pointer;
+  }
+`
+
+export default CountryForm
