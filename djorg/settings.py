8ae006f4766a0505517c877280d1c1c793f3c0a3
Cameron Ray <cameronray@Camerons-MacBook-Pro.local>
2018-12-06 13:56:19 -0800

    CORS added

--- djorg/settings.py
+++ djorg/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
 
@@ -48,6 +49,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- notes/api.py
+++ notes/api.py
@@ -2,7 +2,7 @@
 # to describe which parts of the model we want to expose to the API
 
 from rest_framework import serializers, viewsets
-from .models import PersonalNote
+from .models import Note, PersonalNote
 
 # Name the serializer class after what it is serializing.
 # It will inherit from the specific serializer we are using for this project:
@@ -13,13 +13,20 @@ class PersonalNoteSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('title', 'content')
 
     def create(self, validated_data):  # override default .create functionality
-        
         user = self.context['request'].user
-        note = PersonalNote.objects.create(**validated_data) # uses kwarg
+        # import pdb; pdb.set_trace()
+        # user = self.context['request'].user
+        note = PersonalNote.objects.create(user=user, **validated_data) # uses kwarg
         return note
 
 class PersonalNoteViewSet(viewsets.ModelViewSet):  # tells which rows to show
     # link to the serializer class:
     serializer_class = PersonalNoteSerializer
     # specify which records to search for:
-    queryset = PersonalNote.objects.all()
+    queryset = Note.objects.all()  # create the variable with an empty dictionary of the correct type
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_anonymous:
+            return PersonalNote.objects.none()
+        else:
+            return PersonalNote.objects.filter(user=user)
