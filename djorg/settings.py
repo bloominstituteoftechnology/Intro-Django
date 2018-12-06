1744cf87366667e6d6e4c02f0f2dca5e169144d7
MichelleOk <umbralcompendium@gmail.com>
2018-12-06 08:29:00 -0700

    day 3 done, starting day 4

--- djorg/settings.py
+++ djorg/settings.py
@@ -41,9 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -78,6 +81,8 @@ REST_FRAMEWORK = {
 }
 # ^ This will allow read/write permissions for logged in users and read only for
 # anonymous users.
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 WSGI_APPLICATION = 'djorg.wsgi.application'
 
--- notes/api.py
+++ notes/api.py
@@ -6,6 +6,7 @@ class PersonalNoteSerializer(serializers.HyperlinkedModelSerializer):
         model = PersonalNote
         fields = ('title', 'content')
     def create(self, validated_data):
+        user = self.context['request'].user
         note = PersonalNote.objects.create(user=user, **validated_data)
         return note
 
@@ -14,6 +15,11 @@ class PersonalNoteViewSet(viewsets.ModelViewSet):
     # Link this back to the serializer class we made previously:
     queryset = PersonalNote.objects.all()
     # Next, add which records to search for.  We could use filters here, but for now, grab all of them:
-    
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_anonymous:
+            return PersonalNote.objects.none()
+        else:
+            return PersonalNote.objects.filter(user=user)
 
     
