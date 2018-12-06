3616a11893d674d97d6de56b00c1081f9e00d43c
Timothy Hoang <timh1203@gmail.com>
2018-12-06 16:20:10 -0600

    Add empty static folder

--- countries/settings.py
+++ countries/settings.py
@@ -123,9 +123,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
