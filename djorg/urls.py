b7ac94039fd003ba6746057581c14b2290f7eb55
Brandon White <bwhite06@stac.edu>
2018-12-06 08:41:58 -0500

    api up and running
    adding cors

--- djorg/urls.py
+++ djorg/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework import routers
 from notes.api import PersonalNoteViewSet
+from django.urls import path, include
 
 router = routers.DefaultRouter()
 router.register(r'notes', PersonalNoteViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
--- notes/api.py
+++ notes/api.py
@@ -2,9 +2,20 @@ from rest_framework import serializers
 from rest_framework import serializers, viewsets
 from notes.models import PersonalNote
 class PersonalNoteSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
+   def create(self, validated_data):
+        note = PersonalNote.objects.create(**validated_data)
+        return note
+   class Meta:
         model = PersonalNote
         fields = ('title', 'content')
+        
 class PersonalNoteViewSet(viewsets.ModelViewSet):
         serializer_class = PersonalNoteSerializer
-        queryset = PersonalNote.objects.all()
+        queryset = PersonalNote.objects.none()
+def get_queryset(self):
+        user = self.request.user
+
+        if user.is_anonymous:
+            return PersonalNote.objects.none()
+        else:
+            return PersonalNote.objects.filter(user=user)
