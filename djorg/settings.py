98d85673d8a817da58e88e72a7b1a2dbe73c7234
mparedes003 <michelle.g.paredes@gmail.com>
2018-12-05 17:47:58 -0800

    Edited DATABASES insettings.py

--- djorg/settings.py
+++ djorg/settings.py
@@ -89,11 +89,13 @@ WSGI_APPLICATION = 'djorg.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        #django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
