6119a80e6a1a1ad563d497c372ecaa3aad012462
BrianARuff <thelonewolf198927@gmail.com>
2018-12-06 18:09:29 -0500

    added home page for API and admin links

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("""
+        <b>1.</b> <a href="http://127.0.0.1:8000/admin/">Admin</a><br><br>
+        <b>2.</b> <a href="http://127.0.0.1:8000/api/">API</a><br><br>
+        <b>3.</b> <a href="http://127.0.0.1:8000/api-token-auth/">API Token Auth</a>
+    """)
