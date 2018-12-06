0292b55ad818160e18d65a7a445233ada96ada15
BrianARuff <thelonewolf198927@gmail.com>
2018-12-06 18:36:37 -0500

    dj-db-url config

--- djorn/settings.py
+++ djorn/settings.py
@@ -101,6 +101,7 @@ DATABASES = {
     }
 }
 
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
