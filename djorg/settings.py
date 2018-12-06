abf61eb90ac32c395d4ec7085380a2f429998711
Carlos Vargas <cornielleandres@gmail.com>
2018-12-05 19:12:48 -0500

    Move cors down in middleware list.

--- djorg/settings.py
+++ djorg/settings.py
@@ -47,9 +47,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
