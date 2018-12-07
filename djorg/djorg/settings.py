86b29a206338aa946987a843d56941d5873665fd
travis-russell <russell.travis@yahoo.com>
2018-12-07 14:29:15 -0600

    settings

--- djorg/djorg/settings.py
+++ djorg/djorg/settings.py
@@ -17,11 +17,6 @@ import dj_database_url
 from whitenoise import WhiteNoise
 
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
-DATABASES['default'] = dj_database_url.config(default='postgres://...')
-DATABASES['default'] = dj_database_url.parse(
-    'postgres://...', conn_max_age=600)
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -153,3 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES['default'] = dj_database_url.config(default='postgres://...')
+DATABASES['default'] = dj_database_url.parse(
+    'postgres://...', conn_max_age=600)
