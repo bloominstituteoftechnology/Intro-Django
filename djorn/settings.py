d74e442374cc0808a017c261eb19bcae791b0986
BrianARuff <thelonewolf198927@gmail.com>
2018-12-07 13:17:11 -0500

    white noise extra

--- djorn/settings.py
+++ djorn/settings.py
@@ -146,4 +146,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
