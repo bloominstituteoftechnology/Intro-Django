571783f51ab376c5107330e044fd7e2c741cd920
BrianARuff <thelonewolf198927@gmail.com>
2018-12-07 13:02:59 -0500

    working on prod db setting

--- djorn/settings.py
+++ djorn/settings.py
@@ -102,6 +102,9 @@ DATABASES = {
     }
 }
 
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+
+
 # DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
