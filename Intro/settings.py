0accfa2a71cbee0ce976fa1c48014ac3a64f137a
David Layman <davidlayman85@gmail.com>
2018-12-07 15:51:11 -0500

    hopefully this works

--- Intro/settings.py
+++ Intro/settings.py
@@ -29,9 +29,6 @@ DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -132,3 +129,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
