dceb4c31f45603f0479f4ae65dceb1dfc6838e89
BrianARuff <thelonewolf198927@gmail.com>
2018-12-06 19:54:46 -0500

    h

--- djorn/settings.py
+++ djorn/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djorn-brian-ruff.herokuapp.com']
 
 CORS_ORIGIN_ALLOW_ALL = True
 
