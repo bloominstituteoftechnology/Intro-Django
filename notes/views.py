9debf94443e70539794df42eba2ce5d6c2aa75e7
BrianARuff <thelonewolf198927@gmail.com>
2018-12-06 20:03:28 -0500

    fixed links

--- notes/views.py
+++ notes/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("""
-        <b>1.</b> <a href="http://127.0.0.1:8000/admin/">Admin</a><br><br>
-        <b>2.</b> <a href="http://127.0.0.1:8000/api/">API</a><br><br>
-        <b>3.</b> <a href="http://127.0.0.1:8000/api-token-auth/">API Token Auth</a>
+        <b>1.</b> <a href="https://djorn-brian-ruff.herokuapp.com/admin/">Admin</a><br><br>
+        <b>2.</b> <a href="https://djorn-brian-ruff.herokuapp.com/api/">API</a><br><br>
+        <b>3.</b> <a href="https://djorn-brian-ruff.herokuapp.com/api-token-auth/">API Token Auth</a>
     """)
