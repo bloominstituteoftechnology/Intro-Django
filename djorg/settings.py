549eed62f9fd65562e19da55ad8f085da2aeefa3
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2018-12-07 10:52:15 -0800

    removes sors middleware from list

--- djorg/settings.py
+++ djorg/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
+    # 'corsheaders.middleware.CorsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
