93778518d69f4b49ff655ef8befaead76546c178
David Layman <davidlayman85@gmail.com>
2018-12-07 17:14:32 -0500

    hope this works

--- Intro/settings.py
+++ Intro/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com','https://git.heroku.com/damp-island-18296.git','0.0.0.0','localhost']
 
 # Application definition
 
