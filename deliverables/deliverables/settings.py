549151157a8e46f55d90e04a9f7b03bd29ee41a6
Luisan <luisangelsc@gmail.com>
2018-12-07 10:45:51 -0600

    debugging

--- deliverables/deliverables/settings.py
+++ deliverables/deliverables/settings.py
@@ -82,16 +82,9 @@ WSGI_APPLICATION = 'deliverables.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)tabase_url.config(default=config('DATABASE_URL'), conn_max_age=600)
-
+DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'), conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
