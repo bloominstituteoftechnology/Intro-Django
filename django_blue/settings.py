34c4d4db88f5fbed24467b12896d2558a85b4783
Csillag61 <csillagok@gmail.com>
2018-12-06 19:18:02 -0500

    change database to remote

--- django_blue/settings.py
+++ django_blue/settings.py
@@ -93,12 +93,13 @@ WSGI_APPLICATION = 'django_blue.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {'default': dj_database_url.parse(DATABASE_URL)}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
