a9cf56c199f99091435d09e15525b349f8aaf661
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-12-07 14:22:40 -0500

    Debug

--- djorg/settings.py
+++ djorg/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 from decouple import config
+import dj_database_url
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
