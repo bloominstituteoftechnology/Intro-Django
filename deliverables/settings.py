52bdc886ef9b5306eb469093be99ecfb0a7efa68
Luisan <luisangelsc@gmail.com>
2018-12-07 11:53:03 -0600

    allowed_hosts refactor

--- deliverables/settings.py
+++ deliverables/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')]
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
+
+
 
 
 # Application definition
