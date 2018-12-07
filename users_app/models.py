1b58bb52225d88579f3a9c37341698feae50aa3d
Csillag61 <csillagok@gmail.com>
2018-12-06 23:51:37 -0500

    models.py error fix

--- users_app/models.py
+++ users_app/models.py
@@ -6,13 +6,13 @@ from django.contrib.auth.models import User as User2
 
 
 class User(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     first_name = models.CharField(max_length=128)
     last_name = models.CharField(max_length=128)
     email = models.EmailField(max_length=264, unique=True)
 
 
 class Note(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
