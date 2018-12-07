fd34825bb8c950b9420bcaafa273ae210d013a63
jaymzsocool <james.mcraejr@gmail.com>
2018-12-07 12:45:50 -0500

    heroku

--- djorg/settings.py
+++ djorg/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-from decouple import config, dj_database_url
+from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -124,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 GRAPHENE = {
     'SCHEMA': 'notes.schema.schema'
 }
@@ -142,5 +143,3 @@ REST_FRAMEWORK = {
 
 DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'))
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
