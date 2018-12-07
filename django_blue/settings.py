6f28c01ec3584cb634e89a675355887d9298dc3c
Csillag61 <csillagok@gmail.com>
2018-12-06 21:41:30 -0500

    fixes in settings

--- django_blue/settings.py
+++ django_blue/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 from decouple import config
 import dj_database_url
-
-#import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
