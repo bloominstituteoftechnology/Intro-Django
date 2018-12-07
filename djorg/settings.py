80e61bfb3bfab437cad895d6614213611178d4f9
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2018-12-07 10:42:06 -0800

    adds database max connection variable

--- djorg/settings.py
+++ djorg/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] = dj_database_url.config(default='DATABASE_URL')
+DATABASES['default'] = dj_database_url.config(default='DATABASE_URL', conn_max_age=500)
 
 
 
