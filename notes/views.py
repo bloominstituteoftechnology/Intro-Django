0512fdeb3861faf0d6d07a7982339c4085cb3b2d
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-12-08 02:37:08 -0500

    revert unable to load html at this time

--- notes/views.py
+++ notes/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+
+# Create your views here.
+
 from django.http import HttpResponse
-from django.template import Context, loader
+
+# Create your views here.
+
 
 def index(request):
-    template = loader.get_template("notes/highlights.html")
-    return HttpResponse(template.render())
+    return HttpResponse("""
+        <h1> "Welcome to Django" </h1>
+        <b>1.</b> <a href="https://notes-huthman.herokuapp.com/admin/">Admin</a><br><br>
+        <b>2.</b> <a href="https://notes-huthman.herokuapp.com/api/">API</a><br><br>
+        <b>3.</b> <a href="https://notes-huthman.herokuapp.com/api-token-auth/">API Token Auth</a>
+    """)
