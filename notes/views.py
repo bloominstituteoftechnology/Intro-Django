165d1c8b8d0c3c38532dad8d15a9e44b451e1fd9
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-12-08 02:27:30 -0500

    render_to_response attempt

--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
 
 # Create your views here.
 
@@ -7,6 +8,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-def index(request):
-    
-    return render('highlights.html')
+def index(request):   
+    return render_to_response('notes/highlights.html')
