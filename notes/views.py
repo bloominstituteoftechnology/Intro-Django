042c3fea700e2f3a96ec11e434497a68b2cde41a
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-12-08 02:33:55 -0500

    typo

--- notes/views.py
+++ notes/views.py
@@ -1,6 +1,7 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import Context, loader
 
 def index(request):
     template = loader.get_template("notes/highlights.html")
-    return HttpResponse(template.render)
+    return HttpResponse(template.render())
