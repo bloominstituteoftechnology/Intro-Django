bf6b01c1b722060ab5e084a568f2fe36fe68b89e
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-12-07 22:09:35 -0500

    Messing with heroku

--- djorg/settings.py
+++ djorg/settings.py
@@ -99,6 +99,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USER': 'admin4',
+        'PASSWORD': '123itworks',
     }
 }
 
