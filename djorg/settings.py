d70a15c3b28b611e751b627261b96e80d89a29b2
Sasha <taylork85@gmail.com>
2018-12-09 14:51:36 -0600

    Change Template

--- djorg/settings.py
+++ djorg/settings.py
@@ -63,7 +63,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
