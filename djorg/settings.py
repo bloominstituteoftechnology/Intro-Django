ae1f3459417e7c90861db927d2efca8239808e95
MichelleOk <umbralcompendium@gmail.com>
2018-12-10 15:16:59 -0700

    deployed fixing bugs

--- djorg/settings.py
+++ djorg/settings.py
@@ -58,7 +58,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
+    # 'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
