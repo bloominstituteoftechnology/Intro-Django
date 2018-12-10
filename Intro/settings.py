69750e38115fec7890f9564d0a436ee2c55e4eea
David Layman <davidlayman85@gmail.com>
2018-12-10 11:09:43 -0500

    might work now

--- Intro/settings.py
+++ Intro/settings.py
@@ -83,14 +83,14 @@ WSGI_APPLICATION = 'Intro.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-DATABASES['default'] = dj_database_url.config(default=os.path.join(BASE_DIR, 'db.sqlite3'))
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
--- Intro/wsgi.py
+++ Intro/wsgi.py
@@ -8,12 +8,10 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
-from dotenv import load_dotenv
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Intro.settings')
-load_dotenv(os.path.join(BASE_DIR,'.env'))
 
 application = get_wsgi_application()
