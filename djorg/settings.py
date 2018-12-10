054497a8ce55741a5df15391f9d832288608ac5a
MichelleOk <umbralcompendium@gmail.com>
2018-12-10 16:35:36 -0700

    wip

--- djorg/settings.py
+++ djorg/settings.py
@@ -40,7 +40,7 @@ DEBUG = config('DEBUG', cast=bool)
 
 # ALLOWED_HOSTS='.herokuapp.com'
 # OR
- ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
 
 # Application definition
