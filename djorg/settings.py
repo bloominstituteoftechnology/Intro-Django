9239bb0c6e72ff71b3cd9b69fba8a526b7788c3e
JohnCoronel <john.m.coronel@gmail.com>
2019-01-01 21:59:27 -0800

    updated database config

--- djorg/settings.py
+++ djorg/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
-
+DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # SECURITY WARNING: don't run with debug turned on in production!
