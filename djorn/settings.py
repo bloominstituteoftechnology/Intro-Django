06f7acd63bad4639a9311adb3d182cd024522543
duckets615 <33557104+duckets615@users.noreply.github.com>
2019-02-05 09:45:26 -0500

    debu

--- djorn/settings.py
+++ djorn/settings.py
@@ -20,7 +20,7 @@ import dj_database_url
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 
