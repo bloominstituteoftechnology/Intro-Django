4bf839373a678df990845242fccff5662fad9937
Brian Illes <brianrilles@gmail.com>
2019-05-30 15:23:50 -0700

    $a shortcut test

--- notes/models.py
+++ notes/models.py
@@ -6,4 +6,5 @@ class Note(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    
+     
+     
