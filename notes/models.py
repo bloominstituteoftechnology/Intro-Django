5affc18618e2b05611e040d147094263b5f23ed2
Brian Illes <brianrilles@gmail.com>
2019-05-30 14:45:12 -0700

    ran migration and configed vars w/ decouple config

---
+++ notes/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from uuid import uuid4
+
+class Note(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    title = models.CharField(max_length=200)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
