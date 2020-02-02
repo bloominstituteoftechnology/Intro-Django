814f9a8d89a27e2ec06e34422eeb422925dbb2df
Todd McKenzie <toddmckenzie@hotmail.com>
2020-02-02 10:00:04 -0600

    added ability to add note by user

--- notes/api.py
+++ notes/api.py
@@ -8,8 +8,12 @@ class PersonalNoteSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = PersonalNote
         fields = ('title', 'content')
+    
+    def create(self, validated_data):
+        user = self.context['request'].user
+        note = PersonalNote.objects.create(user=user, **validated_data)
+        return note
 
 class PersonalNoteViewSet(viewsets.ModelViewSet):
     serializer_class = PersonalNoteSerializer
     queryset = PersonalNote.objects.all()
-    
